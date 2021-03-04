3bed76857b3c3fed9f9d9d2d4c29a0aea7d3a0cf
Alex <alex@42layers.io>
2021-03-04 08:24:43 -0800

    grammar for reassignments and while loops

--- hllc/example_programs/new.rs
+++ hllc/example_programs/new.rs
@@ -2,9 +2,16 @@ predicate {
     trait GenericTrait  <T> where T: Add {
         fn a_tr_fn(a: T): T;
     }
+
     fn main(): bool {
         let x = 5;
         let z = 5 + x;
+
+        // todo: while loop
+        while x < 5 {
+            // todo: reassignment and check mutability
+            x = x + 1;
+        }
         
         let y = 5 - 2 / 1;
         true
