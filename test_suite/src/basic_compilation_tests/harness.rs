deeb4b0b279e6fb30b3eeb1acc84519580e180f4
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-05-12 10:34:06 -0700

    forgotten add

---
+++ test_suite/src/basic_compilation_tests/harness.rs
@@ -0,0 +1,18 @@
+use forc;
+
+/// Returns `true` if a file compiled without any errors or warnings,
+/// and `false` if it did not.
+pub(crate) fn should_compile(file_name: &str) -> bool {
+    println!("Compiling {}", file_name);
+    let res = forc::build(Some(format!(
+        "src/basic_compilation_tests/test_programs/{}",
+        file_name
+    )));
+    match res {
+        Ok(_) => true,
+        Err(_) => {
+            println!("Project \"{}\" failed to compile. ", file_name);
+            return false;
+        }
+    }
+}
---
+++ test_suite/src/basic_compilation_tests/test_cases.rs
@@ -0,0 +1,12 @@
+pub fn run() {
+    let project_names = vec!["script_1"];
+    assert!(project_names.into_iter().all(|name| {
+        let result = crate::basic_compilation_tests::harness::should_compile(name);
+        if !result {
+            println!("Failure: {} should have compiled.", name);
+            false
+        } else {
+            true
+        }
+    }));
+}
