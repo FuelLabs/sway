9de2c51768d63cf3d0f1aec6350fb8242553c29f
John Adler <john.adler@protonmail.ch>
2021-05-12 22:02:11 -0400

    Support underscore in integer literals.

--- core_lang/src/parse_tree/literal.rs
+++ core_lang/src/parse_tree/literal.rs
@@ -33,6 +33,7 @@ impl<'sc> Literal<'sc> {
                         Rule::u8_integer => int_inner
                             .as_str()
                             .trim()
+                            .replace("_", "")
                             .parse()
                             .map(Literal::U8)
                             .map_err(|_| {
@@ -44,6 +45,7 @@ impl<'sc> Literal<'sc> {
                         Rule::u16_integer => int_inner
                             .as_str()
                             .trim()
+                            .replace("_", "")
                             .parse()
                             .map(Literal::U16)
                             .map_err(|_| {
@@ -55,6 +57,7 @@ impl<'sc> Literal<'sc> {
                         Rule::u32_integer => int_inner
                             .as_str()
                             .trim()
+                            .replace("_", "")
                             .parse()
                             .map(Literal::U32)
                             .map_err(|_| {
@@ -66,6 +69,7 @@ impl<'sc> Literal<'sc> {
                         Rule::u64_integer => int_inner
                             .as_str()
                             .trim()
+                            .replace("_", "")
                             .parse()
                             .map(Literal::U64)
                             .map_err(|_| {
