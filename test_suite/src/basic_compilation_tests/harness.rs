787c92921fa25f87b3374622c8d647c997038e8e
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-05-13 18:55:49 -0700

    canonicalize file path for test harness

--- test_suite/src/basic_compilation_tests/harness.rs
+++ test_suite/src/basic_compilation_tests/harness.rs
@@ -4,9 +4,10 @@ use forc;
 /// and `false` if it did not.
 pub(crate) fn should_compile(file_name: &str) -> bool {
     println!("Compiling {}", file_name);
+    let manifest_dir = env!("CARGO_MANIFEST_DIR");
     let res = forc::build(Some(format!(
-        "src/basic_compilation_tests/test_programs/{}",
-        file_name
+        "{}/src/basic_compilation_tests/test_programs/{}",
+        manifest_dir, file_name
     )));
     match res {
         Ok(_) => true,
