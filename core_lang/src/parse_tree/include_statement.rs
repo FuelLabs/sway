50123174d2c40a00675c25099a85a1c2a2626954
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-05-30 08:53:18 -0700

    checkpoint while i get breakfast

--- core_lang/src/parse_tree/include_statement.rs
+++ core_lang/src/parse_tree/include_statement.rs
@@ -1,6 +1,7 @@
 use crate::error::*;
 use pest::Span;
 use pest::iterators::Pair;
+use crate::Ident;
 use crate::parser::Rule;
 
 #[derive(Clone, Debug)]
@@ -10,6 +11,16 @@ pub struct IncludeStatement <'sc>{
 
 impl <'sc> IncludeStatement <'sc> {
     pub(crate) fn parse_from_pair(pair: Pair<'sc, Rule>) -> CompileResult<'sc, Self> {
+        let mut warnings = vec![];
+        let mut errors = vec![];
+        let mut iter = pair.into_inner();
+        let _include_keyword = iter.next();
+        let path_to_file_raw = iter.collect::<Vec<_>>();
+        let mut path_to_file: Vec<Ident<'sc>> = Vec::with_capacity(path_to_file_raw.len());
+        for item in path_to_file_raw {
+            let ident = eval!(Ident::parse_from_pair, warnings, errors, item, continue);
+            path_to_file.push(ident);
+        }
         todo!()
     }
 }
--- stdlib/src/main.sw
+++ stdlib/src/main.sw
@@ -1,6 +1,6 @@
 library ops;
 
-include chain;
+include chain/gas;
 
 pub trait Add {
     fn add(self, other: Self) -> Self;
