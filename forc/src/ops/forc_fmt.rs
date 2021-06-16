3c599b062d9ec24cbc642d84307200f91e0c5551
leviathan88 <elvisdedic@outlook.com>
2021-06-16 18:10:54 +0200

    handle --check flag for forc fmt

--- forc/src/ops/forc_fmt.rs
+++ forc/src/ops/forc_fmt.rs
@@ -1,4 +1,7 @@
-use crate::{cli::FormatCommand, utils::constants::SWAY_EXTENSION};
+use crate::{
+    cli::FormatCommand,
+    utils::{constants::SWAY_EXTENSION, helpers::find_manifest_dir},
+};
 use formatter::get_formatted_data;
 use std::{
     ffi::OsStr,
@@ -12,7 +15,47 @@ pub fn format(command: FormatCommand) -> Result<(), FormatError> {
     match find_manifest_dir(&curr_dir) {
         Some(path) => {
             let files = get_sway_files(path)?;
-            format_sway_files(files)
+            let mut errors_exist = false;
+
+            for file in files {
+                if let Ok(file_content) = fs::read_to_string(&file) {
+                    match core_lang::parse(&file_content) {
+                        core_lang::CompileResult::Ok {
+                            value: _,
+                            warnings: _,
+                            errors: _,
+                        } => {
+                            if !command.check {
+                                format_sway_file(&file_content, &file)?;
+                            }
+                        }
+                        core_lang::CompileResult::Err { warnings, errors } => {
+                            errors_exist = true;
+                            if command.check {
+                                eprintln!("{:?}", file);
+
+                                for warning in warnings {
+                                    eprintln!("{}", warning.to_friendly_warning_string());
+                                }
+
+                                for error in errors {
+                                    eprintln!("{}", error.to_friendly_error_string());
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if command.check {
+                if errors_exist {
+                    std::process::exit(1);
+                } else {
+                    std::process::exit(0);
+                }
+            }
+
+            Ok(())
         }
         _ => Err("Manifest file does not exist".into()),
     }
@@ -40,14 +83,10 @@ fn get_sway_files(path: PathBuf) -> Result<Vec<PathBuf>, FormatError> {
     Ok(files)
 }
 
-fn format_sway_files(files: Vec<PathBuf>) -> Result<(), FormatError> {
-    for file in files {
-        if let Ok(file_content) = fs::read_to_string(&file) {
-            // todo: get tab_size from Manifest file
-            let (_, formatted_content) = get_formatted_data(&file_content, 4);
-            fs::write(file, formatted_content)?;
-        }
-    }
+fn format_sway_file(file_content: &str, file: &PathBuf) -> Result<(), FormatError> {
+    // todo: get tab_size from Manifest file
+    let (_, formatted_content) = get_formatted_data(file_content, 4);
+    fs::write(file, formatted_content)?;
 
     Ok(())
 }
@@ -81,20 +120,4 @@ impl From<io::Error> for FormatError {
             message: e.to_string(),
         }
     }
-}
-// Continually go up in the file tree until a manifest (Forc.toml) is found.
-fn find_manifest_dir(starter_path: &PathBuf) -> Option<PathBuf> {
-    let mut path = fs::canonicalize(starter_path.clone()).ok()?;
-    let empty_path = PathBuf::from("/");
-    while path != empty_path {
-        path.push(crate::utils::constants::MANIFEST_FILE_NAME);
-        if path.exists() {
-            path.pop();
-            return Some(path);
-        } else {
-            path.pop();
-            path.pop();
-        }
-    }
-    None
 }
