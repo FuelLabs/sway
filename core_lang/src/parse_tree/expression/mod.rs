c906219a96b15a5c6abbf0d6872fc00c4309525e
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-07-03 09:48:53 -0700

    Fix struct subfield access type checking bug

--- core_lang/src/parse_tree/expression/mod.rs
+++ core_lang/src/parse_tree/expression/mod.rs
@@ -1112,6 +1112,10 @@ fn arrange_by_order_of_operations<'sc>(
                             span: op.span.clone(),
                         },
                         Ident {
+                            primary_name: "std".into(),
+                            span: op.span.clone(),
+                        },
+                        Ident {
                             primary_name: "ops".into(),
                             span: op.span.clone(),
                         },
