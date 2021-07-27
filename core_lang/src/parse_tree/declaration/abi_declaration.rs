435dd2a8a6935292de74dffb4a7d55950b899848
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-07-26 17:00:01 -0700

    implement parsing for abi keyword

--- core_lang/src/parse_tree/declaration/abi_declaration.rs
+++ core_lang/src/parse_tree/declaration/abi_declaration.rs
@@ -13,7 +13,7 @@ pub struct AbiDeclaration<'sc> {
     /// import this type to call it.
     pub(crate) visibility: Visibility,
     /// The name of the abi trait (also known as a "contract trait")
-    pub(crate) trait_name: Ident<'sc>,
+    pub(crate) name: Ident<'sc>,
     /// The methods a contract is required to implement in order opt in to this interface
     pub(crate) interface_surface: Vec<TraitFn<'sc>>,
     /// The methods provided to a contract "for free" upon opting in to this interface
@@ -22,6 +22,55 @@ pub struct AbiDeclaration<'sc> {
 
 impl<'sc> AbiDeclaration<'sc> {
     pub(crate) fn parse_from_pair(pair: Pair<'sc, Rule>) -> CompileResult<'sc, Self> {
-        todo!()
+        let mut iter = pair.into_inner();
+        let mut warnings = Vec::new();
+        let mut errors = Vec::new();
+        let abi_keyword_or_visibility = iter.next().expect("guaranteed by grammar");
+        let (visibility, _abi_keyword) = match abi_keyword_or_visibility.as_rule() {
+            Rule::visibility => (
+                Visibility::parse_from_pair(abi_keyword_or_visibility),
+                iter.next().unwrap(),
+            ),
+            _ => (Visibility::Private, abi_keyword_or_visibility),
+        };
+        let name = eval!(
+            Ident::parse_from_pair,
+            warnings,
+            errors,
+            iter.next().expect("guaranteed by grammar"),
+            return err(warnings, errors)
+        );
+        let mut interface_surface = vec![];
+        let mut methods = vec![];
+        let mut trait_methods = iter.next().expect("guaranteed by grammar");
+        for func in trait_methods.into_inner() {
+            match func.as_rule() {
+                Rule::fn_signature => interface_surface.push(eval!(
+                    TraitFn::parse_from_pair,
+                    warnings,
+                    errors,
+                    func,
+                    continue
+                )),
+                Rule::fn_decl => methods.push(eval!(
+                    FunctionDeclaration::parse_from_pair,
+                    warnings,
+                    errors,
+                    func,
+                    continue
+                )),
+                _ => unreachable!("guaranteed by grammar"),
+            }
+        }
+        ok(
+            AbiDeclaration {
+                methods,
+                interface_surface,
+                name,
+                visibility,
+            },
+            warnings,
+            errors,
+        )
     }
 }
--- test_suite/src/e2e_vm_tests/test_programs/script_7/src/main.sw
+++ test_suite/src/e2e_vm_tests/test_programs/script_7/src/main.sw
@@ -5,6 +5,8 @@ struct InputStruct { field_1: bool, field_2: u64 }
 abi MyContract {
   fn foo(a: u64);
   fn bar(a: InputStruct);
+} {
+  fn baz() -> bool { true } 
 }
 
 
@@ -18,5 +20,5 @@ impl MyContract for Contract {
 }
 
 fn calls_contract() {
-  let x = abi(MyContract, 0x0000_0000_0000_0000);
+  let x = abi(MyContract, Address(0x0000_0000_0000_0000));
 }
