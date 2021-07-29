c75b6786e274c19e3aae461206b21e2d0fca33fa
leviathan88 <elvisdedic@outlook.com>
2021-07-29 16:07:01 +0200

    added higlightjs/sway and its build script

---
+++ scripts/highlightjs/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+project="highlight.js"
+sway="sway.js"
+
+if ! test -d /$project
+then
+    git clone git@github.com:highlightjs/highlight.js.git
+fi
+
+cp $sway $project/src/languages
+cd $project
+npm install
+
+rm -rf build
+node tools/build.js sway
+cp build/highlight.min.js ../../../docs/theme/highlight.js
+
+# add "keep" in order to keep highlight.js repo
+if [[ $1 != "keep" ]]
+then
+    cd ../
+    rm -rf $project
+fi
---
+++ scripts/highlightjs/readme.md
@@ -0,0 +1,15 @@
+## Instructions
+
+Update "sway.js" file.
+
+Then run:
+
+```cli
+$ bash build.sh
+```
+
+If you are actively developing and testing and want to keep `highlight.js` directory add "keep" in the command
+
+```cli
+$ bash build.sh keep
+```
