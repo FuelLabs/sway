a157f78c4acda95b47aaf83cc2d13a58b3377541
leviathan88 <elvisdedic@outlook.com>
2021-08-02 13:16:23 +0200

    update build scripts

--- scripts/highlightjs/build.sh
+++ scripts/highlightjs/build.sh
@@ -2,13 +2,12 @@
 project="highlight.js"
 sway="sway.js"
 
-if ! test -d /$project
-then
+if ! test -d /${project}; then
     git clone git@github.com:highlightjs/highlight.js.git
 fi
 
-cp $sway $project/src/languages
-cd $project
+cp ${sway} ${project}/src/languages
+cd ${project}
 npm install
 
 rm -rf build
@@ -16,8 +15,7 @@ node tools/build.js sway
 cp build/highlight.min.js ../../../docs/theme/highlight.js
 
 # add "keep" in order to keep highlight.js repo
-if [[ $1 != "keep" ]]
-then
+if [[ ${1} != "keep" ]]; then
     cd ../
-    rm -rf $project
+    rm -rf ${project}
 fi
--- scripts/prism/build.sh
+++ scripts/prism/build.sh
@@ -3,22 +3,20 @@ project="prism"
 components="components.json"
 sway="prism-sway.js"
 
-if ! test -d /$project
-then
+if ! test -d /${project}; then
     git clone git@github.com:PrismJS/prism.git
 fi
 
-cp $sway $project/components
-cp $components $project
-cd $project
+cp ${sway} ${project}/components
+cp ${components} ${project}
+cd ${project}
 npm ci
 
 npm run build
 cp components/prism-sway.min.js ../prism-sway.min.js
 
 # add "keep" in order to keep highlight.js repo
-if [[ $1 != "keep" ]]
-then
+if [[ ${1} != "keep" ]]; then
     cd ../
-    rm -rf $project
+    rm -rf ${project}
 fi
