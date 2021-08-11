59842b10d5ba023111068dad61c592f0a320f737
Brandon Kite <brandonkite92@gmail.com>
2021-08-11 10:53:25 -0700

    update default node url
    use localhost 127.0.0.1 instead of 0.0.0.0

--- forc/src/utils/constants.rs
+++ forc/src/utils/constants.rs
@@ -6,4 +6,4 @@ pub const SWAY_PREDICATE: &'static str = "predicate";
 pub const SWAY_LIBRARY: &'static str = "library";
 pub const SWAY_SCRIPT: &'static str = "script";
 pub const SWAY_CONTRACT: &'static str = "contract";
-pub const DEFAULT_NODE_URL: &'static str = "0.0.0.0:4000";
+pub const DEFAULT_NODE_URL: &'static str = "127.0.0.1:4000";
