733f62bda317754f203250f9d36ae343ec2a5d70
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-08-19 20:26:09 -0700

    Seed storage with an initial contract for testing

--- test_suite/src/e2e_vm_tests/harness.rs
+++ test_suite/src/e2e_vm_tests/harness.rs
@@ -3,25 +3,27 @@ use forc::cli::BuildCommand;
 
 use fuel_tx::{ContractId, Input, Output, Transaction};
 use fuel_vm::interpreter::Interpreter;
-use fuel_vm::prelude::MemoryStorage;
+use fuel_vm::prelude::{Contract, MemoryStorage, Storage};
 
 /// Very basic check that code does indeed run in the VM.
 /// `true` if it does, `false` if not.
 pub(crate) fn runs_in_vm(file_name: &str) {
+    let contract_id = ContractId::from([
+        17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
+        17, 17, 17, 17, 17, 17, 17, 17, 17,
+    ]);
     let input_contract = Input::Contract {
         utxo_id: Default::default(),
         balance_root: Default::default(),
         state_root: Default::default(),
-        contract_id: ContractId::from([
-            17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
-            17, 17, 17, 17, 17, 17, 17, 17, 17, 17,
-        ]),
+        contract_id: contract_id.clone(),
     };
     let output_contract = Output::Contract {
         input_index: 0,
         balance_root: Default::default(),
         state_root: Default::default(),
     };
+
     let script = compile_to_bytes(file_name);
     let gas_price = 10;
     let gas_limit = 10000;
@@ -42,7 +44,9 @@ pub(crate) fn runs_in_vm(file_name: &str) {
     );
     let block_height = (u32::MAX >> 1) as u64;
     tx.validate(block_height).unwrap();
-    let storage = MemoryStorage::default();
+    let mut storage = MemoryStorage::default();
+    Storage::<_, Contract>::insert(&mut storage, &contract_id, &Default::default());
+    //    storage.insert::<_, Contract>(&contract_id, Default::default());
     let mut interpreter = Interpreter::with_storage(storage);
     interpreter.transact(tx).unwrap();
 }
