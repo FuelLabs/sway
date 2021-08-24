41857d494a340aa75796b9ad43533929cfbe85e0
Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>
2021-08-24 07:13:37 -0700

    Revert "temporarily comment out unnecessary test cases for debugging tomorrow"
    This reverts commit 26304c601fa8a5ffece0458b8cd3536c926d3e4f.

--- test_suite/src/e2e_vm_tests/harness.rs
+++ test_suite/src/e2e_vm_tests/harness.rs
@@ -9,6 +9,42 @@ use rand::{Rng, SeedableRng};
 /// Very basic check that code does indeed run in the VM.
 /// `true` if it does, `false` if not.
 pub(crate) fn runs_in_vm(file_name: &str) {
+    let contract_id = ContractId::from([
+        120, 17, 104, 24, 155, 8, 101, 204, 229, 87, 229, 245, 58, 249, 53, 114, 56, 181, 93, 190,
+        208, 220, 207, 192, 22, 164, 101, 5, 41, 106, 65, 161,
+    ]);
+    let input_contract = Input::Contract {
+        utxo_id: Default::default(),
+        balance_root: Default::default(),
+        state_root: Default::default(),
+        contract_id: contract_id.clone(),
+    };
+    let output_contract = Output::Contract {
+        input_index: 0,
+        balance_root: Default::default(),
+        state_root: Default::default(),
+    };
+
+    let script = compile_to_bytes(file_name);
+    let gas_price = 10;
+    let gas_limit = 10000;
+    let maturity = 0;
+    let script_data = vec![];
+    let inputs = vec![input_contract];
+    let outputs = vec![output_contract];
+    let witness = vec![];
+    let tx_to_test = Transaction::script(
+        gas_price,
+        gas_limit,
+        maturity,
+        script,
+        script_data,
+        inputs,
+        outputs,
+        witness,
+    );
+    let block_height = (u32::MAX >> 1) as u64;
+    tx_to_test.validate(block_height).unwrap();
     let mut storage = MemoryStorage::default();
     let program = vec![Opcode::NOOP, Opcode::RET(16)];
 
@@ -24,9 +60,6 @@ pub(crate) fn runs_in_vm(file_name: &str) {
 
     let output = Output::contract_created(contract_id);
 
-    let gas_price = 10;
-    let gas_limit = 10000;
-    let maturity = 0;
     let bytecode_witness = 0;
     let tx = Transaction::create(
         gas_price,
@@ -40,52 +73,9 @@ pub(crate) fn runs_in_vm(file_name: &str) {
         vec![program],
     );
 
-    // Deploy the contract
+    // Deploy the contract into the blockchain
     Interpreter::transition(&mut storage, tx).expect("Failed to transact");
     let mut interpreter = Interpreter::with_storage(storage);
-
-    // construct the test case transaction
-    // this is the ID that the above transition deployed to
-    // in hex, this is 0x781168189b0865cce557e5f53af9357238b55dbed0dccfc016a46505296a41a1
-    let contract_id = ContractId::from([
-        120, 17, 104, 24, 155, 8, 101, 204, 229, 87, 229, 245, 58, 249, 53, 114, 56, 181, 93, 190,
-        208, 220, 207, 192, 22, 164, 101, 5, 41, 106, 65, 161,
-    ]);
-    // mock input
-    let input_contract = Input::Contract {
-        utxo_id: Default::default(),
-        balance_root: Default::default(),
-        state_root: Default::default(),
-        contract_id: contract_id.clone(),
-    };
-
-    // mock output
-    let output_contract = Output::Contract {
-        input_index: 0,
-        balance_root: Default::default(),
-        state_root: Default::default(),
-    };
-
-    // grab the test case and compile it to bytecode
-    let script = compile_to_bytes(file_name);
-    let script_data = vec![];
-    let inputs = vec![input_contract];
-    let outputs = vec![output_contract];
-    let witness = vec![];
-    let tx_to_test = Transaction::script(
-        gas_price,
-        gas_limit,
-        maturity,
-        script,
-        script_data,
-        inputs,
-        outputs,
-        witness,
-    );
-
-    let block_height = (u32::MAX >> 1) as u64;
-    tx_to_test.validate(block_height).unwrap();
-
     // evaluate the test case
     interpreter.transact(tx_to_test).unwrap();
 }
