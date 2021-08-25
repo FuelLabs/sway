c6294c76d5b1dc15a18676fcdeeec29af0934c58
Alexander Hansen <alexanderhansen@Alexanders-MBP.localdomain>
2021-08-25 10:13:21 -0700

    Rename test cases to be more descriptive.

--- test_suite/src/e2e_vm_tests/mod.rs
+++ test_suite/src/e2e_vm_tests/mod.rs
@@ -2,7 +2,13 @@ mod harness;
 
 pub fn run() {
     let project_names = vec![
-        "script_1", "script_2", "script_3", "script_4", "script_5", "script_6", "script_7",
+        "asm_expr_basic",
+        "basic_func_decl",
+        "contract_abi_impl",
+        "dependencies",
+        "if_elseif_enum",
+        "out_of_order_decl",
+        "struct_field_reassignment",
     ];
     project_names.into_iter().for_each(|name| {
         crate::e2e_vm_tests::harness::runs_in_vm(name);
