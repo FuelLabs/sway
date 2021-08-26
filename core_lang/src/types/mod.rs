ccb856f0e5358707bbf6adebc23ace927e22d29b
Emily Herbert <herbert.e@northeastern.edu>
2021-08-26 13:30:26 -0500

    Break out MaybeResolvedType.

--- core_lang/src/types/mod.rs
+++ core_lang/src/types/mod.rs
@@ -1,4 +1,6 @@
 mod resolved_type;
+mod maybe_resolved_type;
 mod type_info;
-pub use resolved_type::{MaybeResolvedType, PartiallyResolvedType, ResolvedType};
+pub use resolved_type::{PartiallyResolvedType, ResolvedType};
 pub use type_info::{IntegerBits, TypeInfo};
+pub use maybe_resolved_type::MaybeResolvedType;
