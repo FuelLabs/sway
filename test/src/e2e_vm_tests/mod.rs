7b23a358a798258d84e21e6d1102318924325a71
Toby Hutton <toby@grusly.com>
2021-09-07 18:18:30 +1000

    Make sure if main() returns Unit that it does return a 0.

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -13,6 +13,7 @@ pub fn run() {
         ("contract_call", ProgramState::Return(0)),
         ("enum_in_fn_decl", ProgramState::Return(255)),
         ("empty_impl", ProgramState::Return(0)),
+        ("main_returns_unit", ProgramState::Return(0)),
     ];
     project_names.into_iter().for_each(|(name, res)| {
         assert_eq!(crate::e2e_vm_tests::harness::runs_in_vm(name), res);
