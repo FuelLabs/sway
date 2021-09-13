b05c8861a66987194b44a8683efc0a9977ed90d6
emilyaherbert <emily.herbert@fuel.sh>
2021-09-13 12:58:25 -0500

    Cargo fmt.

--- core_lang/src/semantic_analysis/node_dependencies.rs
+++ core_lang/src/semantic_analysis/node_dependencies.rs
@@ -2,8 +2,8 @@ use std::collections::{HashMap, HashSet};
 use std::iter::FromIterator;
 
 use crate::{
-    error::*, parse_tree::*, types::IntegerBits, AstNode, AstNodeContent, CodeBlock, Declaration,
-    Expression, ReturnStatement, TypeInfo, WhileLoop, span::Span
+    error::*, parse_tree::*, span::Span, types::IntegerBits, AstNode, AstNodeContent, CodeBlock,
+    Declaration, Expression, ReturnStatement, TypeInfo, WhileLoop,
 };
 
 // -------------------------------------------------------------------------------------------------
