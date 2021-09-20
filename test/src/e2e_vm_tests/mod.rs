94e24e6211c29f35f05ac2db280c4cba020fbfef
Toby Hutton <toby@grusly.com>
2021-09-20 18:14:22 +1000

    Add tests for the `b256` stdlib functions.

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -44,6 +44,7 @@ pub fn run(filter_regex: Option<regex::Regex>) {
         ("asm_without_return", ProgramState::Return(0)),
         ("op_precedence", ProgramState::Return(0)), // 1 == false
         ("b256_bad_jumps", ProgramState::Return(1)),
+        ("b256_ops", ProgramState::Return(100)),
     ];
     project_names.into_iter().for_each(|(name, res)| {
         if filter(name) {
