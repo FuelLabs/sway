d309e0eb970fbb8cf23a6e1a1c534ad90bebbf96
Toby Hutton <toby@grusly.com>
2021-09-21 15:25:33 +1000

    Allow an argument to the test suite for filtering cases to run.
    The optional argument may be a regex which is checked against the name
    of the test.
    
    E.g., `cargo run ^contract`
    
    ... will run only the `contract_abi_impl` and `contract_call` tests (as
    of the time of this commit).

--- test/src/main.rs
+++ test/src/main.rs
@@ -1,5 +1,9 @@
 mod e2e_vm_tests;
 
 fn main() {
-    e2e_vm_tests::run();
+    let filter_regex = std::env::args().nth(1).map(|filter_str| {
+        regex::Regex::new(&filter_str).expect(&format!("Invalid filter regex: '{}'.", filter_str))
+    });
+
+    e2e_vm_tests::run(filter_regex);
 }
