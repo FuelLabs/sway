8d6808830faac1ab8a02b7b48bf2e5c909481701
Alex Hansen <alex@alex-hansen.com>
2021-09-20 18:41:10 -0700

    Allow asm expressions without returns (#253)
    Co-authored-by: Alexander Hansen <alexanderhansen@Alexanders-MacBook-Pro.local>

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -18,7 +18,8 @@ pub fn run() {
         ("main_returns_unit", ProgramState::Return(0)),
         ("unary_not_basic", ProgramState::Return(1)), // 1 == true
         ("unary_not_basic_2", ProgramState::Return(1)), // 1 == true
-        ("op_precedence", ProgramState::Return(0)),   // 1 == false
+        ("asm_without_return", ProgramState::Return(0)),
+        ("op_precedence", ProgramState::Return(0)), // 1 == false
     ];
     project_names.into_iter().for_each(|(name, res)| {
         assert_eq!(crate::e2e_vm_tests::harness::runs_in_vm(name), res);
@@ -27,6 +28,8 @@ pub fn run() {
     // source code that should _not_ compile
     let project_names = vec![
         "recursive_calls",
+        "asm_missing_return",
+        "asm_should_not_have_return",
         "missing_fn_arguments",
         "excess_fn_arguments",
     ];
