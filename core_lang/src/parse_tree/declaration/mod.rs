1a273e2c1cc6619da35d767489d034afbd45c9cc
Toby Hutton <toby@grusly.com>
2021-09-29 17:43:17 +1000

    Move variable declaration parsing to VariableDeclaration::parse_from_pair().

--- core_lang/src/parse_tree/declaration/mod.rs
+++ core_lang/src/parse_tree/declaration/mod.rs
@@ -20,10 +20,7 @@ pub use variable_declaration::*;
 
 use crate::build_config::BuildConfig;
 use crate::error::*;
-use crate::parse_tree::Expression;
 use crate::parser::Rule;
-use crate::types::TypeInfo;
-use crate::Ident;
 use pest::iterators::Pair;
 use std::collections::HashMap;
 
@@ -57,53 +54,12 @@ impl<'sc> Declaration<'sc> {
                 warnings,
                 errors
             ),
-            Rule::var_decl => {
-                let mut var_decl_parts = decl_inner.into_inner();
-                let _let_keyword = var_decl_parts.next();
-                let maybe_mut_keyword = var_decl_parts.next().unwrap();
-                let is_mutable = maybe_mut_keyword.as_rule() == Rule::mut_keyword;
-                let name_pair = if is_mutable {
-                    var_decl_parts.next().unwrap()
-                } else {
-                    maybe_mut_keyword
-                };
-                let mut maybe_body = var_decl_parts.next().unwrap();
-                let type_ascription = match maybe_body.as_rule() {
-                    Rule::type_ascription => {
-                        let type_asc = maybe_body.clone();
-                        maybe_body = var_decl_parts.next().unwrap();
-                        Some(type_asc)
-                    }
-                    _ => None,
-                };
-                let type_ascription = if let Some(ascription) = type_ascription {
-                    Some(check!(
-                        TypeInfo::parse_from_pair(ascription, config.clone()),
-                        TypeInfo::Unit,
-                        warnings,
-                        errors
-                    ))
-                } else {
-                    None
-                };
-                let body = check!(
-                    Expression::parse_from_pair(maybe_body, config.clone(), docstrings),
-                    return err(warnings, errors),
-                    warnings,
-                    errors
-                );
-                Declaration::VariableDeclaration(VariableDeclaration {
-                    name: check!(
-                        Ident::parse_from_pair(name_pair, config.clone()),
-                        return err(warnings, errors),
-                        warnings,
-                        errors
-                    ),
-                    body,
-                    is_mutable,
-                    type_ascription,
-                })
-            }
+            Rule::var_decl => Declaration::VariableDeclaration(check!(
+                VariableDeclaration::parse_from_pair(decl_inner, config, docstrings),
+                return err(warnings, errors),
+                warnings,
+                errors
+            )),
             Rule::reassignment => Declaration::Reassignment(check!(
                 Reassignment::parse_from_pair(decl_inner, config, docstrings),
                 return err(warnings, errors),
