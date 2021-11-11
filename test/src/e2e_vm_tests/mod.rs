7bc7decc74f2257bf50bf15d8e8a6e6cf56f62c8
Alex Hansen <alex@alex-hansen.com>
2021-11-11 11:30:20 -0800

    Revert #296 (#381)
    * Revert #296
    
    * fmt

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -74,8 +74,6 @@ pub fn run(filter_regex: Option<regex::Regex>) {
         "infinite_dependencies",
         "top_level_vars",
         "dependencies_parsing_error",
-        "mut_error_message",
-        "reassignment_to_non_variable_message",
         "disallowed_gm",
         "unify_identical_unknowns",
     ];
