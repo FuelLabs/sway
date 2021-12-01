79bbcc8521124a1d42b8b04ea8ee2ee25b37be65
Nick Furfaro <nfurfaro33@gmail.com>
2021-12-01 11:13:47 -0700

    Add basic keygen, signing and address derivation

--- signatures/src/main.rs
+++ signatures/src/main.rs
@@ -1,73 +1,48 @@
 // use the anyhow crate for easy idiomatic error handling
 use anyhow::Result;
-// use the ethers_core rand for rng
-use ethers_core::rand::thread_rng;
-// use the ethers_signers crate to manage LocalWallet and Signer
-use ethers_signers::{LocalWallet, Signer};
-// use elliptic_curve::*;
-use hex_literal::hex;
-use libsecp256k1::{PublicKey, SecretKey, Signature};
-use sha2::*;
+use libsecp256k1::*;
+use secp256k1_test::{Message as SecpMessage, Secp256k1};
+use sha256::digest_bytes;
 
 // Use the `tokio::main` macro for using async on the main function
 #[tokio::main]
 async fn main() -> Result<()> {
-    let seed = [
-        0x42, 0xfa, 0xf7, 0xc1, 0x63, 0x0a, 0x38, 0x26, 0xc2, 0xa1, 0x41, 0xee, 0x18, 0x72, 0xb7,
-        0x6d, 0xc4, 0x7c, 0xc2, 0x7d, 0xf5, 0x8b, 0x9a, 0x8f, 0xbd, 0x8a, 0xe0, 0xeb, 0x1e, 0x5a,
-        0x65, 0xc8,
-    ];
+    let secp256k1 = Secp256k1::new();
 
-    let seckey: SecretKey = SecretKey::parse(&seed).unwrap();
-    let pubkey: PublicKey = PublicKey::from_secret_key(&seckey);
-    println!("The public Key is: {:?}", pubkey);
-    // The concated public Key is: 1847637560306280107622622502715552825033773557495040513239277815084649981513331589016081394344042535895449449754985848875729991940298763648723723322522
-    //its hex is 0x907F3D3FD34158C4A4348823692272EA4211FD1543D2412E60F9CA87C92283C7802B042FCE39869C4C652024F8F635F3EA885858116133D114DD1D39D849A
+    let message_arr = [42u8; 32];
+    let secret_key = secp256k1_test::key::ONE_KEY; // the number 1 as a secret key
+    let public_key = secp256k1_test::key::PublicKey::from_secret_key(&secp256k1, &secret_key);
+    let message = SecpMessage::from_slice(&message_arr).unwrap();
+    let signature = secp256k1.sign_recoverable(&message, &secret_key);
 
-    // let mut key_hasher = Sha256::new();
-    // key_hasher.update(&pubkey);
+    println!("privkey: {}", secret_key);
+    println!("pubkey: {}", public_key);
+    println!("message: {:?}", message);
+    println!("Signature: {:?}", signature);
 
-    // create a Sha256 object
-    let mut hasher = Sha256::new();
+    let pubkey_a = public_key.serialize_uncompressed();
+    let address = digest_bytes(&pubkey_a);
 
-    // write input message
-    hasher.update(b"Hello from Fuel! The number is 42.");
+    assert_eq!(pubkey_a.len(), 65);
+    println!("Address: {:?}", address);
+    // Derived Address: "50929b74c1a04954b78b4b6035e97a5e078a5a0f28ec96d547bfee9ace803ac0"
 
-    // read hash digest and consume hasher
-    let result = hasher.finalize();
+    let ctx_message = Message::parse(&message_arr);
+    let (rec_id, signature_a) = signature.serialize_compact();
+    assert_eq!(signature_a.len(), 64);
 
-    assert_eq!(
-        result[..],
-        hex!(
-            "
-    ded50450211738811b352a1c4534bd38c9d285063c8aa45517fec882ac91294d
-    "
-        )[..]
-    );
+    let ctx_sig = Signature::parse_standard(&signature_a).expect("signature is valid");
 
-    // @todo find Sha256 function. For now calculate this with online tool: https://emn178.github.io/online-tools/sha256.html
-    // let address = Sha256(pubkey)
-    // let address = 0x7fbf2c5091da720d117dae2ea8e47d6e7b8b96dbe96dfbe7ae6f24c0ee5e132d;
+    // let ctx_pubkey = recover(
+    //     &ctx_message,
+    //     &ctx_sig,
+    //     &RecoveryId::parse(rec_id.to_i32() as u8).unwrap(),
+    // )
+    // .unwrap();
+    // let sp = ctx_pubkey.serialize();
 
-    // Generate a random wallet
-    let wallet = LocalWallet::new(&mut thread_rng());
-    println!("Wallet addr:    {:?}", wallet.address());
-
-    // Declare the message you want to sign.
-    let message = "Hello from Fuel! The number is 42.";
-
-    // sign message from your wallet and print out signature produced.
-    let signature = wallet.sign_message(message).await?;
-    println!("Signature       {}", signature);
-
-    // verify the signature produced from your wallet.
-    signature.verify(message, wallet.address()).unwrap();
-
-    let recovered_addr = signature.recover(message).unwrap();
-
-    assert_eq!(recovered_addr, wallet.address());
-    println!("Signed by:      {:?}", wallet.address());
-    println!("Recovered addr: {:?}", recovered_addr);
-
+    // let sps: &[u8] = &sp;
+    // let gps: &[u8] = &pubkey_a;
+    // assert_eq!(sps, gps);
     Ok(())
 }
