9a5ebf228b5be10131f68091ec81d6f412950c50
Rodrigo Araújo <rod.dearaujo@gmail.com>
2021-12-06 07:37:19 -0800

    Add forc test functionality and forc README (#433)
    * Add forc test functionality and forc README
    
    * Update mdbook with content on testing
    
    * Small nits
    
    * Make Cargo.toml and Forc.toml siblings

--- forc/src/utils/defaults.rs
+++ forc/src/utils/defaults.rs
@@ -14,11 +14,54 @@ entry = "main.sw"
     )
 }
 
+/// Creates a default Cargo manifest for the Rust-based tests.
+/// It includes necessary packages to make the Rust-based
+/// tests work, such as the abigen macro, fuels-rs, and
+/// the fuel client.
+pub(crate) fn default_tests_manifest(project_name: &str) -> String {
+    let real_name = whoami::realname();
+
+    format!(
+        r#"[package]
+author  = "{}"
+license = "MIT"
+name = "{}"
+edition = "2021"
+version = "0.1.0"
+
+[dependencies]
+tokio = {{ version = "1.12", features = ["rt", "macros"] }}
+fuels-abigen-macro = {{ git = "ssh://git@github.com/FuelLabs/fuels-rs.git" }}
+fuels-rs = {{ git = "ssh://git@github.com/FuelLabs/fuels-rs.git" }}
+fuel-client = {{ git = "ssh://git@github.com/FuelLabs/fuel-core", default-features = false }}
+fuel-tx = {{ git = "ssh://git@github.com/FuelLabs/fuel-tx.git" }}
+rand = "0.8"
+
+[[test]]
+name = "integration_tests"
+path = "tests/harness.rs"
+harness = true
+"#,
+        real_name, project_name,
+    )
+}
+
 pub(crate) fn default_program() -> String {
     r#"script;
 
 fn main() {
     
+}
+"#
+    .into()
+}
+
+pub(crate) fn default_test_program() -> String {
+    r#"
+
+#[tokio::test]
+async fn harness() {
+    assert_eq!(true, true);
 }
 "#
     .into()
