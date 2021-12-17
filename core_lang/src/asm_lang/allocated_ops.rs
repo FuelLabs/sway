1609921594cb84fcc219932bd2027400d0a8102f
Nick Furfaro <nfurfaro33@gmail.com>
2021-12-17 11:19:05 -0700

    Fixup

--- core_lang/src/asm_lang/allocated_ops.rs
+++ core_lang/src/asm_lang/allocated_ops.rs
@@ -244,7 +244,7 @@ impl<'sc> fmt::Display for AllocatedOp<'sc> {
             ECR(a, b, c)    => format!("ecr  {} {} {}", a, b, c),
             K256(a, b, c)   => format!("k256 {} {} {}", a, b, c),
             S256(a, b, c)   => format!("s256 {} {} {}", a, b, c),
-            XOS(a, b)   => format!("xos {} {}", a, b),
+            XOS(a, b)       => format!("xos {} {}", a, b),
             NOOP            => "noop".to_string(),
             FLAG(a)         => format!("flag {}", a),
             GM(a, b)         => format!("gm {} {}", a, b),
