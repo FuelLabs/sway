cf89c08067173367982a509a06001805e1e35a41
Nick Furfaro <nfurfaro33@gmail.com>
2021-12-17 12:44:30 -0700

    Adding the xos opcode. (#468)
    * Add XOS opcode to allocated_ops.rs
    
    * Add XOS opcode to mod.rs
    
    * Add XOS opcode to virtual_ops.rs
    
    * Cargo fmt
    
    * Add forc toml file
    
    * Add test
    
    * Add test to mod.rs
    
    * Cargo fmt
    
    * Remove test from mod.rs and add comments
    
    * Apply formatting
    
    * Fixup
    
    * Fixup
    
    * Fixup
    
    * Fixup

--- core_lang/src/asm_lang/virtual_ops.rs
+++ core_lang/src/asm_lang/virtual_ops.rs
@@ -125,6 +125,7 @@ pub(crate) enum VirtualOp {
     ECR(VirtualRegister, VirtualRegister, VirtualRegister),
     K256(VirtualRegister, VirtualRegister, VirtualRegister),
     S256(VirtualRegister, VirtualRegister, VirtualRegister),
+    XOS(VirtualRegister, VirtualRegister),
     NOOP,
     FLAG(VirtualRegister),
     GM(VirtualRegister, VirtualImmediate18),
@@ -208,6 +209,7 @@ impl VirtualOp {
             ECR(r1, r2, r3) => vec![r1, r2, r3],
             K256(r1, r2, r3) => vec![r1, r2, r3],
             S256(r1, r2, r3) => vec![r1, r2, r3],
+            XOS(r1, r2) => vec![r1, r2],
             NOOP => vec![],
             FLAG(r1) => vec![r1],
             GM(r1, _imm) => vec![r1],
@@ -545,6 +547,9 @@ impl VirtualOp {
                 map_reg(&mapping, reg2),
                 map_reg(&mapping, reg3),
             ),
+            XOS(reg1, reg2) => {
+                AllocatedOpcode::XOS(map_reg(&mapping, reg1), map_reg(&mapping, reg2))
+            }
             NOOP => AllocatedOpcode::NOOP,
             FLAG(reg) => AllocatedOpcode::FLAG(map_reg(&mapping, reg)),
             GM(reg, imm) => AllocatedOpcode::GM(map_reg(&mapping, reg), imm.clone()),
