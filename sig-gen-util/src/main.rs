acc6c438a67c42cc913744523d2b4f3544434e13
Nick Furfaro <nfurfaro33@gmail.com>
2022-01-11 13:02:13 -0700

    Apply formatting

--- sig-gen-util/src/main.rs
+++ sig-gen-util/src/main.rs
@@ -6,22 +6,23 @@ use fuel_tx::crypto::Hasher;
 use fuel_vm::crypto;
 use fuel_vm::prelude::*;
 
-use std::str::FromStr;
-use secp256k1::{PublicKey, Secp256k1, SecretKey};
 use anyhow::Result;
+use secp256k1::{PublicKey, Secp256k1, SecretKey};
+use std::str::FromStr;
 
 fn main() -> Result<()> {
-
     let secp = Secp256k1::new();
-    let secret = SecretKey::from_str("3b940b5586823dfd02ae3b461bb4336b5ecbaefd6627aa922efc048fec0c881c").unwrap();
+    let secret =
+        SecretKey::from_str("3b940b5586823dfd02ae3b461bb4336b5ecbaefd6627aa922efc048fec0c881c")
+            .unwrap();
     let public = PublicKey::from_secret_key(&secp, &secret).serialize_uncompressed();
     let public = Bytes64::try_from(&public[1..]).expect("Failed to parse public key!");
     let address = Hasher::hash(&public[..]);
 
     let message = b"The gift of words is the gift of deception and illusion.";
     let e = Hasher::hash(&message[..]);
-    let sig =
-        crypto::secp256k1_sign_compact_recoverable(secret.as_ref(), e.as_ref()).expect("Failed to generate signature");
+    let sig = crypto::secp256k1_sign_compact_recoverable(secret.as_ref(), e.as_ref())
+        .expect("Failed to generate signature");
 
     println!("Secret Key: {:?}", secret);
     println!("Public Key: {:?}", public);
