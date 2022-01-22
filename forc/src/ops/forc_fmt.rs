ad4c67f222690344254332db94c583391722cde1
vnepveu <nepveu.victor@hotmail.fr>
2022-01-22 18:57:11 +0100

    fix: format Forc.toml manifests with `forc fmt`
    - Fixes an incorrect if/else branching

--- forc/src/ops/forc_fmt.rs
+++ forc/src/ops/forc_fmt.rs
@@ -66,7 +66,6 @@ fn format_after_build(command: FormatCommand) -> Result<(), FormatError> {
                     }
                 }
             }
-
             // format manifest using taplo formatter
             if let Ok(file_content) = fs::read_to_string(&manifest_file) {
                 let taplo_alphabetize = taplo_fmt::Options {
@@ -74,16 +73,16 @@ fn format_after_build(command: FormatCommand) -> Result<(), FormatError> {
                     ..Default::default()
                 };
                 let formatted_content = taplo_fmt::format(&file_content, taplo_alphabetize);
-                if command.check {
-                    if formatted_content != file_content {
-                        if !contains_edits {
-                            contains_edits = true;
-                        }
-                        eprintln!("\nManifest Forc.toml improperly formatted");
-                        display_file_diff(&file_content, &formatted_content)?;
-                    } else {
-                        format_file(&manifest_file, &formatted_content)?;
+                if !command.check {
+                    format_file(&manifest_file, &formatted_content)?;
+                } else if formatted_content != file_content {
+                    if !contains_edits {
+                        contains_edits = true;
                     }
+                    eprintln!("\nManifest Forc.toml improperly formatted");
+                    display_file_diff(&file_content, &formatted_content)?;
+                } else {
+                    eprintln!("\nManifest Forc.toml properly formatted")
                 }
             }
 
