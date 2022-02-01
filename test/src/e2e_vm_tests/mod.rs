a9e13500c62e5b68dc9e46c45154e0b9e8a8fb8f
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2022-01-31 16:57:33 -0800

    forgot to enable the test

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -122,6 +122,7 @@ pub fn run(filter_regex: Option<regex::Regex>) {
         "script_calls_impure",
         "contract_pure_calls_impure",
         "literal_too_large_for_type",
+        "star_import_alias",
     ];
     number_of_tests_run += negative_project_names.iter().fold(0, |acc, name| {
         if filter(name) {
