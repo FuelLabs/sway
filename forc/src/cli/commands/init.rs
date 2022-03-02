43c026ac1e53b713ef5695581f6ca98da912cfa1
JoshuaBatty <joshpbatty@gmail.com>
2022-03-02 15:21:37 +1100

    clean up help formatting

--- forc/src/cli/commands/init.rs
+++ forc/src/cli/commands/init.rs
@@ -4,11 +4,13 @@ use clap::Parser;
 
 static TEMPLATE_HELP: &str = r#"Initialize a new project from a template.
 
-Option 1: Create a new project using supported examples from FuelLabs
-example templates: 
+Option 1: 
+Create a new project using supported examples from FuelLabs.
+Example Templates: 
  - counter
 
-Option 2: Create a new project from a GitHub URL containing a sway project."#;
+Option 2: 
+Create a new project from a GitHub URL containing a sway project."#;
 
 /// Create a new Forc project.
 #[derive(Debug, Parser)]
