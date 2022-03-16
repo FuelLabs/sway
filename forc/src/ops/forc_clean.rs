6c2f190a18f4a972e260514861516a7c31474f83
Chris O'Brien <obrchr96@gmail.com>
2022-03-16 17:57:21 -0500

    rmv unused import

--- forc/src/ops/forc_clean.rs
+++ forc/src/ops/forc_clean.rs
@@ -1,6 +1,6 @@
 use crate::cli::CleanCommand;
 use anyhow::{anyhow, bail, Result};
-use forc_util::{default_output_directory, println_yellow_err};
+use forc_util::default_output_directory;
 use std::{path::PathBuf, process};
 use sway_utils::{find_cargo_manifest_dir, find_manifest_dir, MANIFEST_FILE_NAME};
 
