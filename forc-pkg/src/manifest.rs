cb78eb2486583aaadbb85a7354707c02bd4e07e8
John Adler <adlerjohn@users.noreply.github.com>
2022-03-24 17:54:52 -0400

    Remove author manifest field. (#1021)
    * Remove author manifest field.
    
    * Fix cap

--- forc-pkg/src/manifest.rs
+++ forc-pkg/src/manifest.rs
@@ -19,8 +19,6 @@ pub struct Manifest {
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(rename_all = "kebab-case")]
 pub struct Project {
-    #[deprecated = "use the authors field instead, the author field will be removed soon."]
-    pub author: Option<String>,
     pub authors: Option<Vec<String>>,
     pub name: String,
     pub organization: Option<String>,
