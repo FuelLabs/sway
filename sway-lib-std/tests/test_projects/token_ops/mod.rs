7e1835eab0700f5901d5c1c511962d4378b161d5
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-29 12:42:20 -0600

    test: finish test for force_transfer

--- sway-lib-std/tests/test_projects/token_ops/mod.rs
+++ sway-lib-std/tests/test_projects/token_ops/mod.rs
@@ -1,7 +1,9 @@
 use fuel_tx::{ContractId, Salt};
 use fuels_abigen_macro::abigen;
 use fuels_contract::{contract::Contract, parameters::TxParameters};
-use fuels_signers::util::test_helpers;
+use fuels_signers::provider::Provider;
+use fuels_signers::util::test_helpers::setup_test_provider_and_wallet;
+use fuels_signers::wallet::Wallet;
 
 abigen!(
     TestFuelCoinContract,
@@ -10,10 +12,13 @@ abigen!(
 
 #[tokio::test]
 async fn can_mint() {
-    let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance().await;
+    let (provider, wallet) = setup_test_provider_and_wallet().await;
+    let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance(provider, wallet).await;
 
-    let target = testfuelcoincontract_mod::ContractId { value: fuelcoin_id.into() };
-    let asset_id = target;
+    let target = testfuelcoincontract_mod::ContractId {
+        value: fuelcoin_id.into(),
+    };
+    let asset_id = target.clone();
 
     let mut balance_result = fuelcoin_instance
         .get_balance(target.clone(), asset_id.clone())
@@ -24,17 +29,23 @@ async fn can_mint() {
 
     fuelcoin_instance.mint_coins(11).call().await.unwrap();
 
-    balance_result = fuelcoin_instance.get_balance(target, asset_id).call().await.unwrap();
+    balance_result = fuelcoin_instance
+        .get_balance(target, asset_id)
+        .call()
+        .await
+        .unwrap();
     assert_eq!(balance_result.value, 11);
 }
 
 #[tokio::test]
 async fn can_burn() {
-    let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance().await;
+    let (provider, wallet) = setup_test_provider_and_wallet().await;
+    let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance(provider, wallet).await;
 
-    let target = testfuelcoincontract_mod::ContractId { value: fuelcoin_id.into() };
-    // let asset_id = testfuelcoincontract_mod::ContractId { value: fuelcoin_id.into() };
-    let asset_id = target;
+    let target = testfuelcoincontract_mod::ContractId {
+        value: fuelcoin_id.into(),
+    };
+    let asset_id = target.clone();
 
     let mut balance_result = fuelcoin_instance
         .get_balance(target.clone(), asset_id.clone())
@@ -46,14 +57,20 @@ async fn can_burn() {
     fuelcoin_instance.mint_coins(11).call().await.unwrap();
     fuelcoin_instance.burn_coins(7).call().await.unwrap();
 
-    balance_result = fuelcoin_instance.get_balance(target, asset_id).call().await.unwrap();
+    balance_result = fuelcoin_instance
+        .get_balance(target, asset_id)
+        .call()
+        .await
+        .unwrap();
     assert_eq!(balance_result.value, 4);
 }
 
 #[tokio::test]
 async fn can_force_transfer() {
-    let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance().await;
-    let balance_id = get_balance_contract_id().await;
+    let (provider, wallet) = setup_test_provider_and_wallet().await;
+    let (fuelcoin_instance, fuelcoin_id) =
+        get_fuelcoin_instance(provider.clone(), wallet.clone()).await;
+    let balance_id = get_balance_contract_id(provider, wallet).await;
 
     let asset_id = testfuelcoincontract_mod::ContractId {
         value: fuelcoin_id.into(),
@@ -63,61 +80,82 @@ async fn can_force_transfer() {
         value: balance_id.into(),
     };
 
-    let mut balance_result = fuel_coin_instance.get_balance(asset_id.clone(), asset_id.clone()).call().await.unwrap();
-    assert_eq!(balance_result.value, 0);
-
-    fuel_coin_instance
-        .mint_coins(100)
+    let mut balance_result = fuelcoin_instance
+        .get_balance(asset_id.clone(), asset_id.clone())
         .call()
         .await
         .unwrap();
+    assert_eq!(balance_result.value, 0);
 
-    balance_result = fuel_coin_instance.get_balance(asset_id.clone(), asset_id.clone()).call().await.unwrap();
+    fuelcoin_instance.mint_coins(100).call().await.unwrap();
+
+    balance_result = fuelcoin_instance
+        .get_balance(asset_id.clone(), asset_id.clone())
+        .call()
+        .await
+        .unwrap();
     assert_eq!(balance_result.value, 100);
 
     // confirm initial balance on balance contract (recipient)
-    balance_result = fuel_coin_instance.get_balance(asset_id.clone(), target.clone()).set_contracts(&[balance_contract_id]).call().await.unwrap();
+    balance_result = fuelcoin_instance
+        .get_balance(asset_id.clone(), target.clone())
+        .set_contracts(&[balance_id])
+        .call()
+        .await
+        .unwrap();
     assert_eq!(balance_result.value, 0);
 
     let coins = 42u64;
 
-    fuel_coin_instance
+    fuelcoin_instance
         .force_transfer_coins(coins, asset_id.clone(), target.clone())
-        .set_contracts(&[fuelcoin_id, balance_contract_id])
+        .set_contracts(&[fuelcoin_id, balance_id])
         .call()
         .await
         .unwrap();
 
     // confirm remaining balance on fuelcoin contract
-    balance_result = fuel_coin_instance.get_balance(asset_id.clone(), asset_id.clone()).call().await.unwrap();
+    balance_result = fuelcoin_instance
+        .get_balance(asset_id.clone(), asset_id.clone())
+        .call()
+        .await
+        .unwrap();
     assert_eq!(balance_result.value, 58);
 
     // confirm new balance on balance contract (recipient)
-    balance_result = fuel_coin_instance.get_balance(asset_id.clone(), target.clone()).set_contracts(&[balance_contract_id]).call().await.unwrap();
+    balance_result = fuelcoin_instance
+        .get_balance(asset_id.clone(), target.clone())
+        .set_contracts(&[balance_id])
+        .call()
+        .await
+        .unwrap();
     assert_eq!(balance_result.value, 42);
 }
 
-async fn get_fuelcoin_instance() -> (TestFuelCoinContract, ContractId) {
+async fn get_fuelcoin_instance(
+    provider: Provider,
+    wallet: Wallet,
+) -> (TestFuelCoinContract, ContractId) {
     let salt = Salt::from([0u8; 32]);
     let compiled =
         Contract::load_sway_contract("test_projects/token_ops/out/debug/token_ops.bin", salt)
             .unwrap();
-    let (provider, wallet) = test_helpers::setup_test_provider_and_wallet().await;
     let fuelcoin_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
         .await
         .unwrap();
 
-    let fuelcoin_instance = TestRegistersContract::new(id.to_string(), provider, wallet);
+    let fuelcoin_instance = TestFuelCoinContract::new(fuelcoin_id.to_string(), provider, wallet);
 
     (fuelcoin_instance, fuelcoin_id)
 }
 
-async fn get_balance_contract_id() -> ContractId {
+async fn get_balance_contract_id(provider: Provider, wallet: Wallet) -> ContractId {
     let salt = Salt::from([0u8; 32]);
-    let compiled =
-        Contract::load_sway_contract("test_artifacts/balance_contract/out/debug/balance_contract.bin", salt)
-            .unwrap();
-    let (provider, wallet) = test_helpers::setup_test_provider_and_wallet().await;
+    let compiled = Contract::load_sway_contract(
+        "test_artifacts/balance_contract/out/debug/balance_contract.bin",
+        salt,
+    )
+    .unwrap();
     let balance_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
         .await
         .unwrap();
