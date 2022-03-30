bd5bc8e1d202dba3f7e3e6690be06df5d322912c
Nick Furfaro <nfurfaro33@gmail.com>
2022-03-30 12:15:18 -0600

    test: add 2 tests for reentrancy

--- sway-lib-std/tests/test_projects/reentrancy/mod.rs
+++ sway-lib-std/tests/test_projects/reentrancy/mod.rs
@@ -1,32 +1,81 @@
-use fuel_tx::Salt;
+use fuel_tx::{ContractId, Salt};
 use fuels_abigen_macro::abigen;
-use fuels_contract::contract::Contract;
-use rand::rngs::StdRng;
-use rand::{Rng, SeedableRng};
+use fuels_contract::{contract::Contract, parameters::TxParameters};
+use fuels_signers::provider::Provider;
+use fuels_signers::util::test_helpers::setup_test_provider_and_wallet;
+use fuels_signers::wallet::Wallet;
 
-abigen!(
-    TestFuelCoinContract,
-    "test_projects/token_ops/out/debug/token_ops-abi.json"
-);
-abigen!(AttackerContract, "test_artifacts/reentrancy_attacker_contract/src/abi.json",);
+abigen!(AttackerContract, "test_artifacts/reentrancy_attacker_contract/out/debug/reentrancy_attacker_contract-abi.json",);
 
 #[tokio::test]
-#[ignore]
-async fn not_reentrant() {
+async fn can_detect_reentrancy() {
+    let (provider, wallet) = setup_test_provider_and_wallet().await;
+    let (attacker_instance, _) =
+    get_attacker_instance(provider.clone(), wallet.clone()).await;
+    let target_id = get_target_contract_id(provider, wallet).await;
+
+    let sway_target_id = attackercontract_mod::ContractId {
+        value: target_id.into()
+    };
+
+    let result = attacker_instance
+        .launch_attack(sway_target_id)
+        .set_contracts(&[target_id])
+        .call()
+        .await
+        .unwrap();
+
+    assert_eq!(result.value, true);
+
 }
 
 #[tokio::test]
-async fn is_reentrant() {
+#[should_panic]
+async fn can_block_reentrancy() {
+    let (provider, wallet) = setup_test_provider_and_wallet().await;
+    let (attacker_instance, _) =
+    get_attacker_instance(provider.clone(), wallet.clone()).await;
+    let target_id = get_target_contract_id(provider, wallet).await;
+
+    let sway_target_id = attackercontract_mod::ContractId {
+        value: target_id.into()
+    };
+
+    attacker_instance
+        .launch_thwarted_attack(sway_target_id)
+        .set_contracts(&[target_id])
+        .call()
+        .await
+        .unwrap();
+}
+
+async fn get_attacker_instance(
+    provider: Provider,
+    wallet: Wallet,
+) -> (AttackerContract, ContractId) {
     let salt = Salt::from([0u8; 32]);
     let compiled =
-        Contract::load_sway_contract("test_projects/token_ops/out/debug/token_ops.bin", salt)
+        Contract::load_sway_contract("test_artifacts/reentrancy_attacker_contract/out/debug/reentrancy_attacker_contract.bin", salt)
             .unwrap();
-    let client = Provider::launch(Config::local_node()).await.unwrap();
-    let id = Contract::deploy(&compiled, &client).await.unwrap();
-    let fuelcoin_instance = TestFuelCoinContract::new(id.to_string(), client);
+    let id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
+        .await
+        .unwrap();
+
+    let instance = AttackerContract::new(id.to_string(), provider, wallet);
+
+    (instance, id)
 }
 
-#[tokio::test]
-#[ignore]
-async fn script_usage_of_is_reentrant() {
+async fn get_target_contract_id(provider: Provider, wallet: Wallet) -> ContractId {
+    let salt = Salt::from([0u8; 32]);
+    let compiled = Contract::load_sway_contract(
+        "test_artifacts/reentrancy_target_contract/out/debug/reentrancy_target_contract.bin",
+        salt,
+    )
+    .unwrap();
+    let id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
+        .await
+        .unwrap();
+
+    id
 }
