20cfbcf0d3f4ec9e9d848267dc590be732299d54
bing <tanbinghwang@gmail.com>
2022-04-09 11:31:23 +0800

    Extract code from main.rs into helpers.rs

--- scripts/forc-documenter/src/constants.rs
+++ scripts/forc-documenter/src/constants.rs
@@ -1,6 +1,8 @@
 pub const SUBHEADERS: &[&str] = &["USAGE:", "ARGS:", "OPTIONS:", "SUBCOMMANDS:"];
 pub const INDEX_HEADER: &str = "Here are a list of commands available to forc:\n\n";
 
+pub static RUN_WRITE_DOCS_MESSAGE: &str = "please run `cargo run --bin forc-documenter write-docs`";
+
 pub static EXAMPLES_HEADER: &str = "\n## EXAMPLES:\n";
 pub static FORC_INIT_EXAMPLE: &str = r#"
 ```console
