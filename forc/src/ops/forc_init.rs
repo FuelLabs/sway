0cd406b5b0ba282e4d64d60b9c9d9992d6c7b58e
Nicholas Dodson <nick@Nicholass-MacBook-Pro.local>
2022-04-14 15:18:17 -0700

    add welcome message to tempalte and no template init

--- forc/src/ops/forc_init.rs
+++ forc/src/ops/forc_init.rs
@@ -53,6 +53,40 @@ struct ContentResponse {
     url: String,
 }
 
+fn print_welcome_message() {
+    let read_the_docs = format!(
+        "Read the Docs:\n- {}\n- {}\n- {}",
+        Link::new("Sway Book", "https://fuellabs.github.io/sway/latest/"),
+        Link::new(
+            "Rust SDK Book",
+            "https://fuellabs.github.io/fuels-rs/latest/index.html"
+        ),
+        Link::new(
+            "Typescript SDK Docs",
+            "https://github.com/FuelLabs/fuels-ts"
+        )
+    );
+
+    let join_the_community = format!(
+        "Join the Community:\n- Follow us {}
+- Ask questions in dev-chat on {}",
+        Link::new("@SwayLang", "https://twitter.com/SwayLang"),
+        Link::new("Discord", "https://discord.com/invite/xfpK4Pe")
+    );
+
+    let report_bugs = format!(
+        "Report Bugs:\n- {}",
+        Link::new("Sway Issues", "https://github.com/FuelLabs/sway/issues/new")
+    );
+
+    let try_forc = "Now try `forc build` or `forc test`".to_string();
+
+    println!(
+        "\n{}\n\n----\n\n{}\n\n{}\n\n{}\n\n",
+        try_forc, read_the_docs, join_the_community, report_bugs
+    );
+}
+
 pub fn init(command: InitCommand) -> Result<()> {
     let project_name = command.project_name;
     validate_name(&project_name, "project name")?;
@@ -116,37 +150,7 @@ pub(crate) fn init_new_project(project_name: String) -> Result<()> {
 
     println_green(&format!("Successfully created: {}", project_name));
 
-    let read_the_docs = format!(
-        "Read the Docs:\n- {}\n- {}\n- {}",
-        Link::new("Sway Book", "https://fuellabs.github.io/sway/latest/"),
-        Link::new(
-            "Rust SDK Book",
-            "https://fuellabs.github.io/fuels-rs/latest/index.html"
-        ),
-        Link::new(
-            "Typescript SDK Docs",
-            "https://github.com/FuelLabs/fuels-ts"
-        )
-    );
-
-    let join_the_community = format!(
-        "Join the Community:\n- Follow us {}
-- Ask questions in dev-chat on {}",
-        Link::new("@SwayLang", "https://twitter.com/SwayLang"),
-        Link::new("Discord", "https://discord.com/invite/xfpK4Pe")
-    );
-
-    let report_bugs = format!(
-        "Report Bugs:\n- {}",
-        Link::new("Sway Issues", "https://github.com/FuelLabs/sway/issues/new")
-    );
-
-    let try_forc = "Now try `forc build` or `forc test`".to_string();
-
-    println!(
-        "\n{}\n\n----\n\n{}\n\n{}\n\n{}\n\n",
-        try_forc, read_the_docs, join_the_community, report_bugs
-    );
+    print_welcome_message();
 
     Ok(())
 }
@@ -206,7 +210,8 @@ pub(crate) fn init_from_git_template(project_name: String, example_url: &Url) ->
     }
 
     println_green(&format!("Successfully created: {}", project_name));
-    println_green("Now try and run 'forc test'");
+
+    print_welcome_message();
 
     Ok(())
 }
