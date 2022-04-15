ef8da14b2e3c9b903bcb2e2a5d459bf4dc261bb7
mitchmindtree <mitchell.nordine@fuel.sh>
2022-04-15 14:05:08 +1000

    Remove unnecessary module clone in struct expression type check

--- sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
+++ sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
@@ -1335,7 +1335,7 @@ impl TypedExpression {
             errors
         );
 
-        let decl = match module.clone().get_symbol(&call_path.suffix).value {
+        let decl = match module.get_symbol(&call_path.suffix).value {
             Some(TypedDeclaration::StructDeclaration(decl)) => decl,
             Some(_) => {
                 errors.push(CompileError::DeclaredNonStructAsStruct {
