61d77dd56c60002af3d8ba3327b56a178d5393f8
Toby Hutton <toby@grusly.com>
2022-04-18 09:50:22 +1000

    Fix contract call args handling. (#1272)
    Re-introduce the special case where if there's a single argument to a
    function call it is embedded directly in the call frame rather than
    being a pointer to the args elsewhere.

--- sway-core/tests/ir_to_asm/nested_single_word_struct.asm
+++ sway-core/tests/ir_to_asm/nested_single_word_struct.asm
@@ -11,10 +11,8 @@ eq   $r0 $r1 $r0              ; function selector comparison
 jnei $zero $r0 i11            ; jump to selected function
 rvrt $zero                    ; revert if no selectors matched
 lw   $r0 $fp i74              ; Base register for method parameter
-addi $r0 $r0 i0               ; Get address for arg input1
 addi $r0 $r0 i0               ; extract address
 lw   $r0 $r0 i0               ; extract_value @ 0
 ret  $r0
-noop                          ; word-alignment of data section
 .data:
 data_0 .u32 0x495d4a23
