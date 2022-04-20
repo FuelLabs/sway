133b19e3545213ccd25171dd10709438479285ec
Nick Furfaro <nfurfaro33@gmail.com>
2022-04-20 13:56:14 -0600

    build: update build.sh and move it

---
+++ test/src/sdk-harness/build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+FILES="./test_*/*"
+MANIFEST="forc.toml"
+
+for f in $FILES
+do
+if [ ! -e ${f}/$MANIFEST ]
+  then
+  echo "Not building $f, no manifest found."
+  else
+  if [ -d "${f}" ];
+    then
+    echo "building test $f..."
+      forc build -o temp -p $f
+      if ! [ -f temp ];
+        then
+        echo "Failed to build $f"
+        exit 1
+      fi
+      rm temp
+  fi
+fi
+done
+
+echo "Successfully built all projects."
