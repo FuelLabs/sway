a7534378da8aedaad47e452e6991eefafdb8473d
Joshua Batty <joshpbatty@gmail.com>
2022-04-20 14:34:27 +1000

    add test harness for counter example

---
+++ examples/counter/tests/harness.rs
@@ -0,0 +1,48 @@
+use fuel_tx::Salt;
+use fuels_abigen_macro::abigen;
+use fuels_contract::contract::Contract;
+use fuels_contract::parameters::TxParameters;
+use fuels_signers::util::test_helpers::setup_test_provider_and_wallet;
+use rand::rngs::StdRng;
+use rand::{Rng, SeedableRng};
+
+// Generate Rust bindings from our contract JSON ABI
+abigen!(MyContract, "examples/counter/out/debug/counter-abi.json");
+
+#[tokio::test]
+async fn harness() {
+    let rng = &mut StdRng::seed_from_u64(2322u64);
+
+    // Build the contract
+    let salt: [u8; 32] = rng.gen();
+    let salt = Salt::from(salt);
+
+    // Launch a local network and deploy the contract
+    let compiled = Contract::load_sway_contract("./out/debug/counter.bin", salt).unwrap();
+    let (provider, wallet) = setup_test_provider_and_wallet().await;
+    let contract_id = Contract::deploy(&compiled, &provider, &wallet, TxParameters::default())
+        .await
+        .unwrap();
+    println!("Contract deployed @ {:x}", contract_id);
+
+    let contract_instance = MyContract::new(contract_id.to_string(), provider, wallet);
+
+    // Call `initialize_counter()` method in our deployed contract.
+    // Note that, here, you get type-safety for free!
+    let result = contract_instance
+        .initialize_counter(42)
+        .call()
+        .await
+        .unwrap();
+
+    assert_eq!(42, result.value);
+
+    // Call `increment_counter()` method in our deployed contract.
+    let result = contract_instance
+        .increment_counter(10)
+        .call()
+        .await
+        .unwrap();
+
+    assert_eq!(52, result.value);
+}
