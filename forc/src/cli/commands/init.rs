ca9f09ed4ab745ba30a8041c40b983342e2f64cb
Chris O'Brien <obrchr96@gmail.com>
2022-04-22 13:48:39 -0500

    rmv needless help msg

--- forc/src/cli/commands/init.rs
+++ forc/src/cli/commands/init.rs
@@ -7,14 +7,6 @@ const TEMPLATE_HELP: &str = r#"Initialize a new project from a template.
 Example Templates: 
  - counter"#;
 
-const PROJECT_HELP: &str = r#"Initialize a new project of a specific type. If a project type is not given the default project type is a contract.
-
-Project Types:
- - contract
- - script
- - predicate
- - library"#;
-
 /// Create a new Forc project.
 #[derive(Debug, Parser)]
 pub struct Command {
@@ -23,13 +15,13 @@ pub struct Command {
     pub template: Option<String>,
     /// There are four accepted project types: contract, script, predicate or library.
     /// If a project type is not given the default project type is a contract.
-    #[clap(long, help = PROJECT_HELP)]
+    #[clap(long)]
     pub contract: bool,
-    #[clap(long, help = PROJECT_HELP)]
+    #[clap(long)]
     pub script: bool,
-    #[clap(long, help = PROJECT_HELP)]
+    #[clap(long)]
     pub predicate: bool,
-    #[clap(long = "lib", help = PROJECT_HELP)]
+    #[clap(long = "lib")]
     pub library: bool,
     /// The name of your project
     pub project_name: String,
