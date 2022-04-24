6723c7e5965c4be09e0a4fd9f0859ef9991023a6
bing <binggh@proton.me>
2022-04-24 10:12:35 +0800

    emit file names by default, with option of full path

--- forc/src/cli/commands/plugins.rs
+++ forc/src/cli/commands/plugins.rs
@@ -1,8 +1,35 @@
+use crate::cli::PluginsCommand;
 use anyhow::Result;
+use clap::Parser;
+use std::path::PathBuf;
 
-pub(crate) fn exec() -> Result<()> {
+/// Find all forc plugins available via `PATH`.
+///
+/// Prints information about each discovered plugin.
+#[derive(Debug, Parser)]
+pub struct Command {
+    /// Prints the absolute path to each discovered plugin.
+    #[clap(long = "verbose", short = 'v')]
+    print_full_path: bool,
+}
+
+pub(crate) fn exec(command: PluginsCommand) -> Result<()> {
+    let PluginsCommand { print_full_path } = command;
+
     for path in crate::cli::plugin::find_all() {
-        println!("{}", path.display());
+        print_plugin(path, print_full_path);
     }
     Ok(())
+}
+
+fn print_plugin(path: PathBuf, print_full_path: bool) {
+    if print_full_path {
+        println!("{}", path.display());
+    } else {
+        if let Some(file_name) = path.file_name() {
+            println!("{}", file_name.to_str().unwrap());
+        } else {
+            println!("{}", path.display());
+        }
+    }
 }
