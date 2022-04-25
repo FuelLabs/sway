ac23d595f14f4ddb9c6fe57ee46b5e7816a06f33
Nick Furfaro <nfurfaro33@gmail.com>
2022-04-25 10:55:35 -0600

    test: add test for store_u64

--- test/src/sdk-harness/test_projects/storage/mod.rs
+++ test/src/sdk-harness/test_projects/storage/mod.rs
@@ -9,7 +9,8 @@ abigen!(Storage, "test_projects/storage/out/debug/storage-abi.json");
 async fn get_contract_instance() -> (Storage, ContractId) {
     // Deploy the compiled contract
     let salt = Salt::from([0u8; 32]);
-    let compiled = Contract::load_sway_contract("test_projects/storage/out/debug/storage.bin", salt).unwrap();
+    let compiled =
+        Contract::load_sway_contract("test_projects/storage/out/debug/storage.bin", salt).unwrap();
 
     // Launch a local network and deploy the contract
     let (provider, wallet) = test_helpers::setup_test_provider_and_wallet().await;
@@ -24,8 +25,10 @@ async fn get_contract_instance() -> (Storage, ContractId) {
 }
 
 #[tokio::test]
-async fn can_get_contract_id() {
-    let (_instance, _id) = get_contract_instance().await;
-
-    // Now you have an instance of your contract you can use to test each function
+async fn can_store_u64() {
+    let (instance, _id) = get_contract_instance().await;
+    let n = 42;
+    instance.store_u64(n).call().await.unwrap();
+    let result = instance.get_u64().call().await.unwrap();
+    assert_eq!(result.value, n);
 }
