00b618621117a2514683fce0667cf4767acb2e4e
bing <binggh@proton.me>
2022-04-28 14:35:44 +0800

    use clap for build-all-examples

--- scripts/build-all-examples/src/main.rs
+++ scripts/build-all-examples/src/main.rs
@@ -2,11 +2,32 @@
 //!
 //! NOTE: This expects `forc`, `forc-fmt`, and `cargo` to be available in `PATH`.
 
+use anyhow::anyhow;
+use clap::{Parser, Subcommand};
 use std::{
     fs,
     io::{self, Write},
     path::{Path, PathBuf},
 };
+
+#[derive(Parser)]
+#[clap(name = "build-all-examples", about = "Forc Examples Builder")]
+struct Cli {
+    /// the command to run
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    BuildExamples(BuildExamplesCommand),
+}
+
+#[derive(Debug, Parser)]
+struct BuildExamplesCommand {
+    #[clap(long)]
+    pub paths: Option<Vec<String>>,
+}
 
 fn get_sway_path() -> PathBuf {
     let mut curr_path = std::env::current_dir().unwrap();
@@ -18,75 +39,60 @@ fn get_sway_path() -> PathBuf {
     }
 }
 
-fn main() {
-    let examples_dir = get_sway_path().join("examples");
+fn build_and_report_result(entry: fs::DirEntry) -> (PathBuf, bool) {
+    let success = false;
+    let path = entry.path();
 
-    // Track discovered projects and whether or not they were successful.
+    if !path.is_dir() || !dir_contains_forc_manifest(&path) {
+        return (path, success);
+    }
+
+    let build_output = std::process::Command::new("forc")
+        .args(["build", "--path"])
+        .arg(&path)
+        .output()
+        .expect("failed to run `forc build` for example project");
+
+    let fmt_output = std::process::Command::new("forc")
+        .args(["fmt", "--check", "--path"])
+        .arg(&path)
+        .output()
+        .expect("failed to run `forc fmt --check` for example project");
+
+    // Print output on failure so we can read it in CI.
+    let success = if !build_output.status.success() || !fmt_output.status.success() {
+        io::stdout().write_all(&build_output.stdout).unwrap();
+        io::stdout().write_all(&fmt_output.stdout).unwrap();
+        io::stdout().write_all(&build_output.stderr).unwrap();
+        io::stdout().write_all(&fmt_output.stderr).unwrap();
+        false
+    } else {
+        true
+    };
+
+    (path, success)
+}
+
+fn build_examples(command: BuildExamplesCommand) {
+    let BuildExamplesCommand { paths } = command;
+    let paths_vec = Vec::from(paths);
+
     let mut summary: Vec<(PathBuf, bool)> = vec![];
-
-    for res in fs::read_dir(examples_dir).expect("failed to walk examples directory") {
+    for res in fs::read_dir(paths).expect("failed to walk examples directory") {
         let entry = match res {
             Ok(entry) => entry,
             _ => continue,
         };
-        let path = entry.path();
-        if !path.is_dir() || !dir_contains_forc_manifest(&path) {
-            continue;
-        }
-
-        let build_output = std::process::Command::new("forc")
-            .args(["build", "--path"])
-            .arg(&path)
-            .output()
-            .expect("failed to run `forc build` for example project");
-
-        let fmt_output = std::process::Command::new("forc")
-            .args(["fmt", "--check", "--path"])
-            .arg(&path)
-            .output()
-            .expect("failed to run `forc fmt --check` for example project");
-
-        // Print output on failure so we can read it in CI.
-        let success = if !build_output.status.success() || !fmt_output.status.success() {
-            io::stdout().write_all(&build_output.stdout).unwrap();
-            io::stdout().write_all(&fmt_output.stdout).unwrap();
-            io::stdout().write_all(&build_output.stderr).unwrap();
-            io::stdout().write_all(&fmt_output.stderr).unwrap();
-            false
-        } else {
-            true
-        };
-
-        summary.push((path, success));
+        summary.push(build_and_report_result(entry))
     }
+}
 
-    println!("\nBuild and check formatting of all examples summary:");
-    let mut successes = 0;
-    for (path, success) in &summary {
-        let (checkmark, status) = if *success {
-            ("[✓]", "succeeded")
-        } else {
-            ("[x]", "failed")
-        };
-        println!("  {}: {} {}!", checkmark, path.display(), status);
-        if *success {
-            successes += 1;
-        }
-    }
-    let failures = summary.len() - successes;
-    let successes_str = if successes == 1 {
-        "success"
-    } else {
-        "successes"
-    };
-    let failures_str = if failures == 1 { "failure" } else { "failures" };
-    println!(
-        "{} {}, {} {}",
-        successes, successes_str, failures, failures_str
-    );
+fn main() -> Result<(), anyhow::Error> {
+    let cli = Cli::parse();
+    let examples_dir = get_sway_path().join("examples");
 
-    if failures > 0 {
-        std::process::exit(1);
+    match cli.command {
+        Commands::BuildExamples(command) => build_examples(command)?,
     }
 }
 
