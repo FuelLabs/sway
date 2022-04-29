fe776ce42ca70e5420b07596b9c0ce98ce09e9b5
Nick Furfaro <nfurfaro33@gmail.com>
2022-04-29 12:08:57 -0600

    test: refactor evm_ecr to use script_runner

--- test/src/sdk-harness/test_projects/evm_ecr/mod.rs
+++ test/src/sdk-harness/test_projects/evm_ecr/mod.rs
@@ -1,36 +1,9 @@
-use fuel_core::service::Config;
-use fuel_tx::Transaction;
-use fuels_contract::script::Script;
-use fuels_signers::provider::Provider;
-use std::fs::read;
+use test_helpers::script_runner;
 
-async fn execute_script(bin_path: &str) -> u64 {
-    let bin = read(bin_path);
-    let client = Provider::launch(Config::local_node()).await.unwrap();
-
-    let tx = Transaction::Script {
-        gas_price: 0,
-        gas_limit: 1_000_000,
-        maturity: 0,
-        byte_price: 0,
-        receipts_root: Default::default(),
-        script: bin.unwrap(), // Here we pass the compiled script into the transaction
-        script_data: vec![],
-        inputs: vec![],
-        outputs: vec![],
-        witnesses: vec![vec![].into()],
-        metadata: None,
-    };
-
-    let script = Script::new(tx);
-    let receipts = script.call(&client).await.unwrap();
-
-    receipts[0].val().unwrap()
-}
 
 #[tokio::test]
 async fn evm_ecr_implementation() {
     let path_to_bin = "test_projects/evm_ecr/out/debug/evm_ecr.bin";
-    let return_val = execute_script(path_to_bin).await;
+    let return_val = script_runner(path_to_bin).await;
     assert_eq!(1, return_val);
 }
