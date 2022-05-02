36763fc768dd535f3d9eeaa3b0d31a9035dc0a36
Rashad Alston <rashad@Rashads-MBP.attlocal.net>
2022-05-02 16:51:36 -0700

    Add --describe -d opt to forc plugins

--- forc/src/cli/commands/plugins.rs
+++ forc/src/cli/commands/plugins.rs
@@ -11,15 +11,57 @@ pub struct Command {
     /// Prints the absolute path to each discovered plugin.
     #[clap(long = "paths", short = 'p')]
     print_full_path: bool,
+    /// Prints the long description associated with each listed plugin
+    #[clap(long = "describe", short = 'd')]
+    describe: bool,
 }
 
 pub(crate) fn exec(command: PluginsCommand) -> Result<()> {
-    let PluginsCommand { print_full_path } = command;
+    let PluginsCommand {
+        print_full_path,
+        describe,
+    } = command;
 
+    println!("Installed Plugins:");
     for path in crate::cli::plugin::find_all() {
-        print_plugin(path, print_full_path);
+        print_plugin(path, print_full_path, describe);
     }
     Ok(())
+}
+
+fn parse_description_for_plugin(plugin: &str) -> String {
+    use std::process::Command;
+    let proc = Command::new(plugin)
+        .arg("-h")
+        .output()
+        .expect("Could not get plugin description.");
+
+    let stdout = String::from_utf8_lossy(&proc.stdout);
+    stdout
+        .split('\n')
+        .nth(1)
+        .unwrap_or("No description found for this plugin.")
+        .to_owned()
+}
+
+fn format_print_description(path: PathBuf, print_full_path: bool, describe: bool) -> String {
+    let display = if print_full_path {
+        path.display().to_string()
+    } else {
+        path.file_name()
+            .expect("Failed to read file name")
+            .to_str()
+            .expect("Failed to print file name")
+            .to_string()
+    };
+
+    let description = parse_description_for_plugin(&display);
+
+    if describe {
+        return format!("  {} \t\t{}", display, description);
+    }
+
+    display
 }
 
 /// # Panics
@@ -28,16 +70,9 @@ pub(crate) fn exec(command: PluginsCommand) -> Result<()> {
 /// paths yielded from plugin::find_all(), as well as that the file names are in valid
 /// unicode format since file names should be prefixed with `forc-`. Should one of these 2
 /// assumptions fail, this function panics.
-fn print_plugin(path: PathBuf, print_full_path: bool) {
-    if print_full_path {
-        println!("{}", path.display());
-    } else {
-        println!(
-            "{}",
-            path.file_name()
-                .expect("Failed to read file name")
-                .to_str()
-                .expect("Failed to print file name")
-        );
-    }
+fn print_plugin(path: PathBuf, print_full_path: bool, describe: bool) {
+    println!(
+        "{}",
+        format_print_description(path, print_full_path, describe)
+    )
 }
