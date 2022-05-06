bb86a539e0da0ac4bc54d9e98c3cdb2ab47b7323
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2022-05-06 14:43:50 -0400

    also test k256

--- test/src/sdk-harness/test_projects/hashing/mod.rs
+++ test/src/sdk-harness/test_projects/hashing/mod.rs
@@ -26,7 +26,11 @@ async fn get_hashing_instance() -> (HashingTestContract, ContractId) {
 async fn test_hash_u64() {
     let (instance, _id) = get_hashing_instance().await;
     // Check that hashing the same `u64` results in the same hash
-    let result1 = instance.get_hash_u64(42).call().await.unwrap();
-    let result2 = instance.get_hash_u64(42).call().await.unwrap();
-    assert_eq!(result1.value, result2.value);
+    let sha256_result1 = instance.get_s256_hash_u64(42).call().await.unwrap();
+    let sha256_result2 = instance.get_s256_hash_u64(42).call().await.unwrap();
+    assert_eq!(sha256_result1.value, sha256_result2.value);
+
+    let keccak256_result1 = instance.get_k256_hash_u64(42).call().await.unwrap();
+    let keccak256_result2 = instance.get_k256_hash_u64(42).call().await.unwrap();
+    assert_eq!(keccak256_result1.value, keccak256_result2.value);
 }
--- test/src/sdk-harness/test_projects/hashing/src/main.sw
+++ test/src/sdk-harness/test_projects/hashing/src/main.sw
@@ -3,11 +3,16 @@ contract;
 use std::hash::{HashMethod, hash_u64};
 
 abi MyContract {
-    fn get_hash_u64(value: u64 ) -> b256;
+    fn get_s256_hash_u64(value: u64 ) -> b256;
+    fn get_k256_hash_u64(value: u64 ) -> b256;
 }
 
 impl MyContract for Contract {
-    fn get_hash_u64(value: u64) -> b256 {
+    fn get_s256_hash_u64(value: u64) -> b256 {
         hash_u64(value, HashMethod::Sha256)
+    }
+
+    fn get_k256_hash_u64(value: u64) -> b256 {
+        hash_u64(value, HashMethod::Keccak256)
     }
 }
