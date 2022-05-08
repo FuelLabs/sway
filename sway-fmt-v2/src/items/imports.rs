39afb88483c26e0c6678e5804096cf1e9c785275
Chris O'Brien <obrchr96@gmail.com>
2022-05-08 01:07:45 -0500

    fmt

--- sway-fmt-v2/src/items/imports.rs
+++ sway-fmt-v2/src/items/imports.rs
@@ -1,5 +1,5 @@
+use crate::fmt::{Format, FormattedCode, Formatter};
 use sway_parse::ItemUse;
-use crate::fmt::{Format, Formatter, FormattedCode};
 
 impl Format for ItemUse {
     fn format(&self, formatter: &Formatter) -> FormattedCode {
