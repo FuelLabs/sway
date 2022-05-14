668d7896b4e98043e3fb42318971e91501e013b3
binggh <binggh@proton.me>
2022-05-14 13:11:08 +0800

    Introduce mdbook-forc-documenter

---
+++ scripts/mdbook-forc-documenter/src/bin/mdbook-forc-documenter.rs
@@ -0,0 +1,72 @@
+use clap::{Arg, ArgMatches, Command};
+use mdbook::book::Summary;
+use mdbook::errors::Error;
+use mdbook::preprocess::{CmdPreprocessor, Preprocessor};
+use semver::{Version, VersionReq};
+use std::fs::read_to_string;
+use std::io;
+use std::process;
+
+use mdbook_forc_documenter::ForcDocumenter;
+
+pub fn make_app() -> Command<'static> {
+    Command::new("nop-preprocessor")
+        .about("A mdbook preprocessor which does precisely nothing")
+        .subcommand(
+            Command::new("supports")
+                .arg(Arg::new("renderer").required(true))
+                .about("Check whether a renderer is supported by this preprocessor"),
+        )
+}
+
+fn main() {
+    let matches = make_app().get_matches();
+
+    // Users will want to construct their own preprocessor here
+    let preprocessor = ForcDocumenter::new();
+
+    if let Some(sub_args) = matches.subcommand_matches("supports") {
+        handle_supports(&preprocessor, sub_args);
+    } else if let Err(e) = handle_preprocessing(&preprocessor) {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}
+
+fn handle_preprocessing(pre: &dyn Preprocessor) -> Result<(), Error> {
+    let (ctx, book) = CmdPreprocessor::parse_input(io::stdin())?;
+
+    let book_version = Version::parse(&ctx.mdbook_version)?;
+    let version_req = VersionReq::parse(mdbook::MDBOOK_VERSION)?;
+
+    if !version_req.matches(&book_version) {
+        eprintln!(
+            "Warning: The {} plugin was built against version {} of mdbook, \
+             but we're being called from version {}",
+            pre.name(),
+            mdbook::MDBOOK_VERSION,
+            ctx.mdbook_version
+        );
+    }
+    let output = process::Command::new("forc")
+        .arg("--version")
+        .output()
+        .expect("Failed running forc --version");
+
+    let processed_book = pre.run(&ctx, book)?;
+    serde_json::to_writer(io::stdout(), &processed_book);
+
+    Ok(())
+}
+
+fn handle_supports(pre: &dyn Preprocessor, sub_args: &ArgMatches) -> ! {
+    let renderer = sub_args.value_of("renderer").expect("Required argument");
+    let supported = pre.supports_renderer(renderer);
+
+    // Signal whether the renderer is supported by exiting with 1 or 0.
+    if supported {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
---
+++ scripts/mdbook-forc-documenter/src/lib.rs
@@ -0,0 +1,123 @@
+use crate::formatter::{format_header_line, format_index_entry, format_line};
+use anyhow::{anyhow, Result};
+use mdbook::book::{Book, BookItem};
+use mdbook::errors::Error;
+use mdbook::preprocess::{Preprocessor, PreprocessorContext};
+use std::process;
+
+mod formatter;
+
+pub struct ForcDocumenter;
+
+impl ForcDocumenter {
+    pub fn new() -> ForcDocumenter {
+        ForcDocumenter
+    }
+}
+
+impl Preprocessor for ForcDocumenter {
+    fn name(&self) -> &str {
+        "forc-documenter"
+    }
+
+    fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
+        let output = process::Command::new("forc")
+            .arg("--version")
+            .output()
+            .expect("Failed running forc --version");
+        let version =
+            String::from_utf8_lossy(&output.stdout) + String::from_utf8_lossy(&output.stderr);
+        let version_message = "Running forc --help using ".to_owned() + &version;
+        eprintln!("{}", version_message);
+
+        let output = process::Command::new("forc")
+            .arg("--help")
+            .output()
+            .expect("Failed running forc --version");
+
+        let s = String::from_utf8_lossy(&output.stdout) + String::from_utf8_lossy(&output.stderr);
+        let lines = s.lines();
+
+        let mut subcommand_is_parsed = false;
+        let mut possible_commands = vec![];
+
+        for line in lines {
+            if subcommand_is_parsed {
+                let (command, _) = line.trim().split_once(' ').unwrap_or(("", ""));
+                possible_commands.push(command);
+            }
+            if line == "SUBCOMMANDS:" {
+                subcommand_is_parsed = true;
+            }
+        }
+
+        book.for_each_mut(|item| {
+            if let BookItem::Chapter(ref mut chapter) = item {
+                if chapter.name == "Commands" {
+                    // add commands stuff here
+                    let mut command_index_content = String::new();
+
+                    for sub_item in chapter.sub_items.iter_mut() {
+                        if let BookItem::Chapter(ref mut command_chapter) = sub_item {
+                            let forc_subcommand = command_chapter.name.split(' ').nth(1).unwrap();
+                            if possible_commands.iter().any(|&i| i == forc_subcommand) {
+                                let mut result = match generate_doc_output(forc_subcommand) {
+                                    Ok(output) => output,
+                                    Err(_) => continue,
+                                };
+
+                                result = result.trim().to_string();
+                                command_index_content
+                                    .push_str(&format_index_entry(&command_chapter.name));
+                                command_chapter.content = result;
+                            }
+                        }
+                    }
+
+                    chapter.content.push_str(&command_index_content);
+                }
+            }
+        });
+
+        Ok(book)
+    }
+
+    fn supports_renderer(&self, renderer: &str) -> bool {
+        renderer == "html"
+    }
+}
+
+fn generate_doc_output(subcommand: &str) -> Result<String> {
+    let mut result = String::new();
+
+    let output = process::Command::new("forc")
+        .args([subcommand, "--help"])
+        .output()
+        .expect("forc --help failed to run");
+
+    if !output.status.success() {
+        return Err(anyhow!("Failed to run forc {} --help", subcommand));
+    }
+
+    let s = String::from_utf8_lossy(&output.stdout) + String::from_utf8_lossy(&output.stderr);
+
+    for (index, line) in s.lines().enumerate() {
+        let mut formatted_line = String::new();
+        let line = line.trim();
+
+        if index == 0 {
+            formatted_line.push_str(&format_header_line(line));
+        } else if index == 1 {
+            formatted_line.push_str(line);
+        } else {
+            formatted_line.push_str(&format_line(line))
+        }
+
+        result.push_str(&formatted_line);
+
+        if !formatted_line.ends_with('\n') {
+            result.push('\n');
+        }
+    }
+    Ok(result)
+}
