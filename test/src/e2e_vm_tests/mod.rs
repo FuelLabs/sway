c46358d3d8ec175c3b521609fe09f49db180469a
Alex Hansen <alex@alex-hansen.com>
2022-05-19 18:00:17 -0400

    wip

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -335,6 +335,10 @@ pub fn run(filter_regex: Option<regex::Regex>) {
                 0x67, 0x6e, 0x5f, 0x13,
             ])),
         ),
+        (
+            "should_pass/language/unused_generic",
+            ProgramState::Return(42),
+        ),
     ];
 
     let mut number_of_tests_run =
