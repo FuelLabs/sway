bb02be07c3287572b011b5fce823829a19507285
Braqzen <sydney.boroch@fuel.sh>
2022-05-19 14:22:00 +0100

    Deprecating old functions

--- test/src/sdk-harness/test_projects/hashing/mod.rs
+++ test/src/sdk-harness/test_projects/hashing/mod.rs
@@ -137,19 +137,6 @@ async fn get_hashing_instance() -> (HashingTestContract, ContractId) {
     (instance, id)
 }
 
-#[tokio::test]
-async fn test_hash_u64() {
-    let (instance, _id) = get_hashing_instance().await;
-    // Check that hashing the same `u64` results in the same hash
-    let sha256_result1 = instance.get_s256_hash_u64(42).call().await.unwrap();
-    let sha256_result2 = instance.get_s256_hash_u64(42).call().await.unwrap();
-    assert_eq!(sha256_result1.value, sha256_result2.value);
-
-    let keccak256_result1 = instance.get_k256_hash_u64(42).call().await.unwrap();
-    let keccak256_result2 = instance.get_k256_hash_u64(42).call().await.unwrap();
-    assert_eq!(keccak256_result1.value, keccak256_result2.value);
-}
-
 mod sha256 {
 
     use super::*;
