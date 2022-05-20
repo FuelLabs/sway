309ab83aa1c4f1e3d9c168394de90fae442e2c82
Kaya Gökalp <kayagokalp@sabanciuniv.edu>
2022-05-20 10:33:55 +0300

    Update forc-util/src/lib.rs
    Co-authored-by: mitchmindtree <mitchell.nordine@fuel.sh>

--- forc-util/src/lib.rs
+++ forc-util/src/lib.rs
@@ -412,7 +412,8 @@ fn construct_window<'a>(
 const LOG_FILTER: &str = "RUST_LOG";
 
 /// A subscriber built from default `tracing_subscriber::fmt::SubscriberBuilder` such that it would match directly using `println!` throughout the repo.
-/// RUST_LOG environment variable can be used to set different minimum level for the subscriber, default is INFO
+///
+/// `RUST_LOG` environment variable can be used to set different minimum level for the subscriber, default is `INFO`.
 pub fn init_tracing_subscriber() {
     let filter = match env::var_os(LOG_FILTER) {
         Some(_) => EnvFilter::try_from_default_env().expect("Invalid `RUST_LOG` provided"),
