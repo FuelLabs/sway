a08570a6c2d71ad24bb6d0346b22029581d49063
Alex Hansen <alex@alex-hansen.com>
2022-05-26 14:25:55 -0700

    Introduce FromTrait method name (#1684)
    * Introduce FromTrait method name
    
    * clippy

--- sway-lsp/src/core/token.rs
+++ sway-lsp/src/core/token.rs
@@ -387,11 +387,7 @@ fn handle_expression(exp: Expression, tokens: &mut Vec<Token>) {
             }
 
             //TODO handle methods from imported modules
-            if let MethodName::FromType {
-                type_name: Some(type_name),
-                ..
-            } = &method_name
-            {
+            if let MethodName::FromType { type_name, .. } = &method_name {
                 handle_custom_type(type_name, tokens);
             }
 
