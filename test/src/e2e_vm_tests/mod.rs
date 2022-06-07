f19629d2f6fa4d60048821ab85f66425b63b1388
emilyaherbert <emily.herbert@fuel.sh>
2022-06-07 18:16:10 -0500

    Remove stale test.

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -565,7 +565,6 @@ pub fn run(filter_regex: Option<regex::Regex>) {
         "should_fail/storage_in_script",
         "should_fail/multiple_impl_abi",
         "should_fail/multiple_impl_fns",
-        "should_fail/impl_with_bad_generic",
     ];
     number_of_tests_run += negative_project_names.iter().fold(0, |acc, name| {
         if filter(name) {
