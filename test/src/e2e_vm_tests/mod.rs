3f0e80c2675587cd6479ed46029709f28da39d95
emilyaherbert <emily.herbert@fuel.sh>
2022-06-10 17:54:25 -0500

    Prevent unconstrained type parameters.

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -576,6 +576,7 @@ pub fn run(filter_regex: Option<regex::Regex>) {
         "should_fail/repeated_enum_variant",
         "should_fail/repeated_storage_field",
         "should_fail/repeated_struct_field",
+        "should_fail/impl_with_bad_generic",
     ];
     number_of_tests_run += negative_project_names.iter().fold(0, |acc, name| {
         if filter(name) {
