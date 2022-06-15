aaeb8224281dadfa8005adb730b52ac60768f5a6
Waseem G <contact@waseem-g.com>
2022-06-16 09:25:52 +1000

    get root of predicate bytecode

--- forc/src/utils/mod.rs
+++ forc/src/utils/mod.rs
@@ -11,3 +11,7 @@ pub const SWAY_GIT_TAG: &str = concat!("v", clap::crate_version!());
 /// The suffix that helps identify the file which contains the hash of the binary file created when
 /// scripts are built.
 pub const SWAY_BIN_HASH_SUFFIX: &str = "-bin-hash";
+
+/// The suffix that helps identify the file which contains the root hash of the binary file created
+/// when predicates are built.
+pub const SWAY_BIN_ROOT_SUFFIX: &str = "-bin-root-hash";
