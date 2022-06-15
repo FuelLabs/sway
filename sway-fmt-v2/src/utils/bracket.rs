a3affa895fbdb36a9873d16312ebaa885d87c74e
Chris O'Brien <obrchr96@gmail.com>
2022-06-15 09:14:49 -0500

    Add docs for associated funtions

--- sway-fmt-v2/src/utils/bracket.rs
+++ sway-fmt-v2/src/utils/bracket.rs
@@ -4,17 +4,20 @@
 use crate::Formatter;
 
 pub trait CurlyDelimiter {
-    /// Handles brace open scenerio. Checks the config for the placement of the brace.
-    /// Modifies the current shape of the formatter.
+    // Handles brace open scenerio. Checks the config for the placement of the brace.
+    // Modifies the current shape of the formatter.
     fn handle_open_brace(push_to: &mut String, formatter: &mut Formatter);
 
-    /// Handles brace close scenerio.
-    /// Currently it simply pushes a `}` and modifies the shape.
+    // Handles brace close scenerio.
+    // Currently it simply pushes a `}` and modifies the shape.
     fn handle_closed_brace(push_to: &mut String, formatter: &mut Formatter);
 }
 
 pub trait Parenthesis {
+    // Handles open parenthesis scenarios, checking the config for placement
+    // and modifying the shape of the formatter where necessary.
     fn open_parenthesis(line: &mut String, formatter: &mut Formatter);
 
+    // Handles the closing parenthesis scenario.
     fn close_parenthesis(line: &mut String, formatter: &mut Formatter);
 }
