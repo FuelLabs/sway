64a33f2d5d9dab4a5993a2ba07d99f022a0be770
Chris O'Brien <obrchr96@gmail.com>
2022-06-18 10:51:38 -0500

    wip

--- sway-fmt-v2/src/utils/attributes.rs
+++ sway-fmt-v2/src/utils/attributes.rs
@@ -1,11 +1,38 @@
 use crate::Formatter;
-use sway_parse::AttributeDecl;
+use sway_parse::attribute::AttributeDecl;
 use sway_types::Spanned;
-pub fn format_attributes(attributes: Vec<AttributeDecl>, _formatter: &mut Formatter) -> String {
-    // TODO format attributes
-    attributes
-        .into_iter()
-        .map(|x| x.span().as_str().to_string())
-        .collect::<Vec<String>>()
-        .join("\n")
+
+use super::bracket::SquareBracket;
+
+trait Format {
+    fn format(&self, line: &mut String, formatter: &mut Formatter);
+}
+
+pub fn format_attributes(attributes: Vec<AttributeDecl>, formatter: &mut Formatter) -> String {
+    let mut formatted_code = String::new();
+    for attr in &attributes {
+        AttributeDecl::format(attr, &mut formatted_code, formatter);
+    }
+
+    formatted_code
+}
+
+impl Format for AttributeDecl {
+    fn format(&self, line: &mut String, _formatter: &mut Formatter) {
+        line.push_str(self.hash_token.span().as_str());
+        Self::open_square_bracket(line, _formatter);
+        // TODO: attributes are joined with their args during `span()`
+        // but we may need to do formatting there eventually
+        line.push_str(self.attribute.span().as_str());
+        Self::close_square_bracket(line, _formatter);
+    }
+}
+
+impl SquareBracket for AttributeDecl {
+    fn open_square_bracket(line: &mut String, _formatter: &mut Formatter) {
+        line.push('[');
+    }
+    fn close_square_bracket(line: &mut String, _formatter: &mut Formatter) {
+        line.push_str("test]\n");
+    }
 }
