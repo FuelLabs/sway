fb9ffbe6e5f1f5a47d89f20c9e42f88166cfd1fe
Chris O'Brien <obrchr96@gmail.com>
2022-06-18 11:38:06 -0500

    updated unimplemented cases to return span

--- sway-fmt-v2/src/items/item_abi.rs
+++ sway-fmt-v2/src/items/item_abi.rs
@@ -1,8 +1,9 @@
 use crate::fmt::{Format, FormattedCode, Formatter};
 use sway_parse::ItemAbi;
+use sway_types::Spanned;
 
 impl Format for ItemAbi {
     fn format(&self, _formatter: &mut Formatter) -> FormattedCode {
-        todo!()
+        self.span().as_str().to_string()
     }
 }
