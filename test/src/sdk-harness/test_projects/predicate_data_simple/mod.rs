7b11b6b5b4f8b0aa78eccbc80960588206285919
Cameron Manavian <cameron.manavian@fuel.sh>
2022-06-19 23:22:36 -0700

    Refactor for readability

--- test/src/sdk-harness/test_projects/predicate_data_simple/mod.rs
+++ test/src/sdk-harness/test_projects/predicate_data_simple/mod.rs
@@ -1,65 +1,87 @@
-use assert_matches::assert_matches;
-use fuel_core::service::{Config, FuelService};
-use fuel_crypto::SecretKey;
-use fuel_gql_client::client::FuelClient;
+use fuel_core::service::Config;
 use fuel_vm::consts::*;
 use fuel_vm::prelude::Opcode;
 use fuels::contract::script::Script;
 use fuels::prelude::*;
-use fuels::signers::wallet::*;
-use fuels::tx::{Address, AssetId, Contract, Input, Output, Receipt, Transaction, UtxoId};
+use fuels::signers::wallet::Wallet;
+use fuels::tx::{Address, AssetId, Contract, Input, Output, Transaction, UtxoId};
 use std::str::FromStr;
 
-#[tokio::test]
-async fn valid_predicate_data_simple() {
-    let amount_to_predicate = 1000;
-    let asset_id = AssetId::default();
-    let predicate_code = std::fs::read("test_projects/predicate_data_simple/out/debug/predicate_data_simple.bin");
-    let predicate_hash = (*Contract::root_from_code(&predicate_code)).into();
-    let secret_key = SecretKey::from_str("0x976e5c3fa620092c718d852ca703b6da9e3075b9f2ecb8ed42d9f746bf26aafb")
-        .unwrap();
+async fn setup() -> (Vec<u8>, Address, Wallet, u64, AssetId) {
+    let predicate_code = std::fs::read("test_projects/predicate_data_simple/out/debug/predicate_data_simple.bin").unwrap();
+    let predicate_address = (*Contract::root_from_code(&predicate_code)).into();
 
-    let mut config = Config::local_node();
-    config.predicates = true;
-    config.utxo_validation = true;
-    let server = FuelService::new_node(config).await.unwrap();
-    let client = FuelClient::from(server.bound_address);
-    let provider = Provider::new(client.clone());
-    let mut wallet = Wallet::new_from_private_key(secret_key, None);
-    wallet.set_provider(provider.clone());
+    let wallet = launch_custom_provider_and_get_single_wallet(Config {
+        predicates: true,
+        utxo_validation: true,
+        ..Config::local_node()
+    })
+    .await;
 
-    let amount = wallet.get_asset_balance(&asset_id).await.unwrap();
+    (
+        predicate_code,
+        predicate_address,
+        wallet,
+        1000,
+        AssetId::default(),
+    )
+}
+
+async fn create_predicate(
+    predicate_address: Address,
+    wallet: &Wallet,
+    amount_to_predicate: u64,
+    asset_id: AssetId,
+) {
     let wallet_coins = wallet
-        .get_asset_inputs_for_amount(asset_id, amount, 0)
+        .get_asset_inputs_for_amount(
+            asset_id,
+            wallet.get_asset_balance(&asset_id).await.unwrap(),
+            0,
+        )
         .await
         .unwrap();
 
-    let output_coin = Output::coin(predicate_hash, amount_to_predicate, asset_id);
-    let mut output_change = Output::change(wallet.address(), 0, asset_id);
-    let script = Opcode::RET(REG_ONE).to_bytes().to_vec();
+    let output_coin = Output::coin(predicate_address, amount_to_predicate, asset_id);
+    let output_change = Output::change(wallet.address(), 0, asset_id);
     let mut tx = Transaction::script(
         1,
         1000000,
         1,
         0,
-        script.clone(),
+        Opcode::RET(REG_ONE).to_bytes().to_vec(),
         vec![],
         wallet_coins,
         vec![output_coin, output_change],
         vec![],
     );
-    let signature = wallet.sign_transaction(&mut tx).await.unwrap();
-    let mut tx_receipts = provider.send_transaction(&tx).await.unwrap();
+    wallet.sign_transaction(&mut tx).await.unwrap();
+    wallet
+        .get_provider()
+        .unwrap()
+        .send_transaction(&tx)
+        .await
+        .unwrap();
+}
 
-    let predicate_data = hex::decode("ef86afa9696cf0dc6385e2c407a6e159a1103cefb7e2ae0636fb33d3cb2a9e4a").unwrap();
-
-    let utxo_predicate_hash = provider
-        .get_spendable_coins(&predicate_hash, asset_id, amount_to_predicate)
+async fn submit_to_predicate(
+    predicate_code: Vec<u8>,
+    predicate_address: Address,
+    wallet: &Wallet,
+    amount_to_predicate: u64,
+    asset_id: AssetId,
+    receiver_address: Address,
+    predicate_data: Vec<u8>,
+) {
+    let utxo_predicate_hash = wallet
+        .get_provider()
+        .unwrap()
+        .get_spendable_coins(&predicate_address, asset_id, amount_to_predicate)
         .await
         .unwrap();
 
     let mut inputs = vec![];
-    let mut tot_amount = 0;
+    let mut total_amount_in_predicate = 0;
 
     for coin in utxo_predicate_hash {
         let input_coin = Input::coin_predicate(
@@ -72,21 +94,12 @@ async fn valid_predicate_data_simple() {
             predicate_data.clone(),
         );
         inputs.push(input_coin);
-        tot_amount += coin.amount.0;
+        total_amount_in_predicate += coin.amount.0;
     }
-    let receiver_address = Address::from_str("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c")
-            .unwrap();
-    let receiver_balance_before = provider
-        .get_asset_balance(&receiver_address, asset_id)
-        .await
-        .unwrap();
 
-    assert_eq!(receiver_balance_before, 0);
-    let output_coin = Output::coin(receiver_address, tot_amount, asset_id);
-
-    let output_change = Output::change(predicate_hash, 0, asset_id);
-
-    let mut new_tx = Transaction::script(
+    let output_coin = Output::coin(receiver_address, total_amount_in_predicate, asset_id);
+    let output_change = Output::change(predicate_address, 0, asset_id);
+    let new_tx = Transaction::script(
         0,
         1000000,
         0,
@@ -99,132 +112,81 @@ async fn valid_predicate_data_simple() {
     );
 
     let script = Script::new(new_tx);
-    let call_result = script.call(&client).await;
+    let _call_result = script.call(&wallet.get_provider().unwrap().client).await;
+}
 
-    let receiver_balance_after = provider
-        .get_asset_balance(&receiver_address, asset_id)
+async fn get_balance(wallet: &Wallet, address: Address, asset_id: AssetId) -> u64 {
+    wallet
+        .get_provider()
+        .unwrap()
+        .get_asset_balance(&address, asset_id)
         .await
-        .unwrap();
+        .unwrap()
+}
 
+#[tokio::test]
+async fn valid_predicate_data_simple() {
+    let predicate_data =
+        hex::decode("ef86afa9696cf0dc6385e2c407a6e159a1103cefb7e2ae0636fb33d3cb2a9e4a").unwrap();
+    let receiver_address =
+        Address::from_str("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c")
+            .unwrap();
+    let (predicate_code, predicate_address, wallet, amount_to_predicate, asset_id) = setup().await;
+
+    create_predicate(predicate_address, &wallet, amount_to_predicate, asset_id).await;
+
+    let receiver_balance_before = get_balance(&wallet, receiver_address, asset_id).await;
+    assert_eq!(receiver_balance_before, 0);
+
+    submit_to_predicate(
+        predicate_code,
+        predicate_address,
+        &wallet,
+        amount_to_predicate,
+        asset_id,
+        receiver_address,
+        predicate_data,
+    )
+    .await;
+
+    let receiver_balance_after = get_balance(&wallet, receiver_address, asset_id).await;
     assert_eq!(
         receiver_balance_before + amount_to_predicate,
         receiver_balance_after
     );
 
-    let predicate_balance = provider
-        .get_asset_balance(&predicate_hash, asset_id)
-        .await
-        .unwrap();
-
+    let predicate_balance = get_balance(&wallet, predicate_address, asset_id).await;
     assert_eq!(predicate_balance, 0);
 }
 
 #[tokio::test]
-#[should_panic(expected = "InvalidPredicate")]
 async fn invalid_predicate_data_simple() {
-    let amount_to_predicate = 1000;
-    let asset_id = AssetId::default();
-    let bin = std::fs::read("test_projects/predicate_data_simple/out/debug/predicate_data_simple.bin");
-    let predicate_hash = (*Contract::root_from_code(&predicate_code)).into();
-    let secret_key = SecretKey::from_str("0x976e5c3fa620092c718d852ca703b6da9e3075b9f2ecb8ed42d9f746bf26aafb")
-        .unwrap();
-
-    let mut config = Config::local_node();
-    config.predicates = true;
-    config.utxo_validation = true;
-    let server = FuelService::new_node(config).await.unwrap();
-    let client = FuelClient::from(server.bound_address);
-    let provider = Provider::new(client.clone());
-    let mut wallet = Wallet::new_from_private_key(secret_key, None);
-    wallet.set_provider(provider.clone());
-
-    let amount = wallet.get_asset_balance(&asset_id).await.unwrap();
-    let wallet_coins = wallet
-        .get_asset_inputs_for_amount(asset_id, amount, 0)
-        .await
-        .unwrap();
-
-    let output_coin = Output::coin(predicate_hash, amount_to_predicate, asset_id);
-    let mut output_change = Output::change(wallet.address(), 0, asset_id);
-    let script = Opcode::RET(REG_ONE).to_bytes().to_vec();
-    let mut tx = Transaction::script(
-        1,
-        1000000,
-        1,
-        0,
-        script.clone(),
-        vec![],
-        wallet_coins,
-        vec![output_coin, output_change],
-        vec![],
-    );
-    let signature = wallet.sign_transaction(&mut tx).await.unwrap();
-    let mut tx_receipts = provider.send_transaction(&tx).await.unwrap();
-
-    let predicate_data = hex::decode("badbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbada").unwrap();
-
-    let utxo_predicate_hash = provider
-        .get_spendable_coins(&predicate_hash, asset_id, amount_to_predicate)
-        .await
-        .unwrap();
-
-    let mut inputs = vec![];
-    let mut tot_amount = 0;
-
-    for coin in utxo_predicate_hash {
-        let input_coin = Input::coin_predicate(
-            UtxoId::from(coin.utxo_id),
-            coin.owner.into(),
-            coin.amount.0,
-            asset_id,
-            0,
-            predicate_code.clone(),
-            predicate_data.clone(),
-        );
-        inputs.push(input_coin);
-        tot_amount += coin.amount.0;
-    }
-    let receiver_address = Address::from_str("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c")
+    let predicate_data =
+        hex::decode("badbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbadbada").unwrap();
+    let receiver_address =
+        Address::from_str("0xde97d8624a438121b86a1956544bd72ed68cd69f2c99555b08b1e8c51ffd511c")
             .unwrap();
-    let receiver_balance_before = provider
-        .get_asset_balance(&receiver_address, asset_id)
-        .await
-        .unwrap();
+    let (predicate_code, predicate_address, wallet, amount_to_predicate, asset_id) = setup().await;
 
+    create_predicate(predicate_address, &wallet, amount_to_predicate, asset_id).await;
+
+    let receiver_balance_before = get_balance(&wallet, receiver_address, asset_id).await;
     assert_eq!(receiver_balance_before, 0);
-    let output_coin = Output::coin(receiver_address, tot_amount, asset_id);
 
-    let output_change = Output::change(predicate_hash, 0, asset_id);
+    submit_to_predicate(
+        predicate_code,
+        predicate_address,
+        &wallet,
+        amount_to_predicate,
+        asset_id,
+        receiver_address,
+        predicate_data,
+    )
+    .await;
 
-    let mut new_tx = Transaction::script(
-        0,
-        1000000,
-        0,
-        0,
-        vec![],
-        vec![],
-        inputs,
-        vec![output_coin, output_change],
-        vec![],
-    );
+    let receiver_balance_after = get_balance(&wallet, receiver_address, asset_id).await;
+    assert_eq!(receiver_balance_before, receiver_balance_after);
 
-    let script = Script::new(new_tx);
-    let call_result = script.call(&client).await;
-
-    let receiver_balance_after = provider
-        .get_asset_balance(&receiver_address, asset_id)
-        .await
-        .unwrap();
-
-    assert_eq!(
-        receiver_balance_before + amount_to_predicate,
-        receiver_balance_after
-    );
-
-    let predicate_balance = provider
-        .get_asset_balance(&predicate_hash, asset_id)
-        .await
-        .unwrap();
-
-    assert_eq!(predicate_balance, 0);
+    let predicate_balance = get_balance(&wallet, predicate_address, asset_id).await;
+    assert_eq!(predicate_balance, amount_to_predicate);
 }
