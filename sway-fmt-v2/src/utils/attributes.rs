feaf3569783d31b32181cdc4db14e64ce09c6b3f
Austin <eureka@Abbys-Air.attlocal.net>
2022-06-20 16:33:06 -0500

    update AttributeDecl::format()

--- sway-fmt-v2/src/utils/attributes.rs
+++ sway-fmt-v2/src/utils/attributes.rs
@@ -2,7 +2,7 @@ use crate::Formatter;
 use sway_parse::attribute::AttributeDecl;
 use sway_types::Spanned;
 
-use super::bracket::SquareBracket;
+use super::bracket::{Parenthesis, SquareBracket};
 
 trait Format {
     fn format(&self, line: &mut String, formatter: &mut Formatter);
@@ -18,13 +18,35 @@ pub fn format_attributes(attributes: Vec<AttributeDecl>, formatter: &mut Formatt
 }
 
 impl Format for AttributeDecl {
-    fn format(&self, line: &mut String, _formatter: &mut Formatter) {
+    fn format(&self, line: &mut String, formatter: &mut Formatter) {
+        // At some point there will be enough attributes to warrant the need
+        // of formatting the list according to `config::lists::ListTactic`.
+        // For now the default implementation will be `Horizontal`.
+        //
+        // `#`
         line.push_str(self.hash_token.span().as_str());
-        Self::open_square_bracket(line, _formatter);
-        // TODO: attributes are joined with their args during `span()`
-        // but we may need to do formatting there eventually
-        line.push_str(self.attribute.span().as_str());
-        Self::close_square_bracket(line, _formatter);
+        // `[`
+        Self::open_square_bracket(line, formatter);
+        let attr = self.attribute.clone().into_inner();
+        // name e.g. `storage`
+        line.push_str(attr.name.span().as_str());
+        // `(`
+        Self::open_parenthesis(line, formatter);
+        // format and add args `read, write`
+        if let Some(args) = attr.args {
+            let mut args = args
+                .into_inner()
+                .value_separator_pairs
+                .iter()
+                .map(|arg| format!("{}{}", arg.0.as_str(), arg.1.span().as_str()))
+                .collect::<Vec<String>>()
+                .join(" ");
+            args.pop(); // pop the ending space
+            args.pop(); // pop the ending comma
+            line.push_str(&args);
+        }
+        // `]\n`
+        Self::close_square_bracket(line, formatter);
     }
 }
 
@@ -34,5 +56,14 @@ impl SquareBracket for AttributeDecl {
     }
     fn close_square_bracket(line: &mut String, _formatter: &mut Formatter) {
         line.push_str("]\n");
+    }
+}
+
+impl Parenthesis for AttributeDecl {
+    fn open_parenthesis(line: &mut String, _formatter: &mut Formatter) {
+        line.push('(')
+    }
+    fn close_parenthesis(line: &mut String, _formatter: &mut Formatter) {
+        line.push(')')
     }
 }
