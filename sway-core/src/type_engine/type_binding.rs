8b874f7d863919d5842575af5d8d4a83ee01a6be
emilyaherbert <emily.herbert@fuel.sh>
2022-06-27 17:59:01 -0500

    Add base TypeMapping type.

---
+++ sway-core/src/type_engine/type_binding.rs
@@ -0,0 +1,85 @@
+use sway_types::{Span, Spanned};
+
+use super::TypeArgument;
+
+/// A `TypeBinding` is the result of using turbofish to bind types to
+/// generic parameters.
+///
+/// For example:
+///
+/// ```ignore
+/// let data = Data::<bool> {
+///   value: true
+/// };
+/// ```
+///
+/// Would produce the type binding (in pseudocode):
+///
+/// ```ignore
+/// TypeBinding {
+///     inner: CallPath(["Data"]),
+///     type_arguments: [bool]
+/// }
+/// ```
+///
+/// ---
+///
+/// Further:
+///
+/// ```ignore
+/// struct Data<T> {
+///   value: T
+/// }
+///
+/// let data1 = Data {
+///   value: true
+/// };
+///
+/// let data2 = Data::<bool> {
+///   value: true
+/// };
+///
+/// let data3: Data<bool> = Data {
+///   value: true
+/// };
+///
+/// let data4: Data<bool> = Data::<bool> {
+///   value: true
+/// };
+/// ```
+///
+/// Each of these 4 examples generates a valid struct expression for `Data`
+/// and passes type checking. But each does so in a unique way:
+/// - `data1` has no type ascription and no type arguments in the `TypeBinding`,
+///     so both are inferred from the value passed to `value`
+/// - `data2` has no type ascription but does have type arguments in the
+///     `TypeBinding`, so the type ascription and type of the value passed to
+///     `value` are both unified to the `TypeBinding`
+/// - `data3` has a type ascription but no type arguments in the `TypeBinding`,
+///     so the type arguments in the `TypeBinding` and the type of the value
+///     passed to `value` are both unified to the type ascription
+/// - `data4` has a type ascription and has type arguments in the `TypeBinding`,
+///     so, with the type from the value passed to `value`, all three are unified
+///     together
+#[derive(Debug, Clone)]
+pub struct TypeBinding<T> {
+    pub inner: T,
+    pub type_arguments: Vec<TypeArgument>,
+    pub span: Span,
+}
+
+impl<T> Spanned for TypeBinding<T> {
+    fn span(&self) -> Span {
+        self.span.clone()
+    }
+}
+
+impl<T> TypeBinding<T> {
+    pub(crate) fn new_with_empty_type_arguments(inner: T, span: Span) -> TypeBinding<T> {
+        TypeBinding {
+            inner,
+            type_arguments: vec![],
+            span,
+        }
+    }
+}
