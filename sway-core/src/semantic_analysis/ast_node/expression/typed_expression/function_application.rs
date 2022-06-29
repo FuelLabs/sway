e95b4cf21e80c8d25258eb37cc20d5fe520af2d7
Joao Matos <joao@tritao.eu>
2022-06-29 16:16:25 +0100

    Fix checking for too many function call arguments.
    This adds explicit checking for the case of too many function call
    arguments in during function application instantiation.
    
    This needs to be done before type checking because after type checking
    we drop the extra function call arguments due to having no type to check
    against.
    
    Also re-enables the should_fail/excess_fn_arguments test.
    
    Closes #2081.

--- sway-core/src/semantic_analysis/ast_node/expression/typed_expression/function_application.rs
+++ sway-core/src/semantic_analysis/ast_node/expression/typed_expression/function_application.rs
@@ -37,6 +37,13 @@ pub(crate) fn instantiate_function_application(
         });
     }
 
+    check!(
+        check_function_arguments_arity(arguments.len(), &function_decl, &call_path),
+        return err(warnings, errors),
+        warnings,
+        errors
+    );
+
     // type check arguments in function application vs arguments in function
     // declaration. Use parameter type annotations as annotations for the
     // arguments
@@ -63,20 +70,15 @@ pub(crate) fn instantiate_function_application(
         .collect();
 
     let span = function_decl.span.clone();
-    let exp = check!(
-        instantiate_function_application_inner(
-            call_path,
-            HashMap::new(),
-            typed_call_arguments,
-            function_decl,
-            None,
-            IsConstant::No,
-            None,
-            span,
-        ),
-        return err(warnings, errors),
-        warnings,
-        errors
+    let exp = instantiate_function_application_inner(
+        call_path,
+        HashMap::new(),
+        typed_call_arguments,
+        function_decl,
+        None,
+        IsConstant::No,
+        None,
+        span,
     );
     ok(exp, warnings, errors)
 }
@@ -92,13 +94,24 @@ pub(crate) fn instantiate_function_application_simple(
     self_state_idx: Option<StateIndex>,
     span: Span,
 ) -> CompileResult<TypedExpression> {
+    let mut warnings = vec![];
+    let mut errors = vec![];
+
+    check!(
+        check_function_arguments_arity(arguments.len(), &function_decl, &call_path),
+        return err(warnings, errors),
+        warnings,
+        errors
+    );
+
     let args_and_names = function_decl
         .parameters
         .iter()
         .zip(arguments.into_iter())
         .map(|(param, arg)| (param.name.clone(), arg))
         .collect::<Vec<(_, _)>>();
-    instantiate_function_application_inner(
+
+    let exp = instantiate_function_application_inner(
         call_path,
         contract_call_params,
         args_and_names,
@@ -107,7 +120,38 @@ pub(crate) fn instantiate_function_application_simple(
         is_constant,
         self_state_idx,
         span,
-    )
+    );
+    ok(exp, warnings, errors)
+}
+
+fn check_function_arguments_arity(
+    arguments_len: usize,
+    function_decl: &TypedFunctionDeclaration,
+    call_path: &CallPath,
+) -> CompileResult<()> {
+    let warnings = vec![];
+    let mut errors = vec![];
+    match arguments_len.cmp(&function_decl.parameters.len()) {
+        std::cmp::Ordering::Equal => ok((), warnings, errors),
+        std::cmp::Ordering::Less => {
+            errors.push(CompileError::TooFewArgumentsForFunction {
+                span: call_path.span(),
+                method_name: function_decl.name.clone(),
+                expected: function_decl.parameters.len(),
+                received: arguments_len,
+            });
+            err(warnings, errors)
+        }
+        std::cmp::Ordering::Greater => {
+            errors.push(CompileError::TooManyArgumentsForFunction {
+                span: call_path.span(),
+                method_name: function_decl.name.clone(),
+                expected: function_decl.parameters.len(),
+                received: arguments_len,
+            });
+            err(warnings, errors)
+        }
+    }
 }
 
 #[allow(clippy::too_many_arguments)]
@@ -121,45 +165,26 @@ fn instantiate_function_application_inner(
     is_constant: IsConstant,
     self_state_idx: Option<StateIndex>,
     span: Span,
-) -> CompileResult<TypedExpression> {
-    let warnings = vec![];
-    let mut errors = vec![];
-    match arguments.len().cmp(&function_decl.parameters.len()) {
-        std::cmp::Ordering::Equal => {
-            let exp = TypedExpression {
-                expression: TypedExpressionVariant::FunctionApplication {
-                    call_path,
-                    contract_call_params,
-                    arguments,
-                    function_body: function_decl.body.clone(),
-                    function_body_name_span: function_decl.name.span(),
-                    function_body_purity: function_decl.purity,
-                    self_state_idx,
-                    selector,
-                },
-                return_type: function_decl.return_type,
-                is_constant,
-                span,
-            };
-            ok(exp, warnings, errors)
-        }
-        std::cmp::Ordering::Less => {
-            errors.push(CompileError::TooFewArgumentsForFunction {
-                span,
-                method_name: function_decl.name,
-                expected: function_decl.parameters.len(),
-                received: arguments.len(),
-            });
-            err(warnings, errors)
-        }
-        std::cmp::Ordering::Greater => {
-            errors.push(CompileError::TooManyArgumentsForFunction {
-                span,
-                method_name: function_decl.name,
-                expected: function_decl.parameters.len(),
-                received: arguments.len(),
-            });
-            err(warnings, errors)
-        }
+) -> TypedExpression {
+    assert_eq!(
+        arguments.len(),
+        function_decl.parameters.len(),
+        "different number of function parameters and arguments"
+    );
+
+    TypedExpression {
+        expression: TypedExpressionVariant::FunctionApplication {
+            call_path,
+            contract_call_params,
+            arguments,
+            function_body: function_decl.body.clone(),
+            function_body_name_span: function_decl.name.span(),
+            function_body_purity: function_decl.purity,
+            self_state_idx,
+            selector,
+        },
+        return_type: function_decl.return_type,
+        is_constant,
+        span,
     }
 }
