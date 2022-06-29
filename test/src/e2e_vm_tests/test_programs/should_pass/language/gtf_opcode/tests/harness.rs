e4f6c34c50b7d498c4d6990b24a79d143b9d8130
Nick Furfaro <nfurfaro33@gmail.com>
2022-06-29 10:00:34 -0600

    test: add initial test skeleton for gtf opcode

---
+++ test/src/e2e_vm_tests/test_programs/should_pass/language/gtf_opcode/tests/harness.rs
@@ -0,0 +1,24 @@
+use fuels::{prelude::*, tx::ContractId};
+
+// Load abi from json
+abigen!(MyContract, "out/debug/gtf_opcode-abi.json");
+
+async fn get_contract_instance() -> (MyContract, ContractId) {
+    // Launch a local network and deploy the contract
+    let wallet = launch_provider_and_get_single_wallet().await;
+
+    let id = Contract::deploy("./out/debug/gtf_opcode.bin", &wallet, TxParameters::default())
+        .await
+        .unwrap();
+
+    let instance = MyContract::new(id.to_string(), wallet);
+
+    (instance, id)
+}
+
+#[tokio::test]
+async fn can_get_contract_id() {
+    let (_instance, _id) = get_contract_instance().await;
+
+    // Now you have an instance of your contract you can use to test each function
+}
