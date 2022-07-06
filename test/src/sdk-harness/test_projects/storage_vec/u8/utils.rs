d9f17e3ba071d3394b101bdeac92aab55f7cf743
SwayStar123 <swayambhanded@gmail.com>
2022-07-06 19:35:54 +0530

    cargo fmt

--- test/src/sdk-harness/test_projects/storage_vec/u8/utils.rs
+++ test/src/sdk-harness/test_projects/storage_vec/u8/utils.rs
@@ -46,12 +46,7 @@ pub mod wrappers {
     }
 
     pub async fn swap_remove(instance: &MyContract, index: u64) -> u8 {
-        instance
-            .u8_swap_remove(index)
-            .call()
-            .await
-            .unwrap()
-            .value
+        instance.u8_swap_remove(index).call().await.unwrap().value
     }
 
     pub async fn insert(instance: &MyContract, index: u64, value: u8) {
