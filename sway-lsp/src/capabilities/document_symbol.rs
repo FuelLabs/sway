c9d4af24c0991cd760b238afe123c9eb54b55ebf
Joshua Batty <joshpbatty@gmail.com>
2022-07-06 14:10:49 +1000

    move token to lsp_types::SymbolKind functions to document_symbol file

--- sway-lsp/src/capabilities/document_symbol.rs
+++ sway-lsp/src/capabilities/document_symbol.rs
@@ -1,12 +1,18 @@
 use crate::core::{
     session::Session,
-    symbol_kind,
-    token::{TokenMap, TokenType},
+    token::{TokenMap, TokenType, AstToken, TypedAstToken},
 };
 use crate::utils::common::get_range_from_span;
 use std::sync::Arc;
+use sway_core::{
+    semantic_analysis::ast_node::{
+        expression::typed_expression_variant::TypedExpressionVariant, TypedDeclaration,
+    },
+    Declaration, Expression, Literal,
+};
 use sway_types::{Ident, Spanned};
-use tower_lsp::lsp_types::{DocumentSymbolResponse, Location, SymbolInformation, Url};
+use tower_lsp::lsp_types::{DocumentSymbolResponse, Location, SymbolInformation, SymbolKind, Url};
+
 
 pub fn document_symbol(session: Arc<Session>, url: Url) -> Option<DocumentSymbolResponse> {
     session
@@ -33,13 +39,107 @@ fn create_symbol_info(ident: &Ident, token: &TokenType, url: Url) -> SymbolInfor
         name: ident.as_str().to_string(),
         kind: {
             match &token.typed {
-                Some(typed_token) => symbol_kind::typed_to_symbol_kind(&typed_token),
-                None => symbol_kind::parsed_to_symbol_kind(&token.parsed),
+                Some(typed_token) => typed_to_symbol_kind(&typed_token),
+                None => parsed_to_symbol_kind(&token.parsed),
             }
         },
         location: Location::new(url, range),
         tags: None,
         container_name: None,
         deprecated: None,
+    }
+}
+
+fn parsed_to_symbol_kind(ast_token: &AstToken) -> SymbolKind {
+    match ast_token {
+        AstToken::Declaration(dec) => {
+            match dec {
+                Declaration::VariableDeclaration(_) => SymbolKind::VARIABLE,
+                Declaration::FunctionDeclaration(_) => SymbolKind::FUNCTION,
+                Declaration::TraitDeclaration(_) => SymbolKind::INTERFACE,
+                Declaration::StructDeclaration(_) => SymbolKind::STRUCT,
+                Declaration::EnumDeclaration(_) => SymbolKind::ENUM,
+                Declaration::ConstantDeclaration(_) => SymbolKind::CONSTANT,
+                Declaration::ImplTrait { .. } => SymbolKind::INTERFACE,
+                Declaration::AbiDeclaration(_) => SymbolKind::INTERFACE,
+                // currently we return `variable` type as default
+                Declaration::Reassignment(_)
+                | Declaration::ImplSelf { .. }
+                | Declaration::StorageDeclaration(_)
+                | Declaration::Break { .. }
+                | Declaration::Continue { .. } => SymbolKind::VARIABLE,
+            }
+        }
+        AstToken::Expression(exp) => {
+            match &exp {
+                Expression::Literal { value, .. } => match value {
+                    Literal::String(_) => SymbolKind::STRING,
+                    Literal::Boolean(_) => SymbolKind::BOOLEAN,
+                    _ => SymbolKind::NUMBER,
+                },
+                Expression::FunctionApplication { .. } => SymbolKind::FUNCTION,
+                Expression::VariableExpression { .. } => SymbolKind::VARIABLE,
+                Expression::Array { .. } => SymbolKind::ARRAY,
+                Expression::StructExpression { .. } => SymbolKind::STRUCT,
+                // currently we return `variable` type as default
+                _ => SymbolKind::VARIABLE,
+            }
+        }
+        AstToken::FunctionDeclaration(_) => SymbolKind::FUNCTION,
+        AstToken::FunctionParameter(_) => SymbolKind::TYPE_PARAMETER,
+        AstToken::StructField(_) => SymbolKind::FIELD,
+        AstToken::EnumVariant(_) => SymbolKind::ENUM_MEMBER,
+        AstToken::TraitFn(_) => SymbolKind::FUNCTION,
+        AstToken::StorageField(_) => SymbolKind::FIELD,
+        AstToken::Reassignment(_) => SymbolKind::VARIABLE,
+    }
+}
+
+fn typed_to_symbol_kind(typed_ast_token: &TypedAstToken) -> SymbolKind {
+    match typed_ast_token {
+        TypedAstToken::TypedDeclaration(dec) => {
+            match dec {
+                TypedDeclaration::VariableDeclaration(_) => SymbolKind::VARIABLE,
+                TypedDeclaration::ConstantDeclaration(_) => SymbolKind::CONSTANT,
+                TypedDeclaration::FunctionDeclaration(_) => SymbolKind::FUNCTION,
+                TypedDeclaration::TraitDeclaration(_) => SymbolKind::INTERFACE,
+                TypedDeclaration::StructDeclaration(_) => SymbolKind::STRUCT,
+                TypedDeclaration::EnumDeclaration(_) => SymbolKind::ENUM,
+                TypedDeclaration::ImplTrait { .. } => SymbolKind::INTERFACE,
+                TypedDeclaration::AbiDeclaration(_) => SymbolKind::INTERFACE,
+                TypedDeclaration::GenericTypeForFunctionScope { .. } => SymbolKind::TYPE_PARAMETER,
+                // currently we return `variable` type as default
+                TypedDeclaration::Reassignment(_)
+                | TypedDeclaration::ErrorRecovery
+                | TypedDeclaration::StorageDeclaration(_)
+                | TypedDeclaration::StorageReassignment(_)
+                | TypedDeclaration::Break { .. }
+                | TypedDeclaration::Continue { .. } => SymbolKind::VARIABLE,
+            }
+        }
+        TypedAstToken::TypedExpression(exp) => {
+            match &exp.expression {
+                TypedExpressionVariant::Literal(lit) => match lit {
+                    Literal::String(_) => SymbolKind::STRING,
+                    Literal::Boolean(_) => SymbolKind::BOOLEAN,
+                    _ => SymbolKind::NUMBER,
+                },
+                TypedExpressionVariant::FunctionApplication { .. } => SymbolKind::FUNCTION,
+                TypedExpressionVariant::VariableExpression { .. } => SymbolKind::VARIABLE,
+                TypedExpressionVariant::Array { .. } => SymbolKind::ARRAY,
+                TypedExpressionVariant::StructExpression { .. } => SymbolKind::STRUCT,
+                TypedExpressionVariant::StructFieldAccess { .. } => SymbolKind::FIELD,
+                // currently we return `variable` type as default
+                _ => SymbolKind::VARIABLE,
+            }
+        }
+        TypedAstToken::TypedFunctionDeclaration(_) => SymbolKind::FUNCTION,
+        TypedAstToken::TypedFunctionParameter(_) => SymbolKind::TYPE_PARAMETER,
+        TypedAstToken::TypedStructField(_) => SymbolKind::FIELD,
+        TypedAstToken::TypedEnumVariant(_) => SymbolKind::ENUM_MEMBER,
+        TypedAstToken::TypedTraitFn(_) => SymbolKind::FUNCTION,
+        TypedAstToken::TypedStorageField(_) => SymbolKind::FIELD,
+        TypedAstToken::TypeCheckedStorageReassignDescriptor(_)
+        | TypedAstToken::TypedReassignment(_) => SymbolKind::VARIABLE,
     }
 }
--- sway-lsp/src/core/mod.rs
+++ sway-lsp/src/core/mod.rs
@@ -1,6 +1,5 @@
 pub mod document;
 pub mod session;
-pub(crate) mod symbol_kind;
 pub(crate) mod token;
 pub(crate) mod traverse_parse_tree;
 pub(crate) mod traverse_typed_tree;
