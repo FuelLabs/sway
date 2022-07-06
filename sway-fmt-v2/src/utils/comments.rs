9dc606a9721b5ff1fe25c58e85e409687727410a
kayagokalp <kaya.gokalp@fuel.sh>
2022-07-06 16:35:41 +0300

    Handle in-block and next-to item cases for comment map generation

--- sway-fmt-v2/src/utils/comments.rs
+++ sway-fmt-v2/src/utils/comments.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use std::{cmp::Ordering, collections::BTreeMap, sync::Arc};
-use sway_parse::token::{lex_commented, Comment, CommentedTokenTree};
+use sway_parse::token::{lex_commented, Comment, CommentedTokenTree, CommentedTree};
 
 /// Represents a span for the comments in a spesific file
 /// A stripped down version of sway-types::src::Span
@@ -41,17 +41,32 @@ pub fn construct_comment_map(input: Arc<str>) -> Result<CommentMap> {
     let tts = commented_token_stream.token_trees().iter();
 
     for comment in tts {
-        if let CommentedTokenTree::Comment(comment) = comment {
+        get_comment_from_token_stream(comment, &mut comment_map);
+    }
+    Ok(comment_map)
+}
+
+/// Get `Comment` from the token stream and insert it with its span to the `CommentMap`.
+/// Handles both the standalone and in-block comments.
+fn get_comment_from_token_stream(
+    commented_token_tree: &CommentedTokenTree,
+    comment_map: &mut CommentMap,
+) {
+    match commented_token_tree {
+        CommentedTokenTree::Comment(comment) => {
             let comment_span = CommentSpan {
                 start: comment.span.start(),
                 end: comment.span.end(),
             };
             comment_map.insert(comment_span, comment.clone());
         }
-        // TODO: implement CommentedTokenTree::Tree case for getting the comments inside code
-        // blocks
+        CommentedTokenTree::Tree(CommentedTree::Group(group)) => {
+            for item in group.token_stream.token_trees().iter() {
+                get_comment_from_token_stream(item, comment_map);
+            }
+        }
+        _ => {}
     }
-    Ok(comment_map)
 }
 
 #[cfg(test)]
@@ -62,8 +77,8 @@ mod tests {
     #[test]
     fn test_comment_span_ordering() {
         let first_span = CommentSpan { start: 2, end: 4 };
-        let second_span  = CommentSpan { start: 2, end: 6 };
-        let third_span  = CommentSpan { start: 4, end: 7 };
+        let second_span = CommentSpan { start: 2, end: 6 };
+        let third_span = CommentSpan { start: 4, end: 7 };
 
         let mut vec = vec![second_span.clone(), third_span.clone(), first_span.clone()];
         vec.sort();
@@ -71,13 +86,13 @@ mod tests {
         assert_eq!(vec[0], first_span);
         assert_eq!(vec[1], second_span);
         assert_eq!(vec[2], third_span);
-
     }
 
     #[test]
-    fn test_comment_span_map() {
+    fn test_comment_span_map_standalone_comment() {
         let input = r#"
         // Single-line comment.
+        let var = 256; // This is a comment.
         struct Foo {
             /* multi-
              * line-
@@ -94,5 +109,61 @@ mod tests {
             .last()
             .unwrap();
         assert_eq!(found_comment.1.span.as_str(), "// Single-line comment.");
+    }
+    #[test]
+    fn test_comment_span_map_standalone_next_to_item() {
+        let input = r#"
+        // Single-line comment.
+        let var = 256; // This is a comment.
+        struct Foo {
+            /* multi-
+             * line-
+             * comment */
+            bar: i32,
+        }
+        "#;
+        let map = construct_comment_map(Arc::from(input)).unwrap();
+        assert!(!map.is_empty());
+        let range_start_span = CommentSpan { start: 40, end: 54 };
+        let range_end_span = CommentSpan {
+            start: 100,
+            end: 115,
+        };
+        let found_comment = map
+            .range((Included(range_start_span), Included(range_end_span)))
+            .last()
+            .unwrap();
+        assert_eq!(found_comment.1.span.as_str(), "// This is a comment.");
+    }
+    #[test]
+    fn test_comment_span_map_standalone_inside_block() {
+        let input = r#"
+        // Single-line comment.
+        let var = 256; // This is a comment.
+        struct Foo {
+            /* multi-
+             * line-
+             * comment */
+            bar: i32,
+        }
+        "#;
+        let map = construct_comment_map(Arc::from(input)).unwrap();
+        assert!(!map.is_empty());
+        let range_start_span = CommentSpan {
+            start: 110,
+            end: 116,
+        };
+        let range_end_span = CommentSpan {
+            start: 200,
+            end: 201,
+        };
+        let found_comment = map
+            .range((Included(range_start_span), Included(range_end_span)))
+            .last()
+            .unwrap();
+        assert_eq!(
+            found_comment.1.span.as_str(),
+            "/* multi-\n             * line-\n             * comment *"
+        );
     }
 }
