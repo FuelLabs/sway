a6b6ab863dcae549f257f4ff2de80fa38a962e84
SwayStar123 <swayambhanded@gmail.com>
2022-07-09 20:17:08 +0530

    updated tests with preconditional tests

--- test/src/sdk-harness/test_projects/storage_vec/u8/mod.rs
+++ test/src/sdk-harness/test_projects/storage_vec/u8/mod.rs
@@ -5,14 +5,14 @@ use utils::{
     wrappers::{clear, get, insert, is_empty, len, pop, push, remove, swap_remove},
 };
 
-// TODO: Many of these tests have no preconditional assertions because options are not supported
-// by the SDK
+// TODO: Replace many of the get calls with direct storage values
+// once the SDK supports that
 
 mod success {
     use super::*;
 
     #[tokio::test]
-    async fn can_push() {
+    async fn can_get() {
         let (instance, _id) = get_contract_instance().await;
 
         push(&instance, 50).await;
@@ -22,13 +22,34 @@ mod success {
     }
 
     #[tokio::test]
+    async fn can_push() {
+        let (instance, _id) = get_contract_instance().await;
+
+        let len_before_push = len(&instance).await;
+        assert_eq!(len_before_push, 0);
+
+        push(&instance, 50).await;
+        let item = get(&instance, 0).await;
+
+        assert_eq!(item, 50);
+
+        let len_after_push = len(&instance).await;
+        assert_eq!(len_after_push, 1);
+    }
+
+    #[tokio::test]
     async fn can_pop() {
         let (instance, _id) = get_contract_instance().await;
 
         push(&instance, 50).await;
-        let item = pop(&instance).await;
+        let len_before_pop = len(&instance).await;
+        assert_eq!(len_before_pop, 1);
 
+        let item = pop(&instance).await;
         assert_eq!(item, 50);
+
+        let len_after_pop = len(&instance).await;
+        assert_eq!(len_after_pop, 0);
     }
 
     #[tokio::test]
@@ -39,9 +60,16 @@ mod success {
         push(&instance, 100).await;
         push(&instance, 150).await;
         push(&instance, 200).await;
+
+        let old_item_at_index = get(&instance, 2).await;
+        assert_eq!(150, old_item_at_index);
+
         let item = remove(&instance, 2).await;
 
         assert_eq!(item, 150);
+
+        let new_item_at_index = get(&instance, 2).await;
+        assert_eq!(new_item_at_index, 200);
     }
 
     #[tokio::test]
@@ -52,6 +80,10 @@ mod success {
         push(&instance, 100).await;
         push(&instance, 150).await;
         push(&instance, 200).await;
+
+        let old_item_at_index = get(&instance, 1).await;
+        assert_eq!(old_item_at_index, 100);
+
         let item = swap_remove(&instance, 1).await;
         let new_item_at_index = get(&instance, 1).await;
 
@@ -68,11 +100,14 @@ mod success {
         push(&instance, 150).await;
         push(&instance, 200).await;
 
+        let old_item_at_index = get(&instance, 1).await;
+        assert_eq!(100, old_item_at_index);
+
         insert(&instance, 1, 250).await;
 
         let new_item_at_index = get(&instance, 1).await;
-
         assert_eq!(new_item_at_index, 250);
+
         assert_eq!(100, get(&instance, 2).await);
         assert_eq!(150, get(&instance, 3).await);
         assert_eq!(200, get(&instance, 4).await);
@@ -103,6 +138,27 @@ mod success {
 
     #[tokio::test]
     async fn can_confirm_emptiness() {
+        let (instance, _id) = get_contract_instance().await;
+
+        let isempty = is_empty(&instance).await;
+        assert_eq!(isempty, true);
+
+        push(&instance, 50).await;
+        push(&instance, 100).await;
+        push(&instance, 150).await;
+        push(&instance, 200).await;
+
+        let isempty = is_empty(&instance).await;
+        assert_eq!(isempty, false);
+
+        clear(&instance).await;
+
+        let isempty = is_empty(&instance).await;
+        assert_eq!(isempty, true);
+    }
+
+    #[tokio::test]
+    async fn can_clear() {
         let (instance, _id) = get_contract_instance().await;
 
         let isempty = is_empty(&instance).await;
