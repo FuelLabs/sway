9e467e3a5094faf027e4f342372ee52505368f88
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2022-08-05 18:09:29 +0200

    add inputs for utxo validation

--- test/src/sdk-harness/test_projects/logging/mod.rs
+++ test/src/sdk-harness/test_projects/logging/mod.rs
@@ -1,4 +1,3 @@
-use fuel_core::service::{Config, FuelService};
 use fuels::contract::script::Script;
 use fuels::prelude::*;
 use fuels::tx::{ConsensusParameters, Transaction};
@@ -8,17 +7,14 @@ use hex;
 async fn run_valid() {
     let bin = std::fs::read("test_projects/logging/out/debug/logging.bin");
 
-    let wallet = LocalWallet::new_random(None);
-    let coins = setup_single_asset_coins(
-        wallet.address(),
-        BASE_ASSET_ID,
-        1,
-        1,
-    );
+    let wallet = launch_provider_and_get_wallet().await;
 
-    let (provider, _address) = setup_test_provider(coins.clone(), None).await;
+    let inputs = wallet
+        .get_asset_inputs_for_amount(BASE_ASSET_ID, DEFAULT_SPENDABLE_COIN_AMOUNT, 0)
+        .await
+        .unwrap();
 
-    let tx = Transaction::Script {
+    let mut tx = Transaction::Script {
         gas_price: 0,
         gas_limit: ConsensusParameters::DEFAULT.max_gas_per_tx,
         maturity: 0,
@@ -26,17 +22,20 @@ async fn run_valid() {
         receipts_root: Default::default(),
         script: bin.unwrap(),
         script_data: vec![],
-        inputs: vec![],
+        inputs,
         outputs: vec![],
-        witnesses: vec![vec![].into()],
+        witnesses: vec![],
         metadata: None,
     };
 
-    let script = Script::new(tx);
-    let receipts = script.call(&provider).await.unwrap();
+    wallet.sign_transaction(&mut tx).await.unwrap();
+
+    let provider = wallet.get_provider().unwrap();
+
+    let receipts = Script::new(tx).call(&provider).await.unwrap();
 
     let correct_hex =
         hex::decode("ef86afa9696cf0dc6385e2c407a6e159a1103cefb7e2ae0636fb33d3cb2a9e4a");
 
-    assert!(correct_hex.unwrap() == receipts[0].data().unwrap());
+    assert_eq!(correct_hex.unwrap(), receipts[0].data().unwrap());
 }
