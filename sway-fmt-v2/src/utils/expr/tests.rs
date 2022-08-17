cea143fbd260e79c1ae75e2efd168e928fd81b48
Chris O'Brien <obrchr96@gmail.com>
2022-08-17 18:54:38 -0500

    complete intermediate_whitespace test

--- sway-fmt-v2/src/utils/expr/tests.rs
+++ sway-fmt-v2/src/utils/expr/tests.rs
@@ -117,7 +117,7 @@ fmt_test!(  multiline_match_stmt "match foo {\n    Foo::foo => {}\n    Foo::bar 
 );
 
 fmt_test!(  if_else_block "if foo {\n    foo();\n} else if bar {\n    bar();\n} else {\n    baz();\n}",
-            intermediate_whitespace "if foo {\n    foo();\n} else if bar {\n    bar();\n} else {\n    baz();\n}"
+            intermediate_whitespace "   if    foo  {   \n       foo( ) ; \n }    else  if   bar  { \n     bar( ) ; \n }  else  { \n    baz(\n) ; \n }\n\n"
 );
 
 fmt_test!(  match_branch_kind
