a980f758b3ff04b4cba17efb9d072f36c382ec8d
Vaivaswatha Nagaraj <vaivaswatha.nagaraj@fuel.sh>
2022-08-25 17:40:52 +0530

    Update ir-to-asm tests

--- sway-core/tests/ir_to_asm/simple_enum.asm
+++ sway-core/tests/ir_to_asm/simple_enum.asm
@@ -9,20 +9,19 @@ move $r2 $sp                  ; save locals base register
 cfei i16                      ; allocate 16 bytes for all locals
 move $r1 $sp                  ; save register for temporary stack value
 cfei i16                      ; allocate 16 bytes for temporary struct
-lw   $r0 data_0               ; literal instantiation
-sw   $r1 $r0 i0               ; insert_value @ 0
+sw   $r1 $one i0              ; insert_value @ 0
 addi $r0 $r2 i0               ; get offset reg for get_ptr
 addi $r0 $r2 i0               ; get store offset
 mcpi $r0 $r1 i16              ; store value
 addi $r0 $r2 i0               ; get offset reg for get_ptr
 move $r1 $sp                  ; save register for temporary stack value
 cfei i16                      ; allocate 16 bytes for temporary struct
-lw   $r0 data_1               ; literal instantiation
+lw   $r0 data_0               ; literal instantiation
 sw   $r1 $r0 i0               ; insert_value @ 0
-lw   $r0 data_2               ; literal instantiation
+lw   $r0 data_1               ; literal instantiation
 sw   $r1 $r0 i1               ; insert_value @ 1
 ret  $zero                    ; returning unit as zero
+noop                          ; word-alignment of data section
 .data:
-data_0 .u64 0x01
-data_1 .u64 0x02
-data_2 .u64 0x03
+data_0 .u64 0x02
+data_1 .u64 0x03
