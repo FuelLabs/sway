5649bf9ee703715a32851ac23edcf341f8371cc5
emilyaherbert <emily.herbert@fuel.sh>
2022-08-26 16:07:21 -0500

    fmt

--- sway-core/src/declaration_engine/mod.rs
+++ sway-core/src/declaration_engine/mod.rs
@@ -2,14 +2,14 @@
 
 //! The [DeclarationEngine](declaration_engine::DeclarationEngine) allows the compiler add a layer of
 //! separation between [AST nodes](crate::semantic_analysis::ast_node) and declarations.
-//! 
+//!
 //! As an interface, you can think of the [DeclarationEngine](declaration_engine::DeclarationEngine)
 //! as a mapping from [DeclarationId](declaration_id::DeclarationId) to
 //! [DeclarationWrapper](declaration_wrapper::DeclarationWrapper). When a
 //! [DeclarationWrapper](declaration_wrapper::DeclarationWrapper) is inserted into the
 //! [DeclarationEngine](declaration_engine::DeclarationEngine), a [DeclarationId](declaration_id::DeclarationId)
 //! is generated, which is then used to refer to the declaration.
-//! 
+//!
 //! Monomorphized copies of declarations are added to [DeclarationEngine](declaration_engine::DeclarationEngine).
 
 #[allow(clippy::module_inception)]
