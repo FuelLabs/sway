c5ebbbac9a323b9d3396e1b67d25f6a34c12d824
Joshua Batty <joshpbatty@gmail.com>
2022-09-13 14:25:13 +1000

    clean up

--- sway-lsp/src/core/mod.rs
+++ sway-lsp/src/core/mod.rs
@@ -1,6 +1,6 @@
-pub mod document;
+pub(crate) mod document;
 pub(crate) mod parse_items;
-pub mod session;
+pub(crate) mod session;
 pub(crate) mod token;
 pub(crate) mod traverse_parse_tree;
 pub(crate) mod traverse_typed_tree;
