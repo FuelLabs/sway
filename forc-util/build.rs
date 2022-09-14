33ab737728aa5c9c4364c5b7ccfbc57237d4aa7e
bingcicle <bingcicle@proton.me>
2022-09-14 12:41:04 +0800

    more appropriate error message for vergen

--- forc-util/build.rs
+++ forc-util/build.rs
@@ -6,5 +6,5 @@ fn main() {
     *config.git_mut().sha_kind_mut() = ShaKind::Short;
     *config.git_mut().commit_timestamp_mut() = true;
     *config.git_mut().commit_timestamp_kind_mut() = TimestampKind::DateOnly;
-    vergen(config).expect("Error executing vergen")
+    vergen(config).expect("Failed to configure vergen")
 }
