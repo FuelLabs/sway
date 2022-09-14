1c7d65f6c5a7f444c48ebf62e2548ba0a67004fd
bingcicle <bingcicle@proton.me>
2022-09-14 10:51:01 +0800

    feat: include vergen and build.rs in forc-util

---
+++ forc-util/build.rs
@@ -0,0 +1,10 @@
+use vergen::{vergen, Config, ShaKind, TimestampKind};
+
+fn main() {
+    let mut config = Config::default();
+
+    *config.git_mut().sha_kind_mut() = ShaKind::Short;
+    *config.git_mut().commit_timestamp_mut() = true;
+    *config.git_mut().commit_timestamp_kind_mut() = TimestampKind::DateOnly;
+    vergen(config).expect("Error executing vergen")
+}
