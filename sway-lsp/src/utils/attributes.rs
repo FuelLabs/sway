dba4a599c99c2188cc6779ee7d57ca4d63583496
Joshua Batty <joshpbatty@gmail.com>
2022-09-19 10:52:31 +1000

    update attributes helper methods to take a Token instead of the whole TokenMap

--- sway-lsp/src/utils/attributes.rs
+++ sway-lsp/src/utils/attributes.rs
@@ -1,39 +1,29 @@
 #![allow(dead_code)]
-use crate::{
-    core::token::{AstToken, TokenMap},
-    utils::token::to_ident_key,
-};
+use crate::core::token::{AstToken, Token};
 use sway_core::{Attribute, AttributeKind, AttributesMap, Declaration};
-use sway_types::Ident;
 
-pub(crate) fn attributes_map(decl_ident: &Ident, tokens: &TokenMap) -> Option<AttributesMap> {
-    if let Some(item) = tokens.get(&to_ident_key(decl_ident)) {
-        let token = item.value();
-        return match &token.parsed {
-            AstToken::Declaration(declaration) => match declaration {
-                Declaration::EnumDeclaration(decl) => Some(decl.attributes.clone()),
-                Declaration::FunctionDeclaration(decl) => Some(decl.attributes.clone()),
-                Declaration::StructDeclaration(decl) => Some(decl.attributes.clone()),
-                Declaration::ConstantDeclaration(decl) => Some(decl.attributes.clone()),
-                Declaration::StorageDeclaration(decl) => Some(decl.attributes.clone()),
-                _ => None,
-            },
-            AstToken::StorageField(field) => Some(field.attributes.clone()),
-            AstToken::StructField(field) => Some(field.attributes.clone()),
-            AstToken::TraitFn(trait_fn) => Some(trait_fn.attributes.clone()),
-            AstToken::EnumVariant(variant) => Some(variant.attributes.clone()),
+pub(crate) fn attributes_map(token: &Token) -> Option<AttributesMap> {
+    match &token.parsed {
+        AstToken::Declaration(declaration) => match declaration {
+            Declaration::EnumDeclaration(decl) => Some(decl.attributes.clone()),
+            Declaration::FunctionDeclaration(decl) => Some(decl.attributes.clone()),
+            Declaration::StructDeclaration(decl) => Some(decl.attributes.clone()),
+            Declaration::ConstantDeclaration(decl) => Some(decl.attributes.clone()),
+            Declaration::StorageDeclaration(decl) => Some(decl.attributes.clone()),
             _ => None,
-        };
+        },
+        AstToken::StorageField(field) => Some(field.attributes.clone()),
+        AstToken::StructField(field) => Some(field.attributes.clone()),
+        AstToken::TraitFn(trait_fn) => Some(trait_fn.attributes.clone()),
+        AstToken::EnumVariant(variant) => Some(variant.attributes.clone()),
+        _ => None,
     }
-    None
 }
 
-pub(crate) fn doc_attributes(decl_ident: &Ident, tokens: &TokenMap) -> Option<Vec<Attribute>> {
-    attributes_map(decl_ident, tokens)
-        .and_then(|mut attributes| attributes.remove(&AttributeKind::Doc))
+pub(crate) fn doc_attributes(token: &Token) -> Option<Vec<Attribute>> {
+    attributes_map(token).and_then(|mut attributes| attributes.remove(&AttributeKind::Doc))
 }
 
-pub(crate) fn storage_attributes(decl_ident: &Ident, tokens: &TokenMap) -> Option<Vec<Attribute>> {
-    attributes_map(decl_ident, tokens)
-        .and_then(|mut attributes| attributes.remove(&AttributeKind::Storage))
+pub(crate) fn storage_attributes(token: &Token) -> Option<Vec<Attribute>> {
+    attributes_map(token).and_then(|mut attributes| attributes.remove(&AttributeKind::Storage))
 }
