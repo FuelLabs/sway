33d857f40a28dcc9ace94eca7addc572957fe0e4
PixelCircuits <circuitpixels@gmail.com>
2022-09-19 22:39:04 -0400

    Ran formatter

--- test/src/sdk-harness/test_projects/tx_fields/mod.rs
+++ test/src/sdk-harness/test_projects/tx_fields/mod.rs
@@ -1,10 +1,10 @@
+use fuel_vm::consts::REG_ONE;
+use fuel_vm::fuel_asm::Opcode;
 use fuel_vm::fuel_crypto::Hasher;
 use fuel_vm::fuel_tx::ConsensusParameters;
 use fuel_vm::fuel_tx::Transaction as FuelTransaction;
-use fuel_vm::fuel_asm::Opcode;
-use fuel_vm::consts::REG_ONE;
 use fuels::prelude::*;
-use fuels::tx::{Bytes32, ContractId, Contract as TxContract, Input as TxInput, TxPointer, UtxoId};
+use fuels::tx::{Bytes32, Contract as TxContract, ContractId, Input as TxInput, TxPointer, UtxoId};
 use std::str::FromStr;
 
 abigen!(
@@ -16,7 +16,8 @@ async fn get_contracts() -> (TxContractTest, ContractId, WalletUnlocked) {
     let mut provider_config = Config::local_node();
     provider_config.predicates = true;
     let wallet_config = WalletsConfig::new(Some(1), None, None);
-    let mut wallets = launch_custom_provider_and_get_wallets(wallet_config, Some(provider_config)).await;
+    let mut wallets =
+        launch_custom_provider_and_get_wallets(wallet_config, Some(provider_config)).await;
     let wallet = wallets.pop().unwrap();
 
     let contract_id = Contract::deploy(
@@ -34,7 +35,11 @@ async fn get_contracts() -> (TxContractTest, ContractId, WalletUnlocked) {
     (instance, contract_id.into(), wallet)
 }
 
-async fn generate_coin_predicate_input(amount: u64, data: Vec<u8>, wallet: &WalletUnlocked) -> (Vec<u8>, TxInput) {
+async fn generate_coin_predicate_input(
+    amount: u64,
+    data: Vec<u8>,
+    wallet: &WalletUnlocked,
+) -> (Vec<u8>, TxInput) {
     let mut predicate_bytecode = Opcode::RET(REG_ONE).to_bytes().to_vec();
     predicate_bytecode.append(&mut predicate_bytecode.clone());
     predicate_bytecode.append(&mut predicate_bytecode.clone());
@@ -54,7 +59,10 @@ async fn generate_coin_predicate_input(amount: u64, data: Vec<u8>, wallet: &Wall
         .await
         .unwrap();
 
-    let predicate_coin = &provider.get_coins(&predicate_root.into(), AssetId::default()).await.unwrap()[0];
+    let predicate_coin = &provider
+        .get_coins(&predicate_root.into(), AssetId::default())
+        .await
+        .unwrap()[0];
     let predicate_coin = TxInput::CoinPredicate {
         utxo_id: UtxoId::from(predicate_coin.utxo_id.clone()),
         owner: Address::from(predicate_coin.owner.clone()),
@@ -149,11 +157,7 @@ async fn can_get_input_count() {
     let (contract_instance, _, _) = get_contracts().await;
     let inputs_count = 2;
 
-    let result = contract_instance
-        .get_input_count()
-        .call()
-        .await
-        .unwrap();
+    let result = contract_instance.get_input_count().call().await.unwrap();
     assert_eq!(result.value, inputs_count);
 }
 
@@ -162,11 +166,7 @@ async fn can_get_output_count() {
     let (contract_instance, _, _) = get_contracts().await;
     let outputs_count = 2;
 
-    let result = contract_instance
-        .get_output_count()
-        .call()
-        .await
-        .unwrap();
+    let result = contract_instance.get_output_count().call().await.unwrap();
     assert_eq!(result.value, outputs_count);
 }
 
@@ -256,11 +256,7 @@ async fn can_get_input_type() {
 async fn can_get_input_owner() {
     let (contract_instance, _, wallet) = get_contracts().await;
 
-    let owner_result = contract_instance
-        .get_input_owner(1)
-        .call()
-        .await
-        .unwrap();
+    let owner_result = contract_instance.get_input_owner(1).call().await.unwrap();
     assert_eq!(owner_result.value, wallet.address().into());
 
     //TODO: add test for inputmessage
@@ -271,11 +267,7 @@ async fn can_get_input_amount() {
     let (contract_instance, _, _) = get_contracts().await;
     let amount = 1000000000;
 
-    let amount_result = contract_instance
-        .get_input_amount(1)
-        .call()
-        .await
-        .unwrap();
+    let amount_result = contract_instance.get_input_amount(1).call().await.unwrap();
     assert_eq!(amount_result.value, amount);
 
     //TODO: add test for inputmessage
@@ -286,11 +278,7 @@ async fn can_get_input_asset() {
     let (contract_instance, _, _) = get_contracts().await;
     // TODO set this to a non-zero value once SDK supports setting maturity.
 
-    let asset_result = contract_instance
-        .get_input_asset(1)
-        .call()
-        .await
-        .unwrap();
+    let asset_result = contract_instance.get_input_asset(1).call().await.unwrap();
     assert_eq!(asset_result.value, ContractId::zeroed());
 
     //TODO: add test for inputmessage
@@ -303,7 +291,11 @@ async fn can_get_input_maturity() {
     // TODO set this to a non-zero value once SDK supports setting maturity.
     let maturity = 0;
 
-    let result = contract_instance.get_input_maturity(1).call().await.unwrap();
+    let result = contract_instance
+        .get_input_maturity(1)
+        .call()
+        .await
+        .unwrap();
     assert_eq!(result.value, maturity);
 }
 
@@ -313,7 +305,11 @@ async fn can_get_input_witness_index() {
     // TODO set this to a non-zero value once SDK supports multiple witnesses.
     let witness_index = 0;
 
-    let result = contract_instance.get_input_witness_index(1).call().await.unwrap();
+    let result = contract_instance
+        .get_input_witness_index(1)
+        .call()
+        .await
+        .unwrap();
     assert_eq!(result.value, witness_index);
 
     //TODO: add test for inputmessage
@@ -323,7 +319,8 @@ async fn can_get_input_witness_index() {
 async fn can_get_input_predicate_length() {
     let (contract_instance, _, wallet) = get_contracts().await;
     let provider = wallet.get_provider().unwrap();
-    let (predicate_bytecode, predicate_coin) = generate_coin_predicate_input(100, vec![], &wallet).await;
+    let (predicate_bytecode, predicate_coin) =
+        generate_coin_predicate_input(100, vec![], &wallet).await;
 
     // Add predicate coin to inputs and call contract
     let call_handler = contract_instance.get_input_predicate_length(2);
@@ -331,7 +328,9 @@ async fn can_get_input_predicate_length() {
     if let FuelTransaction::Script { inputs, .. } = &mut script.tx {
         inputs.push(predicate_coin)
     }
-    let result = call_handler.get_response(script.call(provider).await.unwrap()).unwrap();
+    let result = call_handler
+        .get_response(script.call(provider).await.unwrap())
+        .unwrap();
     assert_eq!(result.value, predicate_bytecode.len() as u16);
 
     //TODO: add test for inputmessage
@@ -341,7 +340,8 @@ async fn can_get_input_predicate_length() {
 async fn can_get_input_predicate() {
     let (contract_instance, _, wallet) = get_contracts().await;
     let provider = wallet.get_provider().unwrap();
-    let (predicate_bytecode, predicate_coin) = generate_coin_predicate_input(100, vec![], &wallet).await;
+    let (predicate_bytecode, predicate_coin) =
+        generate_coin_predicate_input(100, vec![], &wallet).await;
     let predicate_bytes: [u8; 32] = predicate_bytecode.try_into().unwrap();
 
     // Add predicate coin to inputs and call contract
@@ -350,7 +350,9 @@ async fn can_get_input_predicate() {
     if let FuelTransaction::Script { inputs, .. } = &mut script.tx {
         inputs.push(predicate_coin)
     }
-    let result = call_handler.get_response(script.call(provider).await.unwrap()).unwrap();
+    let result = call_handler
+        .get_response(script.call(provider).await.unwrap())
+        .unwrap();
     assert_eq!(result.value, predicate_bytes);
 
     //TODO: add test for inputmessage
@@ -362,7 +364,8 @@ async fn can_get_input_predicate_data_length() {
     let provider = wallet.get_provider().unwrap();
     let predicate_data_bytes: [u8; 8] = [1, 2, 3, 4, 5, 6, 7, 8];
     let predicate_data = predicate_data_bytes.to_vec();
-    let (_, predicate_coin) = generate_coin_predicate_input(100, predicate_data.clone(), &wallet).await;
+    let (_, predicate_coin) =
+        generate_coin_predicate_input(100, predicate_data.clone(), &wallet).await;
 
     // Add predicate coin to inputs and call contract
     let call_handler = contract_instance.get_input_predicate_data_length(2);
@@ -370,7 +373,9 @@ async fn can_get_input_predicate_data_length() {
     if let FuelTransaction::Script { inputs, .. } = &mut script.tx {
         inputs.push(predicate_coin)
     }
-    let result = call_handler.get_response(script.call(provider).await.unwrap()).unwrap();
+    let result = call_handler
+        .get_response(script.call(provider).await.unwrap())
+        .unwrap();
     assert_eq!(result.value, predicate_data.len() as u16);
 
     //TODO: add test for inputmessage
@@ -380,9 +385,13 @@ async fn can_get_input_predicate_data_length() {
 async fn can_get_input_predicate_data() {
     let (contract_instance, _, wallet) = get_contracts().await;
     let provider = wallet.get_provider().unwrap();
-    let predicate_data_bytes: [u8; 32] = Bytes32::from_str("49a7e58dc8b6397a25de9090cb50e71c2588c773487d1da7066d0c7198c567c0").unwrap().into();
+    let predicate_data_bytes: [u8; 32] =
+        Bytes32::from_str("49a7e58dc8b6397a25de9090cb50e71c2588c773487d1da7066d0c7198c567c0")
+            .unwrap()
+            .into();
     let predicate_data = predicate_data_bytes.to_vec();
-    let (_, predicate_coin) = generate_coin_predicate_input(100, predicate_data.clone(), &wallet).await;
+    let (_, predicate_coin) =
+        generate_coin_predicate_input(100, predicate_data.clone(), &wallet).await;
 
     // Add predicate coin to inputs and call contract
     let call_handler = contract_instance.get_input_predicate_data(2);
@@ -390,7 +399,9 @@ async fn can_get_input_predicate_data() {
     if let FuelTransaction::Script { inputs, .. } = &mut script.tx {
         inputs.push(predicate_coin)
     }
-    let result = call_handler.get_response(script.call(provider).await.unwrap()).unwrap();
+    let result = call_handler
+        .get_response(script.call(provider).await.unwrap())
+        .unwrap();
     assert_eq!(result.value, predicate_data_bytes);
 
     //TODO: add test for inputmessage
@@ -399,18 +410,10 @@ async fn can_get_input_predicate_data() {
 #[tokio::test]
 async fn can_get_output_type() {
     let (contract_instance, _, _) = get_contracts().await;
-    let result = contract_instance
-        .get_output_type(0)
-        .call()
-        .await
-        .unwrap();
+    let result = contract_instance.get_output_type(0).call().await.unwrap();
     assert_eq!(result.value, Output::Contract());
 
-    let result = contract_instance
-        .get_output_type(1)
-        .call()
-        .await
-        .unwrap();
+    let result = contract_instance.get_output_type(1).call().await.unwrap();
     assert_eq!(result.value, Output::Change());
 }
 
