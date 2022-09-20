b43a1d10a16a5d36157b075fc5f6b28061c64527
JoshuaBatty <joshpbatty@gmail.com>
2022-09-20 12:36:39 +1000

    use unwrap_or instead of match

--- sway-lsp/src/capabilities/hover.rs
+++ sway-lsp/src/capabilities/hover.rs
@@ -63,28 +63,25 @@ fn hover_format(token: &Token, ident: &Ident) -> Hover {
                 }
                 TypedDeclaration::FunctionDeclaration(func) => extract_fn_signature(&func.span()),
                 TypedDeclaration::StructDeclaration(decl_id) => {
-                    match declaration_engine::de_get_struct(decl_id.clone(), &decl.span()) {
-                        Ok(struct_decl) => {
+                    declaration_engine::de_get_struct(decl_id.clone(), &decl.span())
+                        .map(|struct_decl| {
                             format_visibility_hover(struct_decl.visibility, decl.friendly_name())
-                        }
-                        Err(_) => token_name,
-                    }
+                        })
+                        .unwrap_or(token_name)
                 }
                 TypedDeclaration::TraitDeclaration(ref decl_id) => {
-                    match declaration_engine::de_get_trait(decl_id.clone(), &decl.span()) {
-                        Ok(trait_decl) => {
+                    declaration_engine::de_get_trait(decl_id.clone(), &decl.span())
+                        .map(|trait_decl| {
                             format_visibility_hover(trait_decl.visibility, decl.friendly_name())
-                        }
-                        Err(_) => token_name,
-                    }
+                        })
+                        .unwrap_or(token_name)
                 }
                 TypedDeclaration::EnumDeclaration(decl_id) => {
-                    match declaration_engine::de_get_enum(decl_id.clone(), &decl.span()) {
-                        Ok(enum_decl) => {
+                    declaration_engine::de_get_enum(decl_id.clone(), &decl.span())
+                        .map(|enum_decl| {
                             format_visibility_hover(enum_decl.visibility, decl.friendly_name())
-                        }
-                        Err(_) => token_name,
-                    }
+                        })
+                        .unwrap_or(token_name)
                 }
                 _ => token_name,
             },
