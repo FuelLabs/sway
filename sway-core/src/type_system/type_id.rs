0ec6df81937e06e94254196e1b1b65e1979d4185
emilyaherbert <emily.herbert@fuel.sh>
2022-09-22 12:21:09 -0500

    Unrelated clippy changed because clippy updated.

--- sway-core/src/type_system/type_id.rs
+++ sway-core/src/type_system/type_id.rs
@@ -133,10 +133,10 @@ impl TypeId {
         match look_up_type_id(*self) {
             TypeInfo::Enum {
                 type_parameters, ..
-            } => (!type_parameters.is_empty()).then(|| type_parameters),
+            } => (!type_parameters.is_empty()).then_some(type_parameters),
             TypeInfo::Struct {
                 type_parameters, ..
-            } => (!type_parameters.is_empty()).then(|| type_parameters),
+            } => (!type_parameters.is_empty()).then_some(type_parameters),
             _ => None,
         }
     }
