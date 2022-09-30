49b9e7e5a3975d8443920d86df8af26fe62d2733
Alex Hansen <alex@alex-hansen.com>
2022-09-29 18:03:49 -0700

    Remove dead docstrings crate (#2901)
    This is just dead code from a long time ago.
    
    Co-authored-by: Alex Hansen <alex@quickr.us>
    Co-authored-by: Mohammad Fawaz <mohammadfawaz89@gmail.com>

--- docstrings/src/documenter/source_code_module.rs
+++
@@ -1,9 +0,0 @@
-/// Represents a Sway module and its contents as a string.
-pub struct SourceCodeModule {
-    /// The name of a module.
-    /// e.g. if module `root` contains a submodule `foo`, this would be
-    /// vec!["root", "foo"]
-    name: Vec<String>,
-    /// The raw source code contained in the module.
-    source: String,
-}
