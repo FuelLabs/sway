3a09606fb7b5c76d82e3442f177261c10a525ac9
emilyaherbert <emily.herbert@fuel.sh>
2022-10-11 18:11:29 -0500

    Remove the noop TyReturnStatement type.

--- sway-core/src/language/ty/expression/expression_variant.rs
+++ sway-core/src/language/ty/expression/expression_variant.rs
@@ -11,8 +11,8 @@ use crate::{
     semantic_analysis::{
         ContractCallParams, ProjectionKind, TyAsmRegisterDeclaration, TyCodeBlock,
         TyEnumDeclaration, TyEnumVariant, TyIntrinsicFunctionKind, TyReassignment,
-        TyReturnStatement, TyStorageReassignment, TyStructExpressionField, TyStructField,
-        TypeCheckedStorageAccess, VariableMutability,
+        TyStorageReassignment, TyStructExpressionField, TyStructField, TypeCheckedStorageAccess,
+        VariableMutability,
     },
     type_system::*,
     TyFunctionDeclaration,
@@ -129,7 +129,7 @@ pub enum TyExpressionVariant {
     Continue,
     Reassignment(Box<TyReassignment>),
     StorageReassignment(Box<TyStorageReassignment>),
-    Return(Box<TyReturnStatement>),
+    Return(Box<TyExpression>),
 }
 
 // NOTE: Hash and PartialEq must uphold the invariant:
@@ -582,8 +582,8 @@ impl fmt::Display for TyExpressionVariant {
                 };
                 format!("storage reassignment to {}", place)
             }
-            TyExpressionVariant::Return(stmt) => {
-                format!("return {}", stmt.expr)
+            TyExpressionVariant::Return(exp) => {
+                format!("return {}", *exp)
             }
         };
         write!(f, "{}", s)
