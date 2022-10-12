c4298b74cf3b117677fb0e376a88339b640dbb76
Mohammad Fawaz <mohammadfawaz89@gmail.com>
2022-10-12 11:59:55 -0400

    Add tests for Option and Result in the ABI

---
+++ test/src/sdk-harness/test_projects/option_in_abi/src/main.sw
@@ -0,0 +1,61 @@
+contract;
+
+struct MyStruct {
+    first_field: Option<Address>,
+    second_field: u64,
+}
+
+enum MyEnum {
+    FirstVariant: Option<Address>,
+    SecondVariant: u64,
+}
+
+abi MyContract {
+    fn bool_test(input: Option<bool>) -> Option<bool>;
+    fn u8_test(input: Option<u8>) -> Option<u8>;
+    fn u16_test(input: Option<u16>) -> Option<u16>;
+    fn u32_test(input: Option<u32>) -> Option<u32>;
+    fn u64_test(input: Option<u64>) -> Option<u64>;
+    fn b256_test(input: Option<b256>) -> Option<b256>;
+    fn struct_test(input: Option<MyStruct>) -> Option<MyStruct>;
+    fn tuple_test(input: Option<(Option<Address>, u64)>) -> Option<(Option<Address>, u64)>;
+    fn enum_test(input: Option<MyEnum>) -> Option<MyEnum>;
+    fn array_test(input: Option<[Option<Address>; 3]>) -> Option<[Option<Address>; 3]>;
+    fn string_test(input: Option<str[4]>) -> Option<str[4]>;
+}
+
+impl MyContract for Contract {
+    fn bool_test(input: Option<bool>) -> Option<bool> {
+        input
+    }
+    fn u8_test(input: Option<u8>) -> Option<u8> {
+        input
+    }
+    fn u16_test(input: Option<u16>) -> Option<u16> {
+        input
+    }
+    fn u32_test(input: Option<u32>) -> Option<u32> {
+        input
+    }
+    fn u64_test(input: Option<u64>) -> Option<u64> {
+        input
+    }
+    fn b256_test(input: Option<b256>) -> Option<b256> {
+        input
+    }
+    fn struct_test(input: Option<MyStruct>) -> Option<MyStruct> {
+        input
+    }
+    fn tuple_test(input: Option<(Option<Address>, u64)>) -> Option<(Option<Address>, u64)> {
+        input
+    }
+    fn enum_test(input: Option<MyEnum>) -> Option<MyEnum> {
+        input
+    }
+    fn array_test(input: Option<[Option<Address>; 3]>) -> Option<[Option<Address>; 3]> {
+        input
+    }
+    fn string_test(input: Option<str[4]>) -> Option<str[4]> {
+        input
+    }
+}
---
+++ test/src/sdk-harness/test_projects/result_in_abi/mod.rs
@@ -0,0 +1,275 @@
+use fuels::prelude::*;
+use std::str::FromStr;
+
+abigen!(
+    ResultInAbiTestContract,
+    "test_projects/result_in_abi/out/debug/result_in_abi-abi.json"
+);
+
+async fn get_result_in_abi_instance() -> (ResultInAbiTestContract, ContractId) {
+    let wallet = launch_provider_and_get_wallet().await;
+    let id = Contract::deploy(
+        "test_projects/result_in_abi/out/debug/result_in_abi.bin",
+        &wallet,
+        TxParameters::default(),
+        StorageConfiguration::with_storage_path(Some(
+            "test_projects/result_in_abi/out/debug/result_in_abi-storage_slots.json".to_string(),
+        )),
+    )
+    .await
+    .unwrap();
+    let instance = ResultInAbiTestContract::new(id.to_string(), wallet);
+
+    (instance, id.into())
+}
+
+#[tokio::test]
+async fn test_bool() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(true);
+    let response = contract_methods.bool_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok(false);
+    let response = contract_methods.bool_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.bool_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_u8() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(42);
+    let response = contract_methods.u8_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.u8_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_u16() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(42);
+    let response = contract_methods.u16_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.u16_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_u32() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(42);
+    let response = contract_methods.u32_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.u32_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_u64() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(42);
+    let response = contract_methods.u64_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.u64_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_b256() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(Bits256([1u8; 32]));
+    let response = contract_methods.b256_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.b256_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_struct() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(MyStruct {
+        first_field: Ok(Address::from_str(
+            "0x4242424242424242424242424242424242424242424242424242424242424242",
+        )
+        .unwrap()),
+        second_field: 42,
+    });
+    let response = contract_methods.struct_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok(MyStruct {
+        first_field: Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+        second_field: 42,
+    });
+    let response = contract_methods.struct_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.struct_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_tuple() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok((
+        Ok(
+            Address::from_str("0x4242424242424242424242424242424242424242424242424242424242424242")
+                .unwrap(),
+        ),
+        42,
+    ));
+    let response = contract_methods.tuple_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok((
+        Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+        42,
+    ));
+    let response = contract_methods.tuple_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.tuple_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_enum() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok(MyEnum::FirstVariant(Ok(Address::from_str(
+        "0x4242424242424242424242424242424242424242424242424242424242424242",
+    )
+    .unwrap())));
+    let response = contract_methods.enum_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok(MyEnum::FirstVariant(Err(SomeError::SomeErrorString(
+        "error".try_into().unwrap(),
+    ))));
+    let response = contract_methods.enum_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok(MyEnum::SecondVariant(42));
+    let response = contract_methods.enum_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.enum_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_array() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok([
+        Ok(
+            Address::from_str("0x4242424242424242424242424242424242424242424242424242424242424242")
+                .unwrap(),
+        ),
+        Ok(
+            Address::from_str("0x6969696969696969696969696969696969696969696969696969696969696969")
+                .unwrap(),
+        ),
+        Ok(
+            Address::from_str("0x9999999999999999999999999999999999999999999999999999999999999999")
+                .unwrap(),
+        ),
+    ]);
+    let response = contract_methods.array_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok([
+        Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+        Ok(
+            Address::from_str("0x6969696969696969696969696969696969696969696969696969696969696969")
+                .unwrap(),
+        ),
+        Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+    ]);
+    let response = contract_methods.array_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Ok([
+        Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+        Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+        Err(SomeError::SomeErrorString("error".try_into().unwrap())),
+    ]);
+    let response = contract_methods.array_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.array_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_string() -> Result<(), Error> {
+    let (instance, _id) = get_result_in_abi_instance().await;
+    let contract_methods = instance.methods();
+
+    let input = Ok("fuel".try_into().unwrap());
+    let response = contract_methods.string_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    let input = Err(SomeError::SomeErrorString("error".try_into().unwrap()));
+    let response = contract_methods.string_test(input.clone()).call().await?;
+    assert_eq!(input, response.value);
+
+    Ok(())
+}
