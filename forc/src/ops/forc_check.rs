2ea20a569cae0d4fc4f0463d7f064efd521e6f0f
Kaya Gokalp <kaya.gokalp@fuel.sh>
2022-10-28 01:36:22 +0300

    refactor: forc_check directly constructs a ManifestFile

--- forc/src/ops/forc_check.rs
+++ forc/src/ops/forc_check.rs
@@ -1,6 +1,6 @@
 use crate::cli::CheckCommand;
 use anyhow::Result;
-use forc_pkg::{self as pkg, PackageManifestFile};
+use forc_pkg::{self as pkg};
 use pkg::manifest::ManifestFile;
 use std::path::PathBuf;
 use sway_core::{language::ty, CompileResult};
@@ -18,8 +18,7 @@ pub fn check(command: CheckCommand) -> Result<CompileResult<ty::TyProgram>> {
     } else {
         std::env::current_dir()?
     };
-    let manifest = PackageManifestFile::from_dir(&this_dir)?;
-    let manifest_file = ManifestFile::Package(Box::new(manifest));
+    let manifest_file = ManifestFile::from_dir(&this_dir)?;
     let plan = pkg::BuildPlan::from_lock_and_manifest(&manifest_file, locked, offline)?;
 
     Ok(pkg::check(&plan, terse_mode)?.flat_map(|(_, tp)| CompileResult::new(tp, vec![], vec![])))
