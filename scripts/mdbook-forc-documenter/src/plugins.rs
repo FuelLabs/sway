95b255fbc350334ae407e1b61ecbeb0b0b53ae0e
bingcicle <bingcicle@proton.me>
2022-10-28 16:10:36 +0800

    add a note regarding plugin discovery in the CI vs locally

--- scripts/mdbook-forc-documenter/src/plugins.rs
+++ scripts/mdbook-forc-documenter/src/plugins.rs
@@ -1,22 +1,27 @@
 use anyhow::{anyhow, Result};
 use std::{collections::HashSet, process};
 
+/// Detects plugins available via `PATH`.
+///
+/// Note that plugin discovery works reliably within the Sway CI since the installed plugins are in
+/// a controlled environment. Building the book locally on your own machine may create a different
+/// book depending on what plugins are available on your PATH.
 pub(crate) fn forc_plugins_from_path() -> Result<Vec<String>> {
     let output = process::Command::new("forc")
         .arg("plugins")
         .output()
         .expect("Failed running forc plugins");
-    let mut plugins = HashSet::new();
 
     if !output.status.success() {
         return Err(anyhow!("Failed to run forc plugins"));
     }
 
+    let mut plugins = HashSet::new();
     let s = String::from_utf8_lossy(&output.stdout) + String::from_utf8_lossy(&output.stderr);
 
     for plugin in s.lines() {
-        if let Some(command) = plugin.split_once("forc-") {
-            plugins.insert(command.1.to_string());
+        if let Some(("", command)) = plugin.split_once("forc-") {
+            plugins.insert(command.to_string());
         }
     }
 
