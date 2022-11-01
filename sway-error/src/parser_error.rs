38718c3aaa3a8729daa0cf49baa067ba49b419df
marcos henrich <marcoshenrich@gmail.com>
2022-11-01 12:14:17 +0000

    Improves CannotDocCommentDepToken error msg.

--- sway-error/src/parser_error.rs
+++ sway-error/src/parser_error.rs
@@ -8,7 +8,7 @@ pub enum ParseErrorKind {
     ExpectedImportNameGroupOrGlob,
     #[error("Expected an item.")]
     ExpectedAnItem,
-    #[error("Cannot doc comment a dependency.")]
+    #[error("Cannot attach a docstring to a dependency.")]
     CannotDocCommentDepToken,
     #[error("Expected a comma or closing parenthesis in function arguments.")]
     ExpectedCommaOrCloseParenInFnArgs,
