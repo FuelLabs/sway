34f09a3b65f876c41622c40fb29c19fadd9eb606
green <xgreenx9999@gmail.com>
2022-11-01 00:27:55 +0000

    Fix serialization

--- forc-plugins/forc-client/src/ops/tx_util.rs
+++ forc-plugins/forc-client/src/ops/tx_util.rs
@@ -173,6 +173,7 @@ impl<Tx: Buildable + SerializableVec + field::Witnesses + Send> TransactionBuild
             let witness = Witness::from(signature.as_ref());
             tx.replace_witness(signature_witness_index, witness);
         }
+        tx.precompute();
 
         Ok(tx)
     }
