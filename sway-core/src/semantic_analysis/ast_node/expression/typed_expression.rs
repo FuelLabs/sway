3776ec64b9b098be0d75b19573c884ee4525633b
emilyaherbert <emily.herbert@fuel.sh>
2022-11-02 18:57:16 -0500

    Doc comments and clippy.

--- sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
+++ sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
@@ -87,7 +87,7 @@ impl ty::TyExpression {
             warnings,
             errors
         );
-        let return_type = method.return_type.clone();
+        let return_type = method.return_type;
         let args_and_names = method
             .parameters
             .into_iter()
