970c81af0e2dedaa20d678111ef563c4518e9a51
emilyaherbert <emily.herbert@fuel.sh>
2022-11-03 17:38:26 -0600

    Use spans for generic types in function scopes and error recovery declarations.

--- sway-core/src/ir_generation/compile.rs
+++ sway-core/src/ir_generation/compile.rs
@@ -159,7 +159,7 @@ fn compile_declarations(
             | ty::TyDeclaration::AbiDeclaration(_)
             | ty::TyDeclaration::GenericTypeForFunctionScope { .. }
             | ty::TyDeclaration::StorageDeclaration(_)
-            | ty::TyDeclaration::ErrorRecovery => (),
+            | ty::TyDeclaration::ErrorRecovery(_) => (),
         }
     }
     Ok(())
