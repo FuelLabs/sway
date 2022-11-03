79fc8f0fd96f3f2b9e1efbd483b02c6daa8bda2f
mitchmindtree <mitchell.nordine@fuel.sh>
2022-11-03 21:13:08 +1100

    Run cargo fmt on sway-core asm_gen programs abstract.rs

--- sway-core/src/asm_generation/programs/abstract.rs
+++ sway-core/src/asm_generation/programs/abstract.rs
@@ -168,10 +168,9 @@ impl AbstractProgram {
             };
 
             // Put the selector in the data section.
-            let data_label = self.data_section.insert_data_value(Entry::new_word(
-                u32::from_be_bytes(selector) as u64,
-                None,
-            ));
+            let data_label = self
+                .data_section
+                .insert_data_value(Entry::new_word(u32::from_be_bytes(selector) as u64, None));
 
             // Load the data into a register for comparison.
             asm_buf.ops.push(AllocatedAbstractOp {
