4d86993c3efeb0f8a6822855ec1f91ea3d1073fe
Toby Hutton <toby@grusly.com>
2022-11-11 19:47:17 +1100

    Introduce code relocation to avoid the need for far jumps.
    The existing solution for far jumps which can't fit in an immediate is
    to convert them to dynamic jumps, storing the destination address in the
    data section.
    
    This change helps avoid far jumps altogether by moving large blocks to
    beyond the limit to keep immediate mode control flow.

--- sway-core/src/asm_generation/allocated_abstract_instruction_set.rs
+++ sway-core/src/asm_generation/allocated_abstract_instruction_set.rs
@@ -12,6 +12,17 @@ use std::collections::{BTreeSet, HashMap, HashSet};
 
 use either::Either;
 
+// Convenience type for representing a map from a label to its offset and number of instructions
+// following it until the next label (i.e., the length of the basic block).
+pub(crate) type LabeledBlocks = HashMap<Label, BasicBlock>;
+
+#[derive(Clone, Copy, Debug)]
+pub(crate) struct BasicBlock {
+    pub(crate) offs: u64,
+    pub(crate) abstract_len: usize,
+    pub(crate) final_len: u64,
+}
+
 #[derive(Clone)]
 pub struct AllocatedAbstractInstructionSet {
     pub(crate) ops: Vec<AllocatedAbstractOp>,
@@ -156,12 +167,143 @@ impl AllocatedAbstractInstructionSet {
         self
     }
 
+    /// Relocate code to keep as much control flow below the magic 1MB address space accessible by
+    /// a `JNZI` instruction.
+    pub(crate) fn relocate_control_flow(mut self, data_section: &DataSection) -> Self {
+        // Do an analysis pass, gathering basic block offsets and whether any jumps are going to be
+        // a problem.
+        let (has_far_jumps, furthest_offset, blocks) = self.map_label_offsets(data_section);
+
+        if !has_far_jumps {
+            return self;
+        }
+
+        // Sort the blocks by _final_ size, biggest first.
+        let mut sorted_blocks = blocks.into_iter().collect::<Vec<_>>();
+        sorted_blocks.sort_unstable_by(|l, r| l.1.final_len.cmp(&r.1.final_len).reverse());
+
+        let reduction_target = furthest_offset - consts::EIGHTEEN_BITS;
+
+        // We create a map of blocks to move but storing the _abstract_ size of the block.
+        let mut instructions_to_move_count = 0;
+        let blocks_to_move: HashMap<Label, usize> = HashMap::from_iter(
+            sorted_blocks
+                .into_iter()
+                .take_while(|(_lab, blk)| {
+                    let keep_going = instructions_to_move_count < reduction_target;
+                    instructions_to_move_count += blk.final_len;
+                    keep_going
+                })
+                .map(|(lab, blk)| (lab, blk.abstract_len)),
+        );
+
+        // This is all very imperative, but we're trying to keep it efficient.  We expect the
+        // number of instructions in the `moved_ops` list to be `instructions_to_move_count` plus a
+        // label op for each block which is moved.
+        let mut new_ops: Vec<AllocatedAbstractOp> = Vec::with_capacity(self.ops.len());
+        let mut moved_ops: Vec<AllocatedAbstractOp> =
+            Vec::with_capacity(blocks_to_move.len() + instructions_to_move_count as usize);
+
+        // Use a large number for our new moved labels, one which shouldn't be in the existing ops.
+        // This is a little bit hacky. :/
+        let mut new_label_idx = self.ops.len();
+
+        let mut read_idx = 0;
+        while read_idx < self.ops.len() {
+            // Get the next op and copy it over.
+            let op = &self.ops[read_idx];
+            new_ops.push(op.clone());
+            read_idx += 1;
+
+            // Check if this is a label and one for a block we want to move...
+            if let Either::Right(ControlFlowOp::Label(ref cur_label)) = op.opcode {
+                if let Some(count) = blocks_to_move.get(cur_label) {
+                    // The count in the set includes the label.
+                    let count = *count as usize - 1;
+
+                    // We want to move this block.  First add a new label to the moved list for us
+                    // to jump to.
+                    let moved_block_label = Label(new_label_idx);
+                    new_label_idx += 1;
+                    moved_ops.push(AllocatedAbstractOp {
+                        opcode: Either::Right(ControlFlowOp::Label(moved_block_label)),
+                        comment: String::new(),
+                        owning_span: None,
+                    });
+
+                    // Now add all the instruction in this block to the moved list.
+                    moved_ops.extend_from_slice(&self.ops[read_idx..(read_idx + count)]);
+
+                    // Replace the block with a jump to the new label.
+                    new_ops.push(AllocatedAbstractOp {
+                        opcode: Either::Right(ControlFlowOp::Jump(moved_block_label)),
+                        comment: String::new(),
+                        owning_span: None,
+                    });
+
+                    // Move the `read_idx` to beyond the block we just moved.
+                    read_idx += count;
+
+                    // Now check this next op.  If it's a label then we need the moved block to
+                    // jump back to it.  If it's an unconditional jump then it can also be moved to
+                    // the end of the moved block.  Conditional jumps must not be moved so we add a
+                    // new label to jump back to.  I.e., we need to terminate the moved block.
+                    match &self.ops[read_idx].opcode {
+                        Either::Right(ControlFlowOp::Label(return_lab)) => {
+                            // Do _not_ increment read_idx; we need to copy this label to the
+                            // `new_ops` next iteration.
+                            moved_ops.push(AllocatedAbstractOp {
+                                opcode: Either::Right(ControlFlowOp::Jump(*return_lab)),
+                                comment: String::new(),
+                                owning_span: None,
+                            })
+                        }
+
+                        Either::Right(ControlFlowOp::JumpIfNotEq(..))
+                        | Either::Right(ControlFlowOp::JumpIfNotZero(..)) => {
+                            // Also don't increment read_idx to let it be copied next iteration.
+                            let jump_back_label = Label(new_label_idx);
+                            new_label_idx += 1;
+                            new_ops.push(AllocatedAbstractOp {
+                                opcode: Either::Right(ControlFlowOp::Label(moved_block_label)),
+                                comment: String::new(),
+                                owning_span: None,
+                            });
+                            moved_ops.push(AllocatedAbstractOp {
+                                opcode: Either::Right(ControlFlowOp::Label(jump_back_label)),
+                                comment: String::new(),
+                                owning_span: None,
+                            });
+                        }
+
+                        Either::Right(ControlFlowOp::Jump(_)) => {
+                            // Here we _do_ increment read_idx as we want this instruction to be
+                            // counted as a part of the moved block.
+                            moved_ops.push(self.ops[read_idx].clone());
+                            read_idx += 1;
+                        }
+
+                        _otherwise => unreachable!(
+                            "We should not have basic blocks terminated by any other instruction.\
+                            See map_label_offsets() below."
+                        ),
+                    }
+                }
+            }
+        }
+
+        new_ops.append(&mut moved_ops);
+        self.ops = new_ops;
+
+        self
+    }
+
     /// Runs two passes -- one to get the instruction offsets of the labels and one to replace the
     /// labels in the organizational ops
     pub(crate) fn realize_labels(
         mut self,
         data_section: &mut DataSection,
-    ) -> Result<(RealizedAbstractInstructionSet, HashMap<Label, u64>), crate::CompileError> {
+    ) -> Result<(RealizedAbstractInstructionSet, LabeledBlocks), crate::CompileError> {
         let label_offsets = self.resolve_labels(data_section, 0)?;
 
         let mut realized_ops = vec![];
@@ -180,7 +322,7 @@ impl AllocatedAbstractInstructionSet {
                 Either::Right(org_op) => match org_op {
                     ControlFlowOp::Jump(ref lab) | ControlFlowOp::Call(ref lab) => {
                         let imm = VirtualImmediate24::new_unchecked(
-                            *label_offsets.get(lab).unwrap(),
+                            label_offsets.get(lab).unwrap().offs,
                             "Programs with more than 2^24 labels are unsupported right now",
                         );
                         realized_ops.push(RealizedOp {
@@ -191,7 +333,7 @@ impl AllocatedAbstractInstructionSet {
                     }
                     ControlFlowOp::JumpIfNotEq(r1, r2, ref lab) => {
                         let imm = VirtualImmediate12::new_unchecked(
-                            *label_offsets.get(lab).unwrap(),
+                            label_offsets.get(lab).unwrap().offs,
                             "Programs with more than 2^12 labels are unsupported right now",
                         );
                         realized_ops.push(RealizedOp {
@@ -202,7 +344,7 @@ impl AllocatedAbstractInstructionSet {
                     }
                     ControlFlowOp::JumpIfNotZero(r1, ref lab) => {
                         let imm = VirtualImmediate18::new_unchecked(
-                            *label_offsets.get(lab).unwrap(),
+                            label_offsets.get(lab).unwrap().offs,
                             "Programs with more than 2^18 labels are unsupported right now",
                         );
                         realized_ops.push(RealizedOp {
@@ -213,7 +355,7 @@ impl AllocatedAbstractInstructionSet {
                     }
                     ControlFlowOp::MoveAddress(r1, ref lab) => {
                         let imm = VirtualImmediate18::new_unchecked(
-                            *label_offsets.get(lab).unwrap(),
+                            label_offsets.get(lab).unwrap().offs,
                             "Programs with more than 2^18 labels are unsupported right now",
                         );
                         realized_ops.push(RealizedOp {
@@ -230,7 +372,7 @@ impl AllocatedAbstractInstructionSet {
                         });
                     }
                     ControlFlowOp::LoadLabel(r1, ref lab) => {
-                        let offset = *label_offsets.get(lab).unwrap();
+                        let offset = label_offsets.get(lab).unwrap().offs;
                         let data_id = data_section.insert_data_value(Entry::new_word(offset, None));
                         realized_ops.push(RealizedOp {
                             opcode: AllocatedOpcode::LWDataId(r1, data_id),
@@ -256,7 +398,7 @@ impl AllocatedAbstractInstructionSet {
         &mut self,
         data_section: &mut DataSection,
         iter_count: usize,
-    ) -> Result<HashMap<Label, u64>, crate::CompileError> {
+    ) -> Result<LabeledBlocks, crate::CompileError> {
         // Iteratively resolve the label offsets.
         //
         // For very large programs the label offsets may be too large to fit in an immediate jump
@@ -280,7 +422,7 @@ impl AllocatedAbstractInstructionSet {
             ));
         }
 
-        let (remap_needed, label_offsets) = self.map_label_offsets(data_section);
+        let (remap_needed, _, label_offsets) = self.map_label_offsets(data_section);
 
         if !remap_needed || !self.rewrite_far_jumps(&label_offsets) {
             // We didn't need to make any changes to the ops, so the labels are now correct.
@@ -291,9 +433,10 @@ impl AllocatedAbstractInstructionSet {
         }
     }
 
-    fn map_label_offsets(&self, data_section: &DataSection) -> (bool, HashMap<Label, u64>) {
-        let mut label_offsets = HashMap::new();
+    fn map_label_offsets(&self, data_section: &DataSection) -> (bool, u64, LabeledBlocks) {
+        let mut labelled_blocks = LabeledBlocks::new();
         let mut cur_offset = 0;
+        let mut cur_basic_block = None;
 
         // We decide here whether remapping the jumps _may_ be necessary.  We assume that if any
         // label is further than 18bits then we'll probably have to remap JNZIs, and we track JNEIs
@@ -301,12 +444,35 @@ impl AllocatedAbstractInstructionSet {
         let mut furthest_offset = 0;
         let mut jnei_labels = HashSet::new();
 
-        for op in &self.ops {
+        use ControlFlowOp::*;
+
+        for (op_idx, op) in self.ops.iter().enumerate() {
+            // If we're seeing a control flow op then it's the end of the block.
+            if matches!(
+                op.opcode,
+                Either::Right(Label(_) | Jump(_) | JumpIfNotEq(..) | JumpIfNotZero(..))
+            ) {
+                if let Some((lab, idx, offs)) = cur_basic_block {
+                    // Insert the previous basic block.
+                    labelled_blocks.insert(
+                        lab,
+                        BasicBlock {
+                            offs,
+                            abstract_len: op_idx - idx,
+                            final_len: cur_offset - offs,
+                        },
+                    );
+                }
+            }
+
+            // Update the offset.
             match op.opcode {
-                Either::Right(ControlFlowOp::Label(lab)) => {
-                    label_offsets.insert(lab, cur_offset);
+                Either::Right(Label(cur_lab)) => {
+                    // Save the new block label and offset.
+                    cur_basic_block = Some((cur_lab, op_idx, cur_offset));
                     furthest_offset = std::cmp::max(furthest_offset, cur_offset);
                 }
+
                 // A special case for LWDataId which may be 1 or 2 ops, depending on the source size.
                 Either::Left(AllocatedOpcode::LWDataId(_, ref data_id)) => {
                     let has_copy_type = data_section.has_copy_type(data_id).expect(
@@ -315,48 +481,64 @@ impl AllocatedAbstractInstructionSet {
                     );
                     cur_offset += if has_copy_type { 1 } else { 2 };
                 }
+
                 // Another special case for the blob opcode, used for testing.
                 Either::Left(AllocatedOpcode::BLOB(ref count)) => cur_offset += count.value as u64,
+
                 // these ops will end up being exactly one op, so the cur_offset goes up one
-                Either::Right(ControlFlowOp::Jump(..))
-                | Either::Right(ControlFlowOp::JumpIfNotZero(..))
-                | Either::Right(ControlFlowOp::Call(..))
-                | Either::Right(ControlFlowOp::MoveAddress(..))
-                | Either::Right(ControlFlowOp::LoadLabel(..))
+                Either::Right(Jump(..))
+                | Either::Right(JumpIfNotZero(..))
+                | Either::Right(Call(..))
+                | Either::Right(MoveAddress(..))
+                | Either::Right(LoadLabel(..))
                 | Either::Left(_) => {
                     cur_offset += 1;
                 }
-                Either::Right(ControlFlowOp::JumpIfNotEq(_, _, lab)) => {
+
+                Either::Right(JumpIfNotEq(_, _, lab)) => {
                     jnei_labels.insert(lab);
                     cur_offset += 1;
                 }
-                Either::Right(ControlFlowOp::Comment) => (),
-                Either::Right(ControlFlowOp::DataSectionOffsetPlaceholder) => {
+
+                Either::Right(Comment) => (),
+
+                Either::Right(DataSectionOffsetPlaceholder) => {
                     // If the placeholder is 32 bits, this is 1. if 64, this should be 2. We use LW
                     // to load the data, which loads a whole word, so for now this is 2.
                     cur_offset += 2
                 }
 
-                Either::Right(ControlFlowOp::PushAll(_))
-                | Either::Right(ControlFlowOp::PopAll(_)) => unreachable!(
+                Either::Right(PushAll(_)) | Either::Right(PopAll(_)) => unreachable!(
                     "fix me, pushall and popall don't really belong in control flow ops \
                         since they're not about control flow"
                 ),
             }
         }
 
+        // Don't forget the final block.
+        if let Some((lab, idx, offs)) = cur_basic_block {
+            labelled_blocks.insert(
+                lab,
+                BasicBlock {
+                    offs,
+                    abstract_len: self.ops.len() - idx,
+                    final_len: cur_offset - offs,
+                },
+            );
+        }
+
         let need_to_remap_jumps = furthest_offset > consts::EIGHTEEN_BITS
             || jnei_labels
                 .iter()
-                .any(|lab| label_offsets.get(lab).copied().unwrap() > consts::TWELVE_BITS);
+                .any(|lab| labelled_blocks.get(lab).copied().unwrap().offs > consts::TWELVE_BITS);
 
-        (need_to_remap_jumps, label_offsets)
+        (need_to_remap_jumps, furthest_offset, labelled_blocks)
     }
 
     /// If an instruction uses a label which can't fit in its immediate value then translate it
     /// into an instruction which loads the offset from the data section into a register and then
     /// uses the equivalent non-immediate instruction with the register.
-    fn rewrite_far_jumps(&mut self, label_offsets: &HashMap<Label, u64>) -> bool {
+    fn rewrite_far_jumps(&mut self, label_offsets: &LabeledBlocks) -> bool {
         let min_ops = self.ops.len();
         let mut modified = false;
 
@@ -367,7 +549,7 @@ impl AllocatedAbstractInstructionSet {
                 match &op.opcode {
                     Either::Right(ControlFlowOp::Jump(ref lab))
                     | Either::Right(ControlFlowOp::Call(ref lab)) => {
-                        let offset = *label_offsets.get(lab).unwrap();
+                        let offset = label_offsets.get(lab).unwrap().offs;
                         if offset <= consts::TWENTY_FOUR_BITS {
                             new_ops.push(op)
                         } else {
@@ -391,7 +573,7 @@ impl AllocatedAbstractInstructionSet {
                         }
                     }
                     Either::Right(ControlFlowOp::JumpIfNotEq(r1, r2, ref lab)) => {
-                        let offset = *label_offsets.get(lab).unwrap();
+                        let offset = label_offsets.get(lab).unwrap().offs;
                         if offset <= consts::TWELVE_BITS {
                             new_ops.push(op)
                         } else {
@@ -418,7 +600,7 @@ impl AllocatedAbstractInstructionSet {
                         }
                     }
                     Either::Right(ControlFlowOp::JumpIfNotZero(r1, ref lab)) => {
-                        let offset = *label_offsets.get(lab).unwrap();
+                        let offset = label_offsets.get(lab).unwrap().offs;
                         if offset <= consts::EIGHTEEN_BITS {
                             new_ops.push(op)
                         } else {
@@ -445,7 +627,7 @@ impl AllocatedAbstractInstructionSet {
                         }
                     }
                     Either::Right(ControlFlowOp::MoveAddress(r1, ref lab)) => {
-                        let offset = *label_offsets.get(lab).unwrap();
+                        let offset = label_offsets.get(lab).unwrap().offs;
                         if offset <= consts::EIGHTEEN_BITS {
                             new_ops.push(op)
                         } else {
