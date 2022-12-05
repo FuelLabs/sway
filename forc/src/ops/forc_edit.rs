84600dcaeabedec5486c4a8fbfa9ef7beddb963e
Mitchell Mackert <98185767+gr00vytvniks@users.noreply.github.com>
2022-12-05 13:38:44 -0600

    added comments to add function

--- forc/src/ops/forc_edit.rs
+++ forc/src/ops/forc_edit.rs
@@ -2,6 +2,11 @@ use crate::cli::{AddCommand, RemoveCommand};
 use anyhow::Result;
 
 pub fn add(command: AddCommand) -> Result<()> {
+    // 1. How will forc know where to add a dependency to?
+    //
+    // 2. How will we find the dependency block in the forc.toml?
+    //
+    // 3. Write new dependency to that block (name and version)
     Ok(())
 }
 pub fn remove(command: RemoveCommand) -> Result<()> {
