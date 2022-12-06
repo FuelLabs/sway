cdd2aaa94cc11b601e08e7e4e49c24e5a32c9223
Mitchell Mackert <98185767+gr00vytvniks@users.noreply.github.com>
2022-12-06 14:41:54 -0600

    destructured add command

--- forc/src/ops/forc_edit.rs
+++ forc/src/ops/forc_edit.rs
@@ -1,9 +1,19 @@
+use std::path::PathBuf;
+
 use crate::cli::{AddCommand, RemoveCommand};
 use anyhow::Result;
 
 pub fn add(command: AddCommand) -> Result<()> {
     // 1. How will forc know where to add a dependency to?
-    //
+    let AddCommand {
+        crates,
+        manifest_path,
+    } = command;
+
+    let dir = match manifest_path {
+        Some(ref path) => PathBuf::from(path),
+        None => std::env::current_dir()?,
+    };
     // 2. How will we find the dependency block in the forc.toml?
     //
     // 3. Write new dependency to that block (name and version)
