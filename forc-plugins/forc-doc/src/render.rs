a3be0c8730a37930ea26e21a96156bbe7b76ae92
Chris O'Brien <obrchr96@gmail.com>
2022-12-08 13:32:15 -0600

    clippy

--- forc-plugins/forc-doc/src/render.rs
+++ forc-plugins/forc-doc/src/render.rs
@@ -482,7 +482,7 @@ fn attrsmap_to_html_string(attributes: &AttributesMap) -> String {
 }
 /// Takes a formatted String fn and returns only the function signature.
 fn trim_fn_body(f: String) -> String {
-    match f.find("{") {
+    match f.find('{') {
         Some(index) => f.split_at(index).0.to_string(),
         None => f,
     }
