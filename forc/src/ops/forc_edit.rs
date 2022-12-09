ead2086737c228ffb4fed46d64b95291c5545438
Mitchell Mackert <98185767+gr00vytvniks@users.noreply.github.com>
2022-12-09 14:40:12 -0600

    added manifest file path

--- forc/src/ops/forc_edit.rs
+++ forc/src/ops/forc_edit.rs
@@ -1,21 +1,31 @@
 use std::path::PathBuf;
 
 use crate::cli::{AddCommand, RemoveCommand};
-use anyhow::Result;
+use anyhow::{Result, bail};
+use forc_pkg::manifest::ManifestFile;
 
 pub fn add(command: AddCommand) -> Result<()> {
     // 1. How will forc know where to add a dependency to?
+    // 
     let AddCommand {
-        crates: _,
-        manifest_path,
+        crates: _, 
+        manifest_path, // the path to forc.toml
     } = command;
 
-    let _dir = match manifest_path {
+
+    
+    let dir = match manifest_path {
         Some(ref path) => PathBuf::from(path),
         None => std::env::current_dir()?,
     };
+    let manifest = ManifestFile::from_dir(&dir)?;
+    let pkg_manifest = if let ManifestFile::Package(pkg_manifest) = &manifest {
+        pkg_manifest
+    } else {
+        bail!("forc-doc does not support workspaces.")
+    };
     // 2. How will we find the dependency block in the forc.toml?
-    //
+
     // 3. Write new dependency to that block (name and version)
     Ok(())
 }
