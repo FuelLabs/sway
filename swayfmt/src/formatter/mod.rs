5ceed0bb30154532ee6f10a16d7c5c06eb18d0ba
bingcicle <bingcicle@proton.me>
2022-12-09 16:57:10 +0800

    default to multiline if formatted if_expr contains newline

--- swayfmt/src/formatter/mod.rs
+++ swayfmt/src/formatter/mod.rs
@@ -1209,4 +1209,33 @@ fn main() {}
         assert_eq!(correct_sway_code, formatted_sway_code);
         assert!(test_stability(formatted_sway_code, formatter));
     }
+
+    #[test]
+    fn test_if_else_retain_multiline() {
+        let sway_code_to_format = r#"script;
+
+fn main() {
+    if foo    {
+           let x = 1;
+    } else    {
+        bar(y)   ;
+    }
+}
+"#;
+        let correct_sway_code = r#"script;
+
+fn main() {
+    if foo {
+        let x = 1;
+    } else {
+        bar(y);
+    }
+}
+"#;
+        let mut formatter = Formatter::default();
+        let formatted_sway_code =
+            Formatter::format(&mut formatter, Arc::from(sway_code_to_format), None).unwrap();
+        assert_eq!(correct_sway_code, formatted_sway_code);
+        assert!(test_stability(formatted_sway_code, formatter));
+    }
 }
