d52ec3bfbd983041a085c45fc83c3ae2833f6e6b
bingcicle <bingcicle@proton.me>
2022-12-12 11:31:53 +0800

    newline

--- swayfmt/src/utils/language/expr/conditional.rs
+++ swayfmt/src/utils/language/expr/conditional.rs
@@ -71,7 +71,6 @@ fn get_full_width_line_style(
         |temp_formatter| -> Result<LineStyle, FormatterError> {
             let mut if_expr_str = FormattedCode::new();
             format_if_expr(if_expr, &mut if_expr_str, temp_formatter)?;
-
             let if_expr_width = if_expr_str.chars().count();
 
             temp_formatter.shape.code_line.update_width(if_expr_width);
