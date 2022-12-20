00c20da3e56cd6864ece55970f64396be50fd046
Chris O'Brien <obrchr96@gmail.com>
2022-12-19 19:00:08 -0600

    update unreachable

--- sway-core/src/language/ty/declaration/declaration.rs
+++ sway-core/src/language/ty/declaration/declaration.rs
@@ -480,7 +480,7 @@ impl TyDeclaration {
             ImplTrait(_) => "impl_trait",
             FunctionDeclaration(_) => "fn",
             ConstantDeclaration(_) => "constant",
-            _ => "", // non-documentable items, this arm is unreachable
+            _ => unreachable!("these items are non-documentable"),
         }
     }
 
