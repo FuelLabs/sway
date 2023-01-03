e300883b845e94b7e0389caa299c5aee90d529f3
Emily Herbert <17410721+emilyaherbert@users.noreply.github.com>
2023-01-03 11:31:34 -0600

    Update sway-core/src/semantic_analysis/ast_node/expression/typed_expression/struct_instantiation.rs
    Co-authored-by: Toby Hutton <toby@grusly.com>

--- sway-core/src/semantic_analysis/ast_node/expression/typed_expression/struct_instantiation.rs
+++ sway-core/src/semantic_analysis/ast_node/expression/typed_expression/struct_instantiation.rs
@@ -203,7 +203,7 @@ fn unify_field_arguments_and_struct_fields(
                     typed_field.value.return_type,
                     struct_field.type_id,
                     &typed_field.value.span,
-                    "Struct field's type must match up with the type specified in its declaration.",
+                    "Struct field's type must match the type specified in its declaration.",
                     None,
                 )),
                 continue,
