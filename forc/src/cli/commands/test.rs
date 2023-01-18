20fc0bee878be63e568916cdda95eb7926e87fbe
Kaya Gokalp <kaya.gokalp@fuel.sh>
2023-01-18 15:59:05 +0300

    --receipts flag renamed into --log, log formatting moved to forc-util

--- forc/src/cli/commands/test.rs
+++ forc/src/cli/commands/test.rs
@@ -27,11 +27,21 @@ use tracing::info;
 pub struct Command {
     #[clap(flatten)]
     pub build: cli::shared::Build,
+    #[clap(flatten)]
+    pub test_print: PrintCommand,
     /// When specified, only tests containing the given string will be executed.
     pub filter: Option<String>,
-    /// If set prints out the receipts for all the tests.
-    #[clap(long = "receipts")]
-    pub receipts: bool,
+}
+
+/// The set of options provided for controlling output of a test.
+#[derive(Parser, Debug)]
+pub struct PrintCommand {
+    /// If set, the log receipts are formatted.
+    #[clap(long = "pretty")]
+    pub pretty_print: bool,
+    /// If set, `Log` and `LogData` receipts are printed for tests.
+    #[clap(long = "log")]
+    pub print_logs: bool,
 }
 
 pub(crate) fn exec(cmd: Command) -> Result<()> {
@@ -39,12 +49,12 @@ pub(crate) fn exec(cmd: Command) -> Result<()> {
         bail!("unit test filter not yet supported");
     }
 
-    let print_receipts = cmd.receipts;
+    let test_print_opts = test_print_opts_from_cmd(&cmd);
     let opts = opts_from_cmd(cmd);
     let built_tests = forc_test::build(opts)?;
     let start = std::time::Instant::now();
     info!("   Running {} tests", built_tests.test_count());
-    let tested = built_tests.run(print_receipts)?;
+    let tested = built_tests.run(&test_print_opts)?;
     let duration = start.elapsed();
 
     // Eventually we'll print this in a fancy manner, but this will do for testing.
@@ -145,5 +155,12 @@ fn opts_from_cmd(cmd: Command) -> forc_test::Opts {
         time_phases: cmd.build.time_phases,
         binary_outfile: cmd.build.binary_outfile,
         debug_outfile: cmd.build.debug_outfile,
+    }
+}
+
+fn test_print_opts_from_cmd(cmd: &Command) -> forc_test::TestPrintOpts {
+    forc_test::TestPrintOpts {
+        pretty_print: cmd.test_print.pretty_print,
+        print_logs: cmd.test_print.print_logs,
     }
 }
