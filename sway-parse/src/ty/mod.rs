8ebcf658abb287714c3ef4a1f6b6df47f6a6558b
Sophie <sophiedankel@gmail.com>
2023-01-26 12:24:47 -0800

    Fix lifetime error

--- sway-parse/src/ty/mod.rs
+++ sway-parse/src/ty/mod.rs
@@ -55,8 +55,8 @@ impl Parse for Ty {
 }
 
 impl ParseToEnd for TyArrayDescriptor {
-    fn parse_to_end<'a, '_>(
-        mut parser: Parser<'a, '_>,
+    fn parse_to_end<'a, 'b>(
+        mut parser: Parser<'a, 'b>,
     ) -> ParseResult<(TyArrayDescriptor, ParserConsumed<'a>)> {
         let ty = parser.parse()?;
         let semicolon_token = parser.parse()?;
