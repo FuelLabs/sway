bc700d5afda842f709cac4555f0c64d19212e576
mitchmindtree <mitchell.nordine@fuel.sh>
2023-01-31 20:55:45 +1000

    Refactor `forc-client` in preparation for new `forc submit` command (#3941)
    This is a small refactor of the `forc-client` crate in anticipation of
    including the new `forc submit` command (see #3885) as a new binary
    output for the crate.
    
    This PR inverts the forc-client module hierarchy to follow `forc`'s
    layout a little more closely.
    
    It also refactors the command types in order to better share sets of
    clap arguments between different commands with the aim of reducing code
    duplication and providing a more uniform experience across all official
    forc commands/plugins. This will be improved further once the `forc
    submit` command is included, allowing us to share some of the networking
    arguments too.
    
    ---------
    
    Co-authored-by: Joshua Batty <joshpbatty@gmail.com>

--- forc-plugins/forc-client/src/ops/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod deploy;
-pub mod pkg_util;
-pub mod run;
-pub mod tx_util;
--- test/src/e2e_vm_tests/harness.rs
+++ test/src/e2e_vm_tests/harness.rs
@@ -1,8 +1,8 @@
 use anyhow::{anyhow, bail, Result};
 use colored::Colorize;
-use forc_client::ops::{
-    deploy::{cmd::DeployCommand, op::deploy},
-    run::{cmd::RunCommand, op::run},
+use forc_client::{
+    cmd::{Deploy as DeployCommand, Run as RunCommand},
+    op::{deploy, run},
 };
 use forc_pkg::{Built, BuiltPackage};
 use fuel_tx::TransactionBuilder;
@@ -62,11 +62,14 @@ pub(crate) async fn deploy_contract(file_name: &str, run_config: &RunConfig) -> 
     let manifest_dir = env!("CARGO_MANIFEST_DIR");
 
     deploy(DeployCommand {
-        path: Some(format!(
-            "{manifest_dir}/src/e2e_vm_tests/test_programs/{file_name}"
-        )),
-        terse_mode: !run_config.verbose,
-        locked: run_config.locked,
+        pkg: forc_client::cmd::deploy::Pkg {
+            path: Some(format!(
+                "{manifest_dir}/src/e2e_vm_tests/test_programs/{file_name}"
+            )),
+            terse: !run_config.verbose,
+            locked: run_config.locked,
+            ..Default::default()
+        },
         signing_key: Some(SecretKey::from_str(SECRET_KEY).unwrap()),
         ..Default::default()
     })
@@ -96,13 +99,16 @@ pub(crate) async fn runs_on_node(
         }
 
         let command = RunCommand {
-            path: Some(format!(
-                "{manifest_dir}/src/e2e_vm_tests/test_programs/{file_name}"
-            )),
+            pkg: forc_client::cmd::run::Pkg {
+                path: Some(format!(
+                    "{manifest_dir}/src/e2e_vm_tests/test_programs/{file_name}"
+                )),
+                locked: run_config.locked,
+                terse: !run_config.verbose,
+                ..Default::default()
+            },
             node_url: Some(NODE_URL.into()),
-            terse_mode: !run_config.verbose,
             contract: Some(contracts),
-            locked: run_config.locked,
             signing_key: Some(SecretKey::from_str(SECRET_KEY).unwrap()),
             ..Default::default()
         };
