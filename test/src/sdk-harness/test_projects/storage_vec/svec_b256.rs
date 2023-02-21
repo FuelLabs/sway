4fc1aa1def3e0d346d5ec422edc2a9a507b063e8
Ahmed Sagdati <ahmed.sagdati.ets@gmail.com>
2023-02-21 11:13:10 +0100

    suggestions for fixing the tests

--- test/src/sdk-harness/test_projects/storage_vec/svec_b256.rs
+++ test/src/sdk-harness/test_projects/storage_vec/svec_b256.rs
@@ -2,10 +2,10 @@ testgen!(
     test_b256_vec,
     "test_artifacts/storage_vec/svec_b256/out/debug/svec_b256-abi.json",
     "b256",
-    [u8; 32],
-    [1; 32],
-    [2; 32],
-    [3; 32],
-    [4; 32],
-    [5; 32]
+    ::fuels::types::Bits256,
+    ::fuels::types::Bits256([1; 32]),
+    ::fuels::types::Bits256([2; 32]),
+    ::fuels::types::Bits256([3; 32]),
+    ::fuels::types::Bits256([4; 32]),
+    ::fuels::types::Bits256([5; 32])
 );
--- test/src/sdk-harness/test_projects/storage_vec/svec_str.rs
+++ test/src/sdk-harness/test_projects/storage_vec/svec_str.rs
@@ -2,10 +2,10 @@ testgen!(
     test_str_vec,
     "test_artifacts/storage_vec/svec_str/out/debug/svec_str-abi.json",
     "str",
-    String,
-    "yeet".to_string(),
-    "meow".to_string(),
-    "kekw".to_string(),
-    "gmgn".to_string(),
-    "sway".to_string()
+    ::fuels::types::SizedAsciiString::<4>,
+    ::fuels::types::SizedAsciiString::new("yeet".to_string()).unwrap(),
+    ::fuels::types::SizedAsciiString::new("meow".to_string()).unwrap(),
+    ::fuels::types::SizedAsciiString::new("kekw".to_string()).unwrap(),
+    ::fuels::types::SizedAsciiString::new("gmgn".to_string()).unwrap(),
+    ::fuels::types::SizedAsciiString::new("sway".to_string()).unwrap()
 );
