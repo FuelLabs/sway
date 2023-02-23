01b400feea0f890f1027f90531ed77e4e764c249
Joshua Trujillo <Jtriley15@gmail.com>
2023-02-23 15:30:39 -0800

    add svec method tests

--- test/src/sdk-harness/test_projects/storage_vec/testgen.rs
+++ test/src/sdk-harness/test_projects/storage_vec/testgen.rs
@@ -153,6 +153,62 @@ macro_rules! testgen {
                         .await
                         .unwrap();
                 }
+
+                pub async fn swap(instance: &MyContract, index_0: u64, index_1: u64) {
+                    instance.methods()
+                        .swap(index_0, index_1)
+                        .tx_params(TxParameters::new(None, Some(100_000_000), None))
+                        .call()
+                        .await
+                        .unwrap();
+                }
+
+                pub async fn first(instance: &MyContract) -> $type_declaration {
+                    instance.methods()
+                        .first()
+                        .tx_params(TxParameters::new(None, Some(100_000_000), None))
+                        .call()
+                        .await
+                        .unwrap()
+                        .value
+                }
+
+                pub async fn last(instance: &MyContract) -> $type_declaration {
+                    instance.methods()
+                        .last()
+                        .tx_params(TxParameters::new(None, Some(100_000_000), None))
+                        .call()
+                        .await
+                        .unwrap()
+                        .value
+                }
+
+                pub async fn reverse(instance: &MyContract) {
+                    instance.methods()
+                        .reverse()
+                        .tx_params(TxParameters::new(None, Some(100_000_000), None))
+                        .call()
+                        .await
+                        .unwrap();
+                }
+
+                pub async fn fill(instance: &MyContract, value: $type_declaration) {
+                    instance.methods()
+                        .fill(value)
+                        .tx_params(TxParameters::new(None, Some(100_000_000), None))
+                        .call()
+                        .await
+                        .unwrap();
+                }
+
+                pub async fn resize(instance: &MyContract, new_len: u64, value: $type_declaration) {
+                    instance.methods()
+                        .resize(new_len, value)
+                        .tx_params(TxParameters::new(None, Some(100_000_000), None))
+                        .call()
+                        .await
+                        .unwrap();
+                }
             }
 
             // Silences `super::*` warning; required for user-defined types.
@@ -320,6 +376,121 @@ macro_rules! testgen {
 
                     assert!(is_empty(&instance).await);
                 }
+
+                #[tokio::test]
+                async fn can_swap() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+                    push(&instance, $arg2).await;
+                    push(&instance, $arg3).await;
+
+                    swap(&instance, 0, 3).await;
+                    swap(&instance, 1, 2).await;
+
+                    assert_eq!(get(&instance, 0).await, $arg3);
+                    assert_eq!(get(&instance, 1).await, $arg2);
+                    assert_eq!(get(&instance, 2).await, $arg1);
+                    assert_eq!(get(&instance, 3).await, $arg0);
+                }
+
+                #[tokio::test]
+                async fn can_get_first() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+
+                    assert_eq!(first(&instance).await, $arg0);
+                }
+
+                #[tokio::test]
+                async fn can_get_last() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+
+                    assert_eq!(last(&instance).await, $arg1);
+                }
+
+                #[tokio::test]
+                async fn can_reverse() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+                    push(&instance, $arg2).await;
+                    push(&instance, $arg3).await;
+
+                    reverse(&instance).await;
+
+                    assert_eq!(get(&instance, 0).await, $arg3);
+                    assert_eq!(get(&instance, 1).await, $arg2);
+                    assert_eq!(get(&instance, 2).await, $arg1);
+                    assert_eq!(get(&instance, 3).await, $arg0);
+
+                    reverse(&instance).await;
+
+                    assert_eq!(get(&instance, 0).await, $arg0);
+                    assert_eq!(get(&instance, 1).await, $arg1);
+                    assert_eq!(get(&instance, 2).await, $arg2);
+                    assert_eq!(get(&instance, 3).await, $arg3);
+                }
+
+                #[tokio::test]
+                async fn can_fill() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+                    push(&instance, $arg2).await;
+                    push(&instance, $arg3).await;
+
+                    fill(&instance, $arg4).await;
+
+                    assert_eq!(get(&instance, 0).await, $arg4);
+                    assert_eq!(get(&instance, 1).await, $arg4);
+                    assert_eq!(get(&instance, 2).await, $arg4);
+                    assert_eq!(get(&instance, 3).await, $arg4);
+                }
+
+                #[tokio::test]
+                async fn can_resize_up() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+                    push(&instance, $arg2).await;
+                    push(&instance, $arg3).await;
+
+                    resize(&instance, 6, $arg4).await;
+
+                    assert_eq!(len(&instance).await, 6);
+                    assert_eq!(get(&instance, 0).await, $arg0);
+                    assert_eq!(get(&instance, 1).await, $arg1);
+                    assert_eq!(get(&instance, 2).await, $arg2);
+                    assert_eq!(get(&instance, 3).await, $arg3);
+                    assert_eq!(get(&instance, 4).await, $arg4);
+                    assert_eq!(get(&instance, 5).await, $arg4);
+                }
+
+                #[tokio::test]
+                async fn can_resize_down() {
+                    let instance = get_contract_instance().await;
+
+                    push(&instance, $arg0).await;
+                    push(&instance, $arg1).await;
+                    push(&instance, $arg2).await;
+                    push(&instance, $arg3).await;
+
+                    resize(&instance, 2, $arg4).await;
+
+                    assert_eq!(len(&instance).await, 2);
+                    assert_eq!(get(&instance, 0).await, $arg0);
+                    assert_eq!(get(&instance, 1).await, $arg1);
+                }
             }
 
             // Silences `super::*` warning; required for user-defined types.
@@ -377,6 +548,31 @@ macro_rules! testgen {
                     let instance = get_contract_instance().await;
 
                     insert(&instance, 1, $arg1).await;
+                }
+
+                #[tokio::test]
+                #[should_panic(expected = "revert_id: 0")]
+                async fn cant_get_first() {
+                    let instance = get_contract_instance().await;
+
+                    let _ = first(&instance).await;
+                }
+
+                #[tokio::test]
+                #[should_panic(expected = "revert_id: 0")]
+                async fn cant_get_last() {
+                    let instance = get_contract_instance().await;
+
+                    let _ = last(&instance).await;
+                }
+
+
+                #[tokio::test]
+                #[should_panic(expected = "revert_id: 18446744073709486084")]
+                async fn cant_swap() {
+                    let instance = get_contract_instance().await;
+
+                    let _ = swap(&instance, 0, 1).await;
                 }
             }
 
