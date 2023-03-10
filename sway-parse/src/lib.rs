1e5f062d70133a15194041627b6f290616d505a8
JC <jc@pnkr.io>
2023-03-10 23:55:17 +0300

    Share utils between sway-parse unit tests

--- sway-parse/src/lib.rs
+++ sway-parse/src/lib.rs
@@ -59,3 +59,32 @@ pub fn parse_module_kind(
     }
     parser.parse()
 }
+
+#[cfg(test)]
+mod test_utils {
+    use crate::{priv_prelude::ParseToEnd, Parse, Parser};
+    use std::sync::Arc;
+
+    pub fn parse<T>(input: &str) -> T
+    where
+        T: Parse,
+    {
+        let handler = <_>::default();
+        let ts = crate::token::lex(&handler, &Arc::from(input), 0, input.len(), None).unwrap();
+        Parser::new(&handler, &ts)
+            .parse()
+            .unwrap_or_else(|_| panic!("Parse error: {:?}", handler.consume().0))
+    }
+
+    pub fn parse_to_end<T>(input: &str) -> T
+    where
+        T: ParseToEnd,
+    {
+        let handler = <_>::default();
+        let ts = crate::token::lex(&handler, &Arc::from(input), 0, input.len(), None).unwrap();
+        Parser::new(&handler, &ts)
+            .parse_to_end()
+            .map(|(m, _)| m)
+            .unwrap_or_else(|_| panic!("Parse error: {:?}", handler.consume().0))
+    }
+}
