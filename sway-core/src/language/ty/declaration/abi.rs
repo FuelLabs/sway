205c6c5f799fcf87602a693ff39eeba4fcfaca3c
Emily Herbert <17410721+emilyaherbert@users.noreply.github.com>
2023-03-13 12:04:04 -0500

    Introduce `TypeSubstList` and add it to `TyDeclaration` and `DeclRef`. (#4256)
    ## Description
    
    `TypeSubstList` is the necessary component for performing
    monomorphization after type checking and for strengthening trait
    constraint solving. At a high level, after all of these changes are
    complete, `DeclId`'s will be unique to each declaration, so for each
    declaration that the user writes in source code, there is only ever one
    `DeclId` that is used to refer to this declaration. Generic types will
    be extracted to the `TypeSubstList`, so for one declaration in source
    code, there will be one `DeclId`, but potentially multiple
    `TypeSubstList`. In particular `DeclRef` will hold a `TypeSubstList`
    with the generics for a particular `DeclId`:
    
    ```rust
    /// Represents the use of / syntactic reference to a declaration. A
    /// smart-wrapper around a [DeclId], containing additional information about a
    /// declaration.
    #[derive(Debug, Clone)]
    pub struct DeclRef<I> {
        /// The name of the declaration.
        // NOTE: In the case of storage, the name is "storage".
        name: Ident,
    
        /// The index into the [DeclEngine].
        id: I,
    
        /// The type substitution list to apply to the `id` field for type
        /// monomorphization.
        subst_list: TypeSubstList,
    
        /// The [Span] of the entire declaration.
        decl_span: Span,
    }
    ```
    
    And the type system enforces that the `TyDeclaration` AST nodes have
    templated `TypeSubstList` with the `Template` type:
    
    ```rust
    /// An object that is a template for copies from the template.
    ///
    /// This is predominantly used with
    /// [TypeSubstList](crate::type_system::TypeSubstList) and
    /// [TyDeclaration](crate::language::ty::TyDeclaration). The various
    /// variants of [TyDeclaration](crate::language::ty::TyDeclaration) contain
    /// fields `type_subst_list: Template<TypeSubstList>`. This type indicates that
    /// the [TypeSubstList](crate::type_system::TypeSubstList) contained in this
    /// field is simply a template for usages of the
    /// declaration declared in that particular
    /// [TyDeclaration](crate::language::ty::TyDeclaration) node.
    #[derive(Clone, Debug)]
    pub struct Template<T>(T)
    where
        T: Clone;
    
    impl<T> Template<T>
    where
        T: Clone,
    {
        pub(crate) fn new(value: T) -> Template<T> {
            Template(value)
        }
    
        pub(crate) fn into_inner(self) -> T {
            self.0
        }
    
        pub(crate) fn fresh_copy(&self) -> T {
            self.0.clone()
        }
    }
    ```
    
    ```rust
        StructDeclaration {
            name: Ident,
            decl_id: DeclId<TyStructDeclaration>,
            type_subst_list: Template<TypeSubstList>,
            decl_span: Span,
        },
    ```
    
    ## Checklist
    
    - [x] I have linked to any relevant issues.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have updated the documentation where relevant (API docs, the
    reference, and the Sway book).
    - [x] I have added tests that prove my fix is effective or that my
    feature works.
    - [x] I have added (or requested a maintainer to add) the necessary
    `Breaking*` or `New Feature` labels where relevant.
    - [x] I have done my best to ensure that my PR adheres to [the Fuel Labs
    Code Review
    Standards](https://github.com/FuelLabs/rfcs/blob/master/text/code-standards/external-contributors.md).
    - [x] I have requested a review from the relevant team or maintainers.
    
    ---------
    
    Co-authored-by: emilyaherbert <emily.herbert@fuel.sh>

--- sway-core/src/language/ty/declaration/abi.rs
+++ sway-core/src/language/ty/declaration/abi.rs
@@ -21,9 +21,27 @@ pub struct TyAbiDeclaration {
 impl EqWithEngines for TyAbiDeclaration {}
 impl PartialEqWithEngines for TyAbiDeclaration {
     fn eq(&self, other: &Self, engines: Engines<'_>) -> bool {
-        self.name == other.name
-            && self.interface_surface.eq(&other.interface_surface, engines)
-            && self.items.eq(&other.items, engines)
+        let TyAbiDeclaration {
+            name: ln,
+            interface_surface: lis,
+            supertraits: ls,
+            items: li,
+            // these fields are not compared because they aren't relevant/a
+            // reliable source of obj v. obj distinction
+            attributes: _,
+            span: _,
+        } = self;
+        let TyAbiDeclaration {
+            name: rn,
+            interface_surface: ris,
+            supertraits: rs,
+            items: ri,
+            // these fields are not compared because they aren't relevant/a
+            // reliable source of obj v. obj distinction
+            attributes: _,
+            span: _,
+        } = other;
+        ln == rn && lis.eq(ris, engines) && li.eq(ri, engines) && ls.eq(rs, engines)
     }
 }
 
