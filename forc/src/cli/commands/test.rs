24c1cb51c24518bda4b5ed6065cad845f94fab23
kayagokalp <kaya.gokalp@fuel.sh>
2023-04-05 14:48:54 -0400

    chore: fmt

--- forc/src/cli/commands/test.rs
+++ forc/src/cli/commands/test.rs
@@ -33,7 +33,7 @@ pub struct Command {
     /// When specified, only tests containing the given string will be executed.
     pub filter: Option<String>,
     #[clap(long)]
-    /// Number of threads to utilize when running the tests. By default, this is the number of 
+    /// Number of threads to utilize when running the tests. By default, this is the number of
     /// threads available in your system.
     pub test_threads: Option<usize>,
 }
