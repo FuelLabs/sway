a286467b724513c6f8059cde992b90acff44af00
green <xgreenx9999@gmail.com>
2023-05-04 01:11:16 +0100

    Update to `fuel-core 0.18.1` and `fuel-vm 0.31.1`

--- test/src/sdk-harness/test_projects/token_ops/mod.rs
+++ test/src/sdk-harness/test_projects/token_ops/mod.rs
@@ -394,45 +394,44 @@ async fn can_send_message_output_with_data() {
     assert_eq!(vec![100, 75, 50], message_receipt.data().unwrap());
 }
 
-// TODO: Uncomment with 0.31.1 fuel-vm release
-// #[tokio::test]
-// async fn can_send_message_output_without_data() {
-//     let num_wallets = 1;
-//     let coins_per_wallet = 1;
-//     let amount_per_coin = 1_000_000;
-//
-//     let config = WalletsConfig::new(
-//         Some(num_wallets),
-//         Some(coins_per_wallet),
-//         Some(amount_per_coin),
-//     );
-//
-//     let wallets = launch_custom_provider_and_get_wallets(config, None, None).await;
-//     let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance(wallets[0].clone()).await;
-//
-//     let amount = 33u64;
-//     let recipient_hex = "0x000000000000000000000000b46a7a1a23f3897cc83a94521a96da5c23bc58db";
-//     let recipient_address = Address::from_str(&recipient_hex).unwrap();
-//
-//     let call_response = fuelcoin_instance
-//         .methods()
-//         .send_message(Bits256(*recipient_address), Vec::<u64>::new(), amount)
-//         .call()
-//         .await
-//         .unwrap();
-//
-//     let message_receipt = call_response
-//         .receipts
-//         .iter()
-//         .find(|&r| matches!(r, fuels::tx::Receipt::MessageOut { .. }))
-//         .unwrap();
-//
-//     assert_eq!(*fuelcoin_id, **message_receipt.sender().unwrap());
-//     assert_eq!(&recipient_address, message_receipt.recipient().unwrap());
-//     assert_eq!(amount, message_receipt.amount().unwrap());
-//     assert_eq!(0, message_receipt.len().unwrap());
-//     assert_eq!(Vec::<u8>::new(), message_receipt.data().unwrap());
-// }
+#[tokio::test]
+async fn can_send_message_output_without_data() {
+    let num_wallets = 1;
+    let coins_per_wallet = 1;
+    let amount_per_coin = 1_000_000;
+
+    let config = WalletsConfig::new(
+        Some(num_wallets),
+        Some(coins_per_wallet),
+        Some(amount_per_coin),
+    );
+
+    let wallets = launch_custom_provider_and_get_wallets(config, None, None).await;
+    let (fuelcoin_instance, fuelcoin_id) = get_fuelcoin_instance(wallets[0].clone()).await;
+
+    let amount = 33u64;
+    let recipient_hex = "0x000000000000000000000000b46a7a1a23f3897cc83a94521a96da5c23bc58db";
+    let recipient_address = Address::from_str(&recipient_hex).unwrap();
+
+    let call_response = fuelcoin_instance
+        .methods()
+        .send_message(Bits256(*recipient_address), Vec::<u64>::new(), amount)
+        .call()
+        .await
+        .unwrap();
+
+    let message_receipt = call_response
+        .receipts
+        .iter()
+        .find(|&r| matches!(r, fuels::tx::Receipt::MessageOut { .. }))
+        .unwrap();
+
+    assert_eq!(*fuelcoin_id, **message_receipt.sender().unwrap());
+    assert_eq!(&recipient_address, message_receipt.recipient().unwrap());
+    assert_eq!(amount, message_receipt.amount().unwrap());
+    assert_eq!(0, message_receipt.len().unwrap());
+    assert_eq!(Vec::<u8>::new(), message_receipt.data().unwrap());
+}
 
 async fn get_fuelcoin_instance(
     wallet: WalletUnlocked,
