6642d95e0a2f173af19ef7357d5559cdc38995de
green <xgreenx9999@gmail.com>
2023-05-04 01:14:03 +0100

    Return back `--locked` in the ci checks

--- ci_checks.sh
+++ ci_checks.sh
@@ -6,7 +6,7 @@
 # - `cargo run --locked --release --bin test -- --locked`
 #
 # You can install `fuel-core` node by:
-# `cargo install fuel-core-bin --git https://github.com/FuelLabs/fuel-core --tag v0.16.1 --locked`
+# `cargo install fuel-core-bin --git https://github.com/FuelLabs/fuel-core --tag v0.18.1 --locked`
 #
 # And run it with:
 # `fuel-core run --db-type in-memory`
@@ -20,20 +20,20 @@ cargo clippy --all-features --all-targets &&
 cargo sort -w --check &&
 cargo sort -w --check templates/sway-test-rs/template &&
 cargo fmt --all -- --check &&
-cargo build --workspace --all-features --all-targets &&
+cargo build --locked --workspace --all-features --all-targets &&
 cargo test --locked &&
 cargo +nightly udeps --locked --all-targets &&
-cargo install --debug --path ./forc &&
-cargo install --debug --path ./forc-plugins/forc-fmt &&
-cargo install --debug --path ./forc-plugins/forc-lsp &&
-cargo install --debug --path ./forc-plugins/forc-client &&
-cargo install --debug --path ./forc-plugins/forc-tx &&
-cargo install --debug forc-explore &&
-cargo install --debug --path ./scripts/mdbook-forc-documenter &&
+cargo install --locked --debug --path ./forc &&
+cargo install --locked --debug --path ./forc-plugins/forc-fmt &&
+cargo install --locked --debug --path ./forc-plugins/forc-lsp &&
+cargo install --locked --debug --path ./forc-plugins/forc-client &&
+cargo install --locked --debug --path ./forc-plugins/forc-tx &&
+cargo install --locked --debug forc-explore &&
+cargo install --locked --debug --path ./scripts/mdbook-forc-documenter &&
 forc build --path sway-lib-core &&
 forc test --path sway-lib-core &&
-cargo run -p forc -- build --locked --path ./examples/Forc.toml &&
-cargo run -p forc-fmt -- --check --path ./examples &&
+cargo run --locked -p forc -- build --locked --path ./examples/Forc.toml &&
+cargo run --locked -p forc-fmt -- --check --path ./examples &&
 rm -Rf test-proj &&
 forc new test-proj &&
 echo "std = { path = \"../sway-lib-std/\" }" >> test-proj/Forc.toml &&
@@ -42,7 +42,7 @@ forc build --path test-proj &&
 echo "[workspace]" >> test-proj/Cargo.toml &&
 (cd test-proj && cargo test) &&
 rm -R test-proj &&
-cargo run --release --bin test -- --target evm --locked &&
+cargo run --locked --release --bin test -- --target evm --locked &&
 (cd test/src/sdk-harness && bash build.sh --locked) &&
 cargo test --manifest-path ./test/src/sdk-harness/Cargo.toml -- --nocapture &&
-cargo run --release --bin test -- --locked
+cargo run --locked --release --bin test -- --locked
