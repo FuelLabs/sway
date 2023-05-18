7a2f2833d31caa6b299b5145f8869aef566e553b
IGI-111 <igi-111@protonmail.com>
2023-05-19 00:55:41 +0200

    Remove aggressive predicate inlining
    Now that jumps are allowed in predicates, this change uses the regular
    inlining pass and its heuristics instead of just inlining everything,
    which slowed down compilation significantly.

--- sway-ir/src/pass_manager.rs
+++ sway-ir/src/pass_manager.rs
@@ -1,12 +1,11 @@
 use crate::{
     create_arg_demotion_pass, create_const_combine_pass, create_const_demotion_pass,
     create_dce_pass, create_dom_fronts_pass, create_dominators_pass, create_func_dce_pass,
-    create_inline_in_main_pass, create_inline_in_non_predicate_pass,
-    create_inline_in_predicate_pass, create_mem2reg_pass, create_memcpyopt_pass,
-    create_misc_demotion_pass, create_module_printer_pass, create_module_verifier_pass,
-    create_postorder_pass, create_ret_demotion_pass, create_simplify_cfg_pass, Context, Function,
-    IrError, Module, CONSTCOMBINE_NAME, DCE_NAME, FUNC_DCE_NAME, INLINE_NONPREDICATE_NAME,
-    INLINE_PREDICATE_NAME, MEM2REG_NAME, SIMPLIFYCFG_NAME,
+    create_inline_in_main_pass, create_inline_in_module_pass, create_mem2reg_pass,
+    create_memcpyopt_pass, create_misc_demotion_pass, create_module_printer_pass,
+    create_module_verifier_pass, create_postorder_pass, create_ret_demotion_pass,
+    create_simplify_cfg_pass, Context, Function, IrError, Module, CONSTCOMBINE_NAME, DCE_NAME,
+    FUNC_DCE_NAME, INLINE_MODULE_NAME, MEM2REG_NAME, SIMPLIFYCFG_NAME,
 };
 use downcast_rs::{impl_downcast, Downcast};
 use rustc_hash::FxHashMap;
@@ -306,8 +305,7 @@ pub fn register_known_passes(pm: &mut PassManager) {
     pm.register(create_module_verifier_pass());
     // Optimization passes.
     pm.register(create_mem2reg_pass());
-    pm.register(create_inline_in_predicate_pass());
-    pm.register(create_inline_in_non_predicate_pass());
+    pm.register(create_inline_in_module_pass());
     pm.register(create_inline_in_main_pass());
     pm.register(create_const_combine_pass());
     pm.register(create_simplify_cfg_pass());
@@ -320,16 +318,12 @@ pub fn register_known_passes(pm: &mut PassManager) {
     pm.register(create_memcpyopt_pass());
 }
 
-pub fn create_o1_pass_group(is_predicate: bool) -> PassGroup {
+pub fn create_o1_pass_group() -> PassGroup {
     // Create a configuration to specify which passes we want to run now.
     let mut o1 = PassGroup::default();
     // Configure to run our passes.
     o1.append_pass(MEM2REG_NAME);
-    if is_predicate {
-        o1.append_pass(INLINE_PREDICATE_NAME);
-    } else {
-        o1.append_pass(INLINE_NONPREDICATE_NAME);
-    }
+    o1.append_pass(INLINE_MODULE_NAME);
     o1.append_pass(CONSTCOMBINE_NAME);
     o1.append_pass(SIMPLIFYCFG_NAME);
     o1.append_pass(CONSTCOMBINE_NAME);
