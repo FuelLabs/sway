d2c1f03548d4253023279a907d267d39522c1c91
Anton Trunov <anton.a.trunov@gmail.com>
2023-07-27 08:45:50 +0400

    SuperABIs for ABIs (#4272)

---
+++ test/src/sdk-harness/test_projects/superabi/mod.rs
@@ -0,0 +1,42 @@
+use fuels::accounts::wallet::WalletUnlocked;
+use fuels::prelude::*;
+
+abigen!(Contract(
+    name = "SuperAbiTestContract",
+    abi = "test_projects/superabi/out/debug/superabi-abi.json"
+));
+
+async fn get_superabi_instance() -> SuperAbiTestContract<WalletUnlocked> {
+    let wallet = launch_provider_and_get_wallet().await;
+    let id = Contract::load_from(
+        "test_projects/superabi/out/debug/superabi.bin",
+        LoadConfiguration::default(),
+    )
+    .unwrap()
+    .deploy(&wallet, TxParameters::default())
+    .await
+    .unwrap();
+    SuperAbiTestContract::new(id.clone(), wallet)
+}
+
+#[tokio::test]
+async fn abi_test() -> Result<()> {
+    let instance = get_superabi_instance().await;
+    let contract_methods = instance.methods();
+
+    let response = contract_methods.abi_test().call().await?;
+    assert_eq!(42, response.value);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn superabi_test() -> Result<()> {
+    let instance = get_superabi_instance().await;
+    let contract_methods = instance.methods();
+
+    let response = contract_methods.superabi_test().call().await?;
+    assert_eq!(41, response.value);
+
+    Ok(())
+}
