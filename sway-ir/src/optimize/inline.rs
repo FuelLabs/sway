02cff2c49f7ca6a2457fbc2235e96d4b9d487242
Vaivaswatha Nagaraj <vaivaswatha.nagaraj@fuel.sh>
2023-08-02 16:32:35 +0530

    Workaround: Do not inline functions that have pointer args

--- sway-ir/src/optimize/inline.rs
+++ sway-ir/src/optimize/inline.rs
@@ -151,6 +151,14 @@ pub fn inline_in_module(
             return true;
         }
 
+        if func.args_iter(ctx).any(|(_name, arg_val)| {
+            arg_val.get_type(ctx).map_or(false, |ty| {
+                ty.is_ptr(ctx) || !(ty.is_unit(ctx) | ty.is_bool(ctx) | ty.is_uint(ctx))
+            })
+        }) {
+            return true;
+        }
+
         false
     };
 
