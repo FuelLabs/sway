a2a78b178f591049d799e00f83a61069b6ee15e9
JoshuaBatty <joshpbatty@gmail.com>
2023-08-10 11:42:24 +1000

    clippy

--- sway-lsp/benches/bench_main.rs
+++ sway-lsp/benches/bench_main.rs
@@ -2,6 +2,6 @@ mod lsp_benchmarks;
 use criterion::criterion_main;
 
 criterion_main! {
-    // lsp_benchmarks::token_map::benches,
+    lsp_benchmarks::token_map::benches,
     lsp_benchmarks::requests::benches,
 }
