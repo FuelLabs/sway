019bce347109a9744eb1ea05c70f03cee0548353
Voxelot <brandonkite92@gmail.com>
2023-08-10 21:05:36 -0700

    fix sdk-harness tests to use real strings

--- test/src/sdk-harness/test_projects/storage_string/mod.rs
+++ test/src/sdk-harness/test_projects/storage_string/mod.rs
@@ -25,9 +25,7 @@ async fn stores_string() {
     let instance = setup().await;
 
     let string = "Fuel is blazingly fast!";
-    let input = String {
-        bytes: Bytes(string.as_bytes().to_vec()),
-    };
+    let input = string.to_string();
 
     assert_eq!(
         instance.methods().stored_len().call().await.unwrap().value,
@@ -48,7 +46,7 @@ async fn stores_string() {
 
     assert_eq!(
         instance.methods().get_string().call().await.unwrap().value,
-        input.bytes
+        Bytes(input.as_bytes().to_vec())
     );
 }
 
@@ -60,11 +58,9 @@ async fn stores_long_string() {
     let string = "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculis quis, sem. Phasellus rhoncus. Aenean id metus id velit ullamcorper pulvinar. Vestibulum fermentum tortor id mi. Pellentesque ipsum. Nulla non arcu lacinia neque faucibus fringilla. Nulla non lectus sed nisl molestie malesuada. Proin in tellus sit amet nibh dignissim sagittis. Vivamus luctus egestas leo. Maecenas sollicitudin. Nullam rhoncus aliquam metus. Etiam egestas wisi a erat.
 
     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Aliquam erat volutpat. Nunc auctor. Mauris pretium quam et urna. Fusce nibh. Duis risus. Curabitur sagittis hendrerit ante. Aliquam erat volutpat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Duis condimentum augue id magna semper rutrum. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Fusce consectetuer risus a nunc. Aliquam ornare wisi eu metus. Integer pellentesque quam vel velit. Duis pulvinar.
-    
+
     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi gravida libero nec velit. Morbi scelerisque luctus velit. Etiam dui sem, fermentum vitae, sagittis id, malesuada in, quam. Proin mattis lacinia justo. Vestibulum facilisis auctor urna. Aliquam in lorem sit amet leo accumsan lacinia. Integer rutrum, orci vestibulum ullamcorper ultricies, lacus quam ultricies odio, vitae placerat pede sem sit amet enim. Phasellus et lorem id felis nonummy placerat. Fusce dui leo, imperdiet in, aliquam sit amet, feugiat eu, orci. Aenean vel massa quis mauris vehicula lacinia. Quisque tincidunt scelerisque libero. Maecenas libero. Etiam dictum tincidunt diam. Donec ipsum massa, ullamcorper in, auctor et, scelerisque sed, est. Suspendisse nisl. Sed convallis magna eu sem. Cras pede libero, dapibus nec, pretium";
-    let input = String {
-        bytes: Bytes(string.as_bytes().to_vec()),
-    };
+    let input = string.to_string();
 
     assert_eq!(
         instance.methods().stored_len().call().await.unwrap().value,
@@ -87,7 +83,7 @@ async fn stores_long_string() {
 
     assert_eq!(
         instance.methods().get_string().call().await.unwrap().value,
-        input.bytes
+        Bytes(input.as_bytes().to_vec())
     );
 }
 
@@ -97,12 +93,8 @@ async fn stores_string_twice() {
 
     let string1 = "Fuel is the fastest modular execution layer";
     let string2 = "Fuel is blazingly fast!";
-    let input1 = String {
-        bytes: Bytes(string1.as_bytes().to_vec()),
-    };
-    let input2 = String {
-        bytes: Bytes(string2.as_bytes().to_vec()),
-    };
+    let input1 = string1.to_string();
+    let input2 = string2.to_string();
 
     instance
         .methods()
@@ -118,7 +110,7 @@ async fn stores_string_twice() {
 
     assert_eq!(
         instance.methods().get_string().call().await.unwrap().value,
-        input1.bytes
+        Bytes(input1.as_bytes().to_vec())
     );
 
     instance
@@ -135,7 +127,7 @@ async fn stores_string_twice() {
 
     assert_eq!(
         instance.methods().get_string().call().await.unwrap().value,
-        input2.bytes
+        Bytes(input2.as_bytes().to_vec())
     );
 }
 
@@ -144,9 +136,7 @@ async fn clears_bytes() {
     let instance = setup().await;
 
     let string = "Fuel is blazingly fast!";
-    let input = String {
-        bytes: Bytes(string.as_bytes().to_vec()),
-    };
+    let input = string.to_string();
 
     instance
         .methods()
@@ -181,9 +171,7 @@ async fn get_string_length() {
     let instance = setup().await;
 
     let string = "Fuel is blazingly fast!";
-    let input = String {
-        bytes: Bytes(string.as_bytes().to_vec()),
-    };
+    let input = string.to_string();
 
     assert_eq!(
         instance.methods().stored_len().call().await.unwrap().value,
