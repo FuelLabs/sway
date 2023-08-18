a4404d13240fe0b3568109523908bdf53d8a7a22
xunilrj <xunilrj@hotmail.com>
2023-08-18 15:36:23 +0100

    remove dbg

--- sway-parse/src/attribute.rs
+++ sway-parse/src/attribute.rs
@@ -92,7 +92,6 @@ impl<T: Parse> Parse for Annotated<T> {
     where
         Self: Sized,
     {
-        dbg!(1, std::any::type_name::<T>());
         T::error(spans, error).map(|value| Annotated {
             attribute_list: vec![],
             value,
