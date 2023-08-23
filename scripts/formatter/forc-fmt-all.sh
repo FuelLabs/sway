18d24a1da07da7c690e4c7a8f084970b98f90b84
Sophie <sophiedankel@gmail.com>
2023-08-23 16:29:18 -0700

    Add comment on script

--- scripts/formatter/forc-fmt-all.sh
+++ scripts/formatter/forc-fmt-all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script will format all sway projects in the current directory and all subdirectories.
+# This is useful for testing the formatter itself to make sure it's not panicking on any valid
+# sway projects and for checking that it's formatted output is correct.
 forc_manifests=`find . -name Forc.toml`
 let count=0
 let failed=0
@@ -17,5 +20,3 @@ done
 echo ""
 echo "Failed count: $failed"
 echo "Total count: $count"
-
-
