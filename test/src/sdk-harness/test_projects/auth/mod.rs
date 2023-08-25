be5f36251c03dc6a32fe1406f8b69292537fbd9b
bitzoic <bitzoic.eth@gmail.com>
2023-08-25 13:18:02 -0400

    Add tests to auth

--- test/src/sdk-harness/test_projects/auth/mod.rs
+++ test/src/sdk-harness/test_projects/auth/mod.rs
@@ -3,6 +3,8 @@ use fuels::{
     prelude::*,
     types::ContractId,
 };
+use fuel_core_client::client::types::primitives::ChainId;
+use fuel_vm::fuel_tx::{field::*, Input as TxInput};
 
 abigen!(
     Contract(
@@ -54,6 +56,69 @@ async fn msg_sender_from_contract() {
         .unwrap();
 
     assert_eq!(result.value, true);
+}
+
+
+#[tokio::test]
+async fn msg_sender_message() {
+    let (auth_instance, _, _, _, wallet) = get_contracts().await;
+
+    const MESSAGE_DATA: [u8; 3] = [1u8, 2u8, 3u8];
+    let mut wallet = WalletUnlocked::new_random(None);
+    let mut coins = setup_single_asset_coins(wallet.address(), BASE_ASSET_ID, 100, 1000);
+    let messages = setup_single_message(
+        &Bech32Address {
+            hrp: "".to_string(),
+            hash: Default::default(),
+        },
+        wallet.address(),
+        DEFAULT_COIN_AMOUNT,
+        0.into(),
+        MESSAGE_DATA.to_vec(),
+    );
+    let (provider, _address) = setup_test_provider(coins.clone(), vec![messages], None, None).await;
+    wallet.set_provider(provider.clone());
+
+    let contract_id = Contract::load_from(
+        "test_artifacts/auth_testing_contract/out/debug/auth_testing_contract.bin",
+        LoadConfiguration::default(),
+    )
+    .unwrap()
+    .deploy(&wallet, TxParameters::default())
+    .await
+    .unwrap();
+
+    let instance = AuthContract::new(contract_id.clone(), wallet.clone());
+
+    let handler = auth_instance.methods().returns_msg_sender_address(wallet.address());
+    let mut tx = handler.build_tx().await.unwrap();
+    add_message_input(&mut tx, wallet.clone()).await;
+    tx.precompute(*ChainId::default()).unwrap();
+
+    let receipts = wallet
+        .provider()
+        .unwrap()
+        .send_transaction(&tx)
+        .await
+        .unwrap();
+
+    // true = 1
+    assert_eq!(*receipts[1].data().unwrap(), [1u8]);
+}
+
+async fn add_message_input(tx: &mut ScriptTransaction, wallet: WalletUnlocked) {
+    let message = &wallet.get_messages().await.unwrap()[0];
+
+    let message_input = TxInput::message_data_signed(
+        message.sender.clone().into(),
+        message.recipient.clone().into(),
+        message.amount,
+        message.nonce,
+        0,
+        message.data.clone(),
+    );
+
+    tx.tx.inputs_mut().push(message_input);
 }
 
 async fn get_contracts() -> (
