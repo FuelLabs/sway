94a3fa9820bab661b528dcd676347d80611ca296
Sophie <sophiedankel@gmail.com>
2023-08-28 17:29:57 -0700

    test utf-chars

--- scripts/formatter/forc-fmt-check-panic.sh
+++ scripts/formatter/forc-fmt-check-panic.sh
@@ -19,6 +19,13 @@ do
         echo $stderr
         echo ""
     fi
+
+    if [[ $dir == *"utf_chars"* ]]
+    then
+        forc-fmt -p $dir
+        echo ""
+    fi
+
     let count=count+1
 done
 echo ""
