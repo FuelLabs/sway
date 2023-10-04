f5ec66b588dd4edd10a2bdb9c3ae204aa2d4a16b
Cesar <142530682+cr-fuel@users.noreply.github.com>
2023-10-04 17:15:25 -0300

    Use - instead of _
    Context: https://github.com/FuelLabs/fuel-core/pull/1397#discussion_r1346382356

--- forc-plugins/forc-crypto/src/keygen/mod.rs
+++ forc-plugins/forc-crypto/src/keygen/mod.rs
@@ -3,11 +3,10 @@ use clap::ValueEnum;
 pub mod new_key;
 pub mod parse_secret;
 
-pub const BLOCK_PRODUCTION: &str = "block_production";
+pub const BLOCK_PRODUCTION: &str = "block-production";
 pub const P2P: &str = "p2p";
 
 #[derive(Clone, Debug, Default, ValueEnum)]
-#[clap(rename_all = "snake_case")]
 pub enum KeyType {
     #[default]
     BlockProduction,
