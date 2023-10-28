4daab4682dec510ad00f86b7253dd0128fe6f87c
Call Delegation <calldelegation@gmail.com>
2023-10-28 13:53:27 -0400

    Templating issues

--- templates/sway-predicate-test-rs/template/tests/harness.rs
+++ templates/sway-predicate-test-rs/template/tests/harness.rs
@@ -1,19 +1,15 @@
-use fuels::{
-    accounts::predicate::Predicate,
-    prelude::*,
-};
+use fuels::{accounts::predicate::Predicate, prelude::*};
 
 // Load abi from json
 abigen!(Predicate(
     name = "MyPredicate",
-    abi = "out/debug/{{project-name}}-abi.json"
+    abi = "out/debug/predicate-template-abi.json"
 ));
 
 async fn get_predicate_instance() -> Predicate {
-    let bin_path = "./out/debug/{{project-name}}.bin";
+    let bin_path = "./out/debug/predicate-template.bin";
 
-    let instance: Predicate = Predicate::load_from(bin_path)
-        .unwrap();
+    let instance: Predicate = Predicate::load_from(bin_path).unwrap();
 
     instance
 }
@@ -21,7 +17,7 @@ async fn get_predicate_instance() -> Predicate {
 #[tokio::test]
 async fn can_get_predicate_instance() {
     let instance = get_predicate_instance().await;
-    
+
     let _predicate_root = instance.address();
     // Now you have an instance of your predicate
 }
