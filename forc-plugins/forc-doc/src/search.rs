396336c0404e7c5ad84cea0594d9c9d708fb2bb0
Chris O'Brien <obrchr96@gmail.com>
2023-10-31 13:56:06 -0500

    update docs, rename json method

--- forc-plugins/forc-doc/src/search.rs
+++ forc-plugins/forc-doc/src/search.rs
@@ -9,13 +9,14 @@ const JSON_SEARCH_POOL_FILE_NAME: &str = "search_pool.json";
 pub(crate) fn write_search_pool_json(doc_path: &Path, docs: Documentation) -> Result<()> {
     Ok(serde_json::to_writer_pretty(
         fs::File::create(doc_path.join(JSON_SEARCH_POOL_FILE_NAME))?,
-        &docs.to_json_search_items()?,
+        &docs.to_json_value()?,
     )?)
 }
 
 impl Documentation {
-    /// Generates a mapping of program name to a vector of documentable items within the program.
-    fn to_json_search_items(&self) -> Result<serde_json::Value, serde_json::Error> {
+    /// Generates a mapping of program name to a vector of documentable items within the program
+    /// and returns the map as a `serde_json::Value`.
+    fn to_json_value(&self) -> Result<serde_json::Value, serde_json::Error> {
         let mut map: HashMap<String, Vec<JsonSearchItem>> = HashMap::with_capacity(self.0.len());
         for doc in self.0.iter() {
             match map.get_mut(doc.module_info.project_name()) {
@@ -34,6 +35,10 @@ impl Documentation {
     }
 }
 
+/// Item information used in the `search_pool.json`.
+/// The item name is what the fuzzy search will be
+/// matching on, all other information will be used
+/// in generating links to the item.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 struct JsonSearchItem {
     name: String,
