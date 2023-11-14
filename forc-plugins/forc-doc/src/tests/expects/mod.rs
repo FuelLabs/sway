8a92e1d685066d3192ddafb6fc755fe784935fd1
Sophie <sophiedankel@gmail.com>
2023-11-15 02:47:55 +0300

    cargo fmt

--- forc-plugins/forc-doc/src/tests/expects/mod.rs
+++ forc-plugins/forc-doc/src/tests/expects/mod.rs
@@ -7,7 +7,8 @@ mod impl_trait;
 
 pub(crate) fn check_file(doc_path: &PathBuf, path_to_file: &PathBuf, expect: &Expect) {
     let path = doc_path.join(path_to_file);
-    let actual = std::fs::read_to_string(path.clone()).expect(&format!("failed to read file: {:?}", path));
+    let actual =
+        std::fs::read_to_string(path.clone()).expect(&format!("failed to read file: {:?}", path));
     expect.assert_eq(&actual)
 }
 
