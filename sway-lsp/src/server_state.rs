2e07be517e5f04b14746398ddb5ce2e5abe21489
JoshuaBatty <joshpbatty@gmail.com>
2023-12-14 12:16:16 +1100

    only publish diagnostics on open and save events

--- sway-lsp/src/server_state.rs
+++ sway-lsp/src/server_state.rs
@@ -1,7 +1,6 @@
 //! The context or environment in which the language server functions.
 
 use crate::{
-    capabilities::diagnostic::get_diagnostics,
     config::{Config, Warnings},
     core::session::{self, Session},
     error::{DirectoryError, DocumentError, LanguageServerError},
@@ -13,10 +12,8 @@ use dashmap::DashMap;
 use forc_pkg::PackageManifestFile;
 use lsp_types::{Diagnostic, Url};
 use parking_lot::RwLock;
-use sway_core::Engines;
 use std::{path::PathBuf, sync::{Arc, atomic::{AtomicBool, Ordering}}};
 use tower_lsp::{jsonrpc, Client};
-use tokio::sync::watch;
 
 /// `ServerState` is the primary mutable state of the language server
 pub struct ServerState {
@@ -26,7 +23,6 @@ pub struct ServerState {
     pub(crate) sessions: Arc<Sessions>,
     pub(crate) retrigger_compilation: Arc<AtomicBool>,
     pub(crate) is_compiling: Arc<AtomicBool>,
-    pub(crate) watch_tx: Option<watch::Sender<Shared>>,
     pub(crate) mpsc_tx: Option<Sender<Shared>>,
     pub(crate) mpsc_rx: Option<Receiver<Shared>>,
 }
@@ -40,7 +36,6 @@ impl Default for ServerState {
             sessions: Arc::new(Sessions(DashMap::new())),
             retrigger_compilation: Arc::new(AtomicBool::new(false)),
             is_compiling: Arc::new(AtomicBool::new(false)),
-            watch_tx: None,
             mpsc_tx: None,
             mpsc_rx: None,
         }
@@ -64,55 +59,12 @@ impl ServerState {
         eprintln!("ServerState::new");
 
         let (mpsc_tx, mpsc_rx) = crossbeam_channel::bounded(1);
-
-        let (watch_tx, mut watch_rx) = watch::channel(Default::default());
         let state = ServerState {
             client: Some(client),
-            watch_tx: Some(watch_tx),
             mpsc_tx: Some(mpsc_tx),
             mpsc_rx: Some(mpsc_rx.clone()),
             ..Default::default()
         };
-
-        // let is_compiling = state.is_compiling.clone();
-        // let retrigger_compilation = state.retrigger_compilation.clone();
-        // tokio::spawn(async move {
-        //     eprintln!("spawning compilation thread");
-        //     while watch_rx.changed().await.is_ok() {
-        //         eprintln!("new compilation request");
-        //         is_compiling.store(true, Ordering::Relaxed);
-
-        //         // let(version, uri, session, engines_clone) = {
-        //         //     let shared = watch_rx.borrow();
-        //         //     let version = shared.version.unwrap();
-        //         //     let uri = shared.uri.as_ref().unwrap();
-        //         //     let session = shared.session.as_ref().unwrap();
-        //         //     let engines_clone = session.engines.read().clone();
-        //         //     (version.clone(), uri.clone(), session.clone(), engines_clone)
-        //         // };
-
-        //         let uri = watch_rx.borrow().uri.as_ref().unwrap().clone();
-        //         let version = watch_rx.borrow().version.unwrap();
-
-        //         eprintln!("starting parsing project: version: {:?}", version);
-        //         let parse_result = match session::parse_project(&uri, &Engines::default(), Some(retrigger_compilation.clone())) {
-        //             Ok(parse_result) => parse_result,
-        //             Err(err) => {
-        //                 eprintln!("{:?}", err);
-        //                 is_compiling.store(false, Ordering::Relaxed);
-        //                 retrigger_compilation.store(false, Ordering::Relaxed);
-        //                 return;
-        //             },
-        //         };
-        //         eprintln!("finished parsing project: version: {:?}", version);
-
-        //         //*session.engines.write() = engines_clone;
-        //         //session.write_parse_result(parse_result);
-
-        //         is_compiling.store(false, Ordering::Relaxed);
-        //         retrigger_compilation.store(false, Ordering::Relaxed);
-        //     }
-        // });
 
         let is_compiling = state.is_compiling.clone();
         let retrigger_compilation = state.retrigger_compilation.clone();
@@ -155,7 +107,58 @@ impl ServerState {
         Ok(())
     }
 
-    pub(crate) fn diagnostics(&self, uri: &Url, session: Arc<Session>) -> Vec<Diagnostic> {
+    pub(crate) async fn parse_project(
+        &self,
+        uri: &Url,
+        workspace_uri: &Url,
+        version: Option<i32>,
+        session: Arc<Session>,
+    ) {
+        self.is_compiling.store(true, Ordering::Relaxed);
+        match run_blocking_parse_project(uri.clone(), version, session.clone(), Some(self.retrigger_compilation.clone())).await {
+            Ok(_) => {
+                // Note: Even if the computed diagnostics vec is empty, we still have to push the empty Vec
+                // in order to clear former diagnostics. Newly pushed diagnostics always replace previously pushed diagnostics.
+                if let Some(client) = self.client.as_ref() {
+                    client
+                        .publish_diagnostics(
+                            workspace_uri.clone(),
+                            self.diagnostics(&uri, session),
+                            None,
+                        )
+                        .await;
+                }
+            }
+            Err(err) => {
+                if matches!(err, LanguageServerError::FailedToParse) {
+                    tracing::error!("Error parsing project: {:?}", err);
+                }
+            }
+        }
+        self.is_compiling.store(false, Ordering::Relaxed);
+        self.retrigger_compilation.store(false, Ordering::Relaxed);
+    }
+
+    pub(crate) async fn publish_diagnostics(
+        &self,
+        uri: Url,
+        workspace_uri: Url,
+        session: Arc<Session>,
+    ) {
+        // Note: Even if the computed diagnostics vec is empty, we still have to push the empty Vec
+        // in order to clear former diagnostics. Newly pushed diagnostics always replace previously pushed diagnostics.
+        if let Some(client) = self.client.as_ref() {
+            client
+                .publish_diagnostics(
+                    workspace_uri.clone(),
+                    self.diagnostics(&uri, session),
+                    None,
+                )
+                .await;
+        }
+    }
+
+    fn diagnostics(&self, uri: &Url, session: Arc<Session>) -> Vec<Diagnostic> {
         let mut diagnostics_to_publish = vec![];
         let config = &self.config.read();
         let tokens = session.token_map().tokens_for_file(uri);
@@ -184,39 +187,9 @@ impl ServerState {
         }
         diagnostics_to_publish
     }
-
-    pub(crate) async fn parse_project(
-        &self,
-        uri: Url,
-        workspace_uri: Url,
-        version: Option<i32>,
-        session: Arc<Session>,
-    ) {
-        self.is_compiling.store(true, Ordering::Relaxed);
-        match run_blocking_parse_project(uri.clone(), version, session.clone(), Some(self.retrigger_compilation.clone())).await {
-            Ok(_) => {
-                // Note: Even if the computed diagnostics vec is empty, we still have to push the empty Vec
-                // in order to clear former diagnostics. Newly pushed diagnostics always replace previously pushed diagnostics.
-                if let Some(client) = self.client.as_ref() {
-                    client
-                        .publish_diagnostics(
-                            workspace_uri.clone(),
-                            self.diagnostics(&uri, session),
-                            None,
-                        )
-                        .await;
-                }
-            }
-            Err(err) => {
-                if matches!(err, LanguageServerError::FailedToParse) {
-                    tracing::error!("Error parsing project: {:?}", err);
-                }
-            }
-        }
-        self.is_compiling.store(false, Ordering::Relaxed);
-        self.retrigger_compilation.store(false, Ordering::Relaxed);
-    }
 }
+
+
 
 /// Runs parse_project in a blocking thread, because parsing is not async.
 async fn run_blocking_parse_project(
@@ -232,7 +205,7 @@ async fn run_blocking_parse_project(
     }
     tokio::task::spawn_blocking(move || {
         // Lock the diagnostics result to prevent multiple threads from parsing the project at the same time.
-        let mut diagnostics = session.diagnostics.write();
+        //let mut diagnostics = session.diagnostics.write();
 
         if let Some(version) = version {
             // Garbage collection is fairly expsensive so we only clear on every 10th keystroke.
@@ -254,9 +227,7 @@ async fn run_blocking_parse_project(
         *session.engines.write() = engines_clone;
         eprintln!("parse_project: engines_write: {:?}", now.elapsed());
 
-        let (errors, warnings) = parse_result.diagnostics.clone();
         session.write_parse_result(parse_result);
-        *diagnostics = get_diagnostics(&warnings, &errors, session.engines.read().se());
         Ok(())
     })
     .await
