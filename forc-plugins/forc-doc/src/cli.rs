47b629b8cc054b9adb7c1c2b453f195e637a1c14
César D. Rodas <cesar@rodasm.com.py>
2023-12-19 20:07:19 -0300

    Update forc-plugins/forc-doc/src/cli.rs
    Co-authored-by: Braqzen <103777923+Braqzen@users.noreply.github.com>

--- forc-plugins/forc-doc/src/cli.rs
+++ forc-plugins/forc-doc/src/cli.rs
@@ -8,7 +8,7 @@ forc::cli_examples! {
     [ Build the docs for a project in the current path => doc ""]
     [ Build the docs for a project in the current path and open it in the browser => doc "--open" ]
     [ Build the docs for a project located in another path => doc "--manifest-path ../tests_project2" ]
-    [ Build the docs for the current project exportings private types => doc "--document-private-items" ]
+    [ Build the docs for the current project exporting private types => doc "--document-private-items" ]
     [ Build the docs offline without downloading any dependency from the network => doc "--offline" ]
 }
 
