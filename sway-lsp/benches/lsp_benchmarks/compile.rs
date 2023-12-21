48b15ebc257691babb9f7c65ddeca0e08a84dc4e
JoshuaBatty <joshpbatty@gmail.com>
2023-12-21 15:42:04 +1100

    fix compilation benchmarks

--- sway-lsp/benches/lsp_benchmarks/compile.rs
+++ sway-lsp/benches/lsp_benchmarks/compile.rs
@@ -1,39 +1,33 @@
 use criterion::{black_box, criterion_group, Criterion};
 use lsp_types::Url;
 use sway_core::Engines;
-use sway_lsp::core::session::{self, Session};
-use tokio::runtime::Runtime;
+use sway_lsp::core::session;
 
-const NUM_DID_CHANGE_ITERATIONS: usize = 4;
+const NUM_DID_CHANGE_ITERATIONS: usize = 10;
 
 fn benchmarks(c: &mut Criterion) {
     // Load the test project
     let uri = Url::from_file_path(super::benchmark_dir().join("src/main.sw")).unwrap();
-    let session = Runtime::new().unwrap().block_on(async {
-        let session = Session::new();
-        session.handle_open_file(&uri).await;
-        session
-    });
-
     c.bench_function("compile", |b| {
         b.iter(|| {
             let engines = Engines::default();
-            let _ = black_box(session::compile(&uri, &engines, None).unwrap());
+            let _ = black_box(session::compile(&uri, None, &engines, None).unwrap());
         })
     });
 
     c.bench_function("traverse", |b| {
         let engines = Engines::default();
-        let results = black_box(session::compile(&uri, &engines, None).unwrap());
+        let results = black_box(session::compile(&uri, None, &engines, None).unwrap());
         b.iter(|| {
             let _ = black_box(session::traverse(results.clone(), &engines).unwrap());
         })
     });
 
     c.bench_function("did_change_with_caching", |b| {
+        let engines = Engines::default();
         b.iter(|| {
-            for _ in 0..NUM_DID_CHANGE_ITERATIONS {
-                let _ = black_box(session::compile(&uri, &session.engines.read(), None).unwrap());
+            for version in 0..NUM_DID_CHANGE_ITERATIONS {
+                let _ = black_box(session::compile(&uri, Some(version as i32), &engines, None).unwrap());
             }
         })
     });
