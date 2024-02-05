491a07fa5d298c78cf28a9a89db327cdf6fd5e69
Marcos Henrich <marcoshenrich@gmail.com>
2024-02-05 14:05:48 +0000

    Fixes `ReplaceDecls` to no longer depend on filter hack. (#5541)
    `ReplaceDecls` was using a hack to replace some function refs based on
    the self function parameter, this didn't work for associated functions.
    
    Now DeclMapping sources also contain a typeid that guarantees that the
    replacement is done to the correct declaration in case we have nested
    trait methods with the same name replaced.
    
    Fixes https://github.com/FuelLabs/sway/issues/5504
    
    ## Checklist
    
    - [x] I have linked to any relevant issues.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have updated the documentation where relevant (API docs, the
    reference, and the Sway book).
    - [x] I have added tests that prove my fix is effective or that my
    feature works.
    - [x] I have added (or requested a maintainer to add) the necessary
    `Breaking*` or `New Feature` labels where relevant.
    - [x] I have done my best to ensure that my PR adheres to [the Fuel Labs
    Code Review
    Standards](https://github.com/FuelLabs/rfcs/blob/master/text/code-standards/external-contributors.md).
    - [x] I have requested a review from the relevant team or maintainers.
    
    Co-authored-by: Joshua Batty <joshpbatty@gmail.com>

--- sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
+++ sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
@@ -1672,10 +1672,10 @@ impl ty::TyExpression {
                     let method = decl_engine.get_trait_fn(decl_ref);
                     abi_items.push(TyImplItem::Fn(
                         decl_engine
-                            .insert(method.to_dummy_func(AbiMode::ImplAbiFn(
-                                abi_name.suffix.clone(),
-                                Some(*abi_ref.id()),
-                            )))
+                            .insert(method.to_dummy_func(
+                                AbiMode::ImplAbiFn(abi_name.suffix.clone(), Some(*abi_ref.id())),
+                                Some(return_type),
+                            ))
                             .with_parent(decl_engine, (*decl_ref.id()).into()),
                     ));
                 }
