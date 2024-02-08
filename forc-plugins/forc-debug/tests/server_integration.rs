a860ad4888cbbd52650a951f692176ff5c488939
Sophie <sophiedankel@gmail.com>
2024-02-08 13:50:51 -0800

    clippy

--- forc-plugins/forc-debug/tests/server_integration.rs
+++ forc-plugins/forc-debug/tests/server_integration.rs
@@ -269,10 +269,10 @@ fn assert_stopped_next_event(event: Option<Event>) {
 fn assert_output_event_body(event: Option<Event>) -> OutputEventBody {
     match event.expect("received event") {
         Event::Output(body) => {
-            return body;
+            body
         }
         other => panic!("Expected Output event, got {:?}", other),
-    };
+    }
 }
 
 fn assert_not_supported_event(event: Option<Event>) {
