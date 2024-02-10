7bcac37d987c84b77daaf2eb89bcb6d54ba0b189
César D. Rodas <cesar@rodasm.com.py>
2024-02-10 00:54:51 -0300

    Improve cli_examples macro (#5589)
    ## Description
    
    Add macro to test argument parsing rather than testing the external
    command through building and spawning a separated process. This is an
    improved version of #5519
    
    ## Checklist
    
    - [x] I have linked to any relevant issues.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [x] I have updated the documentation where relevant (API docs, the
    reference, and the Sway book).
    - [x] I have added tests that prove my fix is effective or that my
    feature works.
    - [x] I have added (or requested a maintainer to add) the necessary
    `Breaking*` or `New Feature` labels where relevant.
    - [x] I have done my best to ensure that my PR adheres to [the Fuel Labs
    Code Review
    Standards](https://github.com/FuelLabs/rfcs/blob/master/text/code-standards/external-contributors.md).
    - [x] I have requested a review from the relevant team or maintainers.

--- forc-plugins/forc-doc/src/cli.rs
+++ forc-plugins/forc-doc/src/cli.rs
@@ -5,11 +5,13 @@ use forc_pkg::source::IPFSNode;
 const ABOUT: &str = "Forc plugin for building a Sway package's documentation";
 
 forc_util::cli_examples! {
-    [ Build the docs for a project in the current path => doc ""]
-    [ Build the docs for a project in the current path and open it in the browser => doc "--open" ]
-    [ Build the docs for a project located in another path => doc "--manifest-path ../tests_project2" ]
-    [ Build the docs for the current project exporting private types => doc "--document-private-items" ]
-    [ Build the docs offline without downloading any dependency from the network => doc "--offline" ]
+    crate::Command {
+        [ Build the docs for a project in the current path => "forc doc"]
+        [ Build the docs for a project in the current path and open it in the browser => "forc doc --open" ]
+        [ Build the docs for a project located in another path => "forc doc --manifest-path {path}" ]
+        [ Build the docs for the current project exporting private types => "forc doc --document-private-items" ]
+        [ Build the docs offline without downloading any dependency from the network => "forc doc --offline" ]
+    }
 }
 
 #[derive(Debug, Parser, Default)]
