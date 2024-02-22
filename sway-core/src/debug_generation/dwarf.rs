ec29c60ed1587fb028edd43916e92c52c3f3d353
Vaivaswatha Nagaraj <vaivaswatha.nagaraj@fuel.sh>
2024-02-22 14:51:34 +0530

    dummy dwarf line number program into object file

--- sway-core/src/debug_generation/dwarf.rs
+++ sway-core/src/debug_generation/dwarf.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
+use std::io::Write;
 
 use gimli::write::{
-    self, Address, DebugLine, DebugLineStrOffsets, DebugStrOffsets, Dwarf, EndianVec, LineProgram,
+    self, DebugLine, DebugLineStrOffsets, DebugStrOffsets, DwarfUnit, EndianVec, LineProgram,
     LineString,
 };
-use gimli::{Encoding, Format, LineEncoding, LittleEndian};
+use gimli::{BigEndian, LineEncoding};
 use sway_error::handler::{ErrorEmitted, Handler};
 use sway_types::{SourceEngine, Span};
 
@@ -12,6 +13,8 @@ use crate::asm_generation::instruction_set::InstructionSet;
 
 use crate::source_map::SourceMap;
 use crate::CompiledAsm;
+
+use object::write::Object;
 
 use super::DebugInfo;
 
@@ -30,97 +33,91 @@ pub fn generate_debug_info(
         .filter_map(|source_id| match source_id {
             Some(source_id) => source_engine.get_file_name(source_id),
             None => None,
-        } )
+        })
         .collect();
 
     let dir1 = &b"dir1"[..];
     let file1 = &b"file1"[..];
     let file2 = &b"file2"[..];
-    let convert_address = &|address| Some(Address::Constant(address));
 
     let debug_line_str_offsets = DebugLineStrOffsets::none();
     let debug_str_offsets = DebugStrOffsets::none();
 
-    for &version in &[2, 3, 4, 5] {
-        for &address_size in &[4, 8] {
-            for &format in &[Format::Dwarf64] {
-                let encoding = Encoding {
-                    format,
-                    version,
-                    address_size,
-                };
-                let line_base = -5;
-                let line_range = 14;
-                let neg_line_base = (-line_base) as u8;
-                let mut program = LineProgram::new(
-                    encoding,
-                    LineEncoding {
-                        line_base,
-                        line_range,
-                        ..Default::default()
-                    },
-                    LineString::String(dir1.to_vec()),
-                    LineString::String(file1.to_vec()),
-                    None,
-                );
-                let dir_id = program.default_directory();
-                program.add_file(LineString::String(file1.to_vec()), dir_id, None);
-                let file_id = program.add_file(LineString::String(file2.to_vec()), dir_id, None);
+    let encoding = gimli::Encoding {
+        format: gimli::Format::Dwarf64,
+        version: 5,
+        address_size: 8,
+    };
+    let mut program = LineProgram::new(
+        encoding,
+        LineEncoding::default(),
+        LineString::String(dir1.to_vec()),
+        LineString::String(file1.to_vec()),
+        None,
+    );
+    let dir_id = program.default_directory();
+    program.add_file(LineString::String(file1.to_vec()), dir_id, None);
+    program.add_file(LineString::String(file2.to_vec()), dir_id, None);
 
-                // Test sequences.
-                {
-                    let mut program = program.clone();
-                    let address = Address::Constant(0x12);
-                    program.begin_sequence(Some(address));
-                }
+    // Create a base program.
+    program.begin_sequence(None);
+    //program.row.line = 0x1000;
 
-                // Create a base program.
-                program.begin_sequence(None);
-                //program.row.line = 0x1000;
+    program.generate_row();
+    let current_row = program.row();
+    current_row.line = 100;
 
-                program.generate_row();
-                let current_row = program.row();
-                current_row.line = 100;
+    program.end_sequence(0);
 
-                program.end_sequence(0);
+    let mut debug_line = DebugLine::from(EndianVec::new(BigEndian));
+    //let mut debug_line_offsets = Vec::new();
 
-                let mut debug_line = DebugLine::from(EndianVec::new(LittleEndian));
-                //let mut debug_line_offsets = Vec::new();
+    program
+        .write(
+            &mut debug_line,
+            encoding,
+            &debug_line_str_offsets,
+            &debug_str_offsets,
+        )
+        .map_err(|err| {
+            handler.emit_err(sway_error::error::CompileError::InternalOwned(
+                err.to_string(),
+                Span::dummy(),
+            ))
+        })?;
 
-                program.write(
-                    &mut debug_line,
-                    encoding,
-                    &debug_line_str_offsets,
-                    &debug_str_offsets,
-                );
-            }
-        }
-    }
-
-    let mut dwarf = Dwarf::new();
+    let mut dwarf = DwarfUnit::new(encoding);
+    dwarf.unit.line_program = program;
     // Write to new sections
-    let mut write_sections = write::Sections::new(EndianVec::new(LittleEndian));
+    let mut debug_sections = write::Sections::new(EndianVec::new(BigEndian));
     dwarf
-        .write(&mut write_sections)
+        .write(&mut debug_sections)
         .expect("Should write DWARF information");
 
-    let mut file = File::create("test.debug").unwrap();
-    write_sections
-        .for_each(|section_id, endian_vec| {
-            use std::io::prelude::*;
-
-            println!("{:?}", section_id);
-            let section_name_bytes = section_id.name().as_bytes();
-            file.write_all(&section_name_bytes.len().to_le_bytes())
-                .unwrap();
-            file.write_all(section_name_bytes).unwrap();
-
-            let data = endian_vec.slice();
-            file.write_all(&data.len().to_le_bytes()).unwrap();
-
+    let file = File::create("test.debug").unwrap();
+    let mut obj = Object::new(
+        object::BinaryFormat::Elf,
+        object::Architecture::X86_64,
+        object::Endianness::Big,
+    );
+    debug_sections
+        .for_each(|section_id, data| {
+            let sec = obj.add_section(
+                [].into(),
+                section_id.name().into(),
+                object::SectionKind::Other,
+            );
+            obj.set_section_data(sec, data.clone().into_vec(), 8);
             Ok::<(), ()>(())
         })
         .unwrap();
+
+    obj.write_stream(file).map_err(|err| {
+        handler.emit_err(sway_error::error::CompileError::InternalOwned(
+            err.to_string(),
+            Span::dummy(),
+        ))
+    })?;
 
     Ok(DebugInfo {})
 }
@@ -131,11 +128,8 @@ fn gather_source_file_spans(asm: &CompiledAsm) -> Vec<Span> {
         InstructionSet::Fuel { ops } => ops
             .iter()
             .filter_map(|op| match op.owning_span.as_ref() {
-                Some(span) => Some(dbg!(span.clone())),
-                None => {
-                    dbg!("Instruction provides no span", op);
-                    None
-                }
+                Some(span) => Some(span.clone()),
+                None => None,
             })
             .collect::<Vec<_>>(),
         InstructionSet::Evm { .. } => {
