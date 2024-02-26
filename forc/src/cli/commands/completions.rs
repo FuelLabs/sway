70cc82d71582285f0689e931523d3e6255fc33fc
Cesar Rodas <cesar@rodasm.com.py>
2024-02-26 13:54:32 -0300

    Add support for fig completions
    This is the first part of #5612, which will be split into several smaller PRs

--- forc/src/cli/commands/completions.rs
+++ forc/src/cli/commands/completions.rs
@@ -1,7 +1,35 @@
-use clap::Command as ClapCommand;
-use clap::{CommandFactory, Parser};
+use clap::{Command as ClapCommand, CommandFactory, Parser};
 use clap_complete::{generate, Generator, Shell};
 use forc_util::ForcResult;
+
+#[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, clap::ValueEnum)]
+enum Target {
+    /// Bourne Again SHell (bash)
+    Bash,
+    /// Elvish shell
+    Elvish,
+    /// Friendly Interactive SHell (fish)
+    Fish,
+    /// PowerShell
+    PowerShell,
+    /// Z SHell (zsh)
+    Zsh,
+    /// Fig
+    Fig,
+}
+
+impl ToString for Target {
+    fn to_string(&self) -> String {
+        match self {
+            Target::Bash => "bash".to_string(),
+            Target::Elvish => "elvish".to_string(),
+            Target::Fish => "fish".to_string(),
+            Target::PowerShell => "powershell".to_string(),
+            Target::Zsh => "zsh".to_string(),
+            Target::Fig => "fig".to_string(),
+        }
+    }
+}
 
 /// Generate tab-completion scripts for your shell
 #[derive(Debug, Parser)]
@@ -11,13 +39,20 @@ pub struct Command {
     /// [possible values: zsh, bash, fish, powershell, elvish]
     ///
     /// For more info: https://fuellabs.github.io/sway/latest/forc/commands/forc_completions.html
-    #[clap(short = 'S', long)]
-    shell: Shell,
+    #[clap(short = 'T', long, value_enum)]
+    target: Target,
 }
 
 pub(crate) fn exec(command: Command) -> ForcResult<()> {
     let mut cmd = super::super::Opt::command();
-    print_completions(command.shell, &mut cmd);
+    match command.target {
+        Target::Fig => print_completions(clap_complete_fig::Fig, &mut cmd),
+        Target::Bash => print_completions(Shell::Bash, &mut cmd),
+        Target::Elvish => print_completions(Shell::Elvish, &mut cmd),
+        Target::PowerShell => print_completions(Shell::PowerShell, &mut cmd),
+        Target::Zsh => print_completions(Shell::Zsh, &mut cmd),
+        Target::Fish => print_completions(Shell::Fish, &mut cmd),
+    }
     Ok(())
 }
 
