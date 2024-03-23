f87022b598d1d9235c74caf7374a9683b7c92222
Igor Rončević <ironcev@hotmail.com>
2024-03-23 21:05:45 +0100

    Add test duration and test writing guidelines

--- test/src/e2e_vm_tests/mod.rs
+++ test/src/e2e_vm_tests/mod.rs
@@ -22,6 +22,7 @@ use std::{
     path::{Path, PathBuf},
     sync::Arc,
 };
+use std::time::Instant;
 use sway_core::BuildTarget;
 use tokio::sync::Mutex;
 use tracing::Instrument;
@@ -659,6 +660,7 @@ pub async fn run(filter_config: &FilterConfig, run_config: &RunConfig) -> Result
     let mut number_of_tests_failed = 0;
     let mut failed_tests = vec![];
 
+    let start_time = Instant::now();
     for (i, test) in tests.into_iter().enumerate() {
         let cur_profile = if run_config.release {
             BuildProfile::RELEASE
@@ -714,6 +716,7 @@ pub async fn run(filter_config: &FilterConfig, run_config: &RunConfig) -> Result
 
         number_of_tests_executed += 1;
     }
+    let duration = Instant::now().duration_since(start_time);
 
     if number_of_tests_executed == 0 {
         if let Some(skip_until) = &filter_config.skip_until {
@@ -747,7 +750,7 @@ pub async fn run(filter_config: &FilterConfig, run_config: &RunConfig) -> Result
     } else {
         tracing::info!("_________________________________");
         tracing::info!(
-            "Sway tests result: {}. {} total, {} passed; {} failed; {} disabled",
+            "Sway tests result: {}. {} total, {} passed; {} failed; {} disabled [test duration: {:#?}]",
             if number_of_tests_failed == 0 {
                 "ok".green().bold()
             } else {
@@ -756,7 +759,8 @@ pub async fn run(filter_config: &FilterConfig, run_config: &RunConfig) -> Result
             total_number_of_tests,
             number_of_tests_executed - number_of_tests_failed,
             number_of_tests_failed,
-            disabled_tests.len()
+            disabled_tests.len(),
+            duration
         );
         if number_of_tests_failed > 0 {
             tracing::info!("{}", "Failing tests:".red().bold());
