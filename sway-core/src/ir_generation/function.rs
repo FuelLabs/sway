2bce7ede6470f7db91181baa36b3eab3990cc704
Vaivaswatha Nagaraj <vaivaswatha.nagaraj@fuel.sh>
2024-04-06 11:10:04 +0530

    cargo fmt

--- sway-core/src/ir_generation/function.rs
+++ sway-core/src/ir_generation/function.rs
@@ -303,12 +303,13 @@ impl<'eng> FnCompiler<'eng> {
             _ => return Ok(TerminatorValue::new(val, context)),
         };
 
-        let is_argument =
-            if let ty::TyExpressionVariant::VariableExpression { name, .. } = &ast_expr.expression {
-                self.function.get_arg(context, name.as_str()).is_some()
-            } else {
-                false
-            };
+        let is_argument = if let ty::TyExpressionVariant::VariableExpression { name, .. } =
+            &ast_expr.expression
+        {
+            self.function.get_arg(context, name.as_str()).is_some()
+        } else {
+            false
+        };
 
         let ptr_val = if is_argument {
             // The `ptr_to_int` instructions gets the address of a variable into an integer.
