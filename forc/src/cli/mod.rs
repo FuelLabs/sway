063ec688cce2abcfb9ce81bd8237ada0506e9796
Sophie <sophiedankel@gmail.com>
2024-05-13 15:48:48 -0700

    verbose > 0

--- forc/src/cli/mod.rs
+++ forc/src/cli/mod.rs
@@ -142,7 +142,7 @@ pub async fn run_cli() -> ForcResult<()> {
         Forc::ContractId(command) => contract_id::exec(command),
         Forc::PredicateRoot(command) => predicate_root::exec(command),
         Forc::Plugin(args) => {
-            let output = plugin::execute_external_subcommand(&args, opt.verbose)?;
+            let output = plugin::execute_external_subcommand(&args, opt.verbose > 0)?;
             let code = output
                 .status
                 .code()
