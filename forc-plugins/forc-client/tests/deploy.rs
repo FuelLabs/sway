db8ba0bb6c913b7547111d17c1b5164868df1ed1
Kaya Gökalp <kaya.gokalp@fuel.sh>
2024-07-03 03:39:09 -0700

    fix: deployment estimation deferred to sdk (#6212)

---
+++ forc-plugins/forc-client/tests/deploy.rs
@@ -0,0 +1,128 @@
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process::{Child, Command},
+    str::FromStr,
+};
+
+use forc::cli::shared::Pkg;
+use forc_client::{
+    cmd,
+    op::{deploy, DeployedContract},
+    NodeTarget,
+};
+use fuel_tx::{ContractId, Salt};
+use portpicker::Port;
+use tempfile::tempdir;
+use toml_edit::{Document, InlineTable, Item, Value};
+
+fn get_workspace_root() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("../")
+        .join("../")
+        .canonicalize()
+        .unwrap()
+}
+
+/// Return the path to the chain config file which is expected to be in
+/// `.github/workflows/local-node` from sway repo root.
+fn chain_config_path() -> PathBuf {
+    get_workspace_root()
+        .join(".github")
+        .join("workflows")
+        .join("local-testnode")
+}
+
+fn test_data_path() -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("test")
+        .join("data")
+        .canonicalize()
+        .unwrap()
+}
+
+fn run_node() -> (Child, Port) {
+    let port = portpicker::pick_unused_port().expect("No ports free");
+
+    let chain_config = chain_config_path();
+    let child = Command::new("fuel-core")
+        .arg("run")
+        .arg("--debug")
+        .arg("--db-type")
+        .arg("in-memory")
+        .arg("--port")
+        .arg(port.to_string())
+        .arg("--snapshot")
+        .arg(format!("{}", chain_config.display()))
+        .spawn()
+        .expect("Failed to start fuel-core");
+    (child, port)
+}
+
+/// Copy a directory recursively from `source` to `dest`.
+fn copy_dir(source: &Path, dest: &Path) -> anyhow::Result<()> {
+    fs::create_dir_all(dest)?;
+    for e in fs::read_dir(source)? {
+        let entry = e?;
+        let file_type = entry.file_type()?;
+        if file_type.is_dir() {
+            copy_dir(&entry.path(), &dest.join(entry.file_name()))?;
+        } else {
+            fs::copy(entry.path(), dest.join(entry.file_name()))?;
+        }
+    }
+    Ok(())
+}
+
+fn patch_manifest_file_with_path_std(manifest_dir: &Path) -> anyhow::Result<()> {
+    let toml_path = manifest_dir.join(sway_utils::constants::MANIFEST_FILE_NAME);
+    let toml_content = fs::read_to_string(&toml_path).unwrap();
+
+    let mut doc = toml_content.parse::<Document>().unwrap();
+    let new_std_path = get_workspace_root().join("sway-lib-std");
+
+    let mut std_dependency = InlineTable::new();
+    std_dependency.insert("path", Value::from(new_std_path.display().to_string()));
+    doc["dependencies"]["std"] = Item::Value(Value::InlineTable(std_dependency));
+
+    fs::write(&toml_path, doc.to_string()).unwrap();
+    Ok(())
+}
+
+#[tokio::test]
+async fn simple_deploy() {
+    let (mut node, port) = run_node();
+    let tmp_dir = tempdir().unwrap();
+    let project_dir = test_data_path().join("standalone_contract");
+    copy_dir(&project_dir, tmp_dir.path()).unwrap();
+    patch_manifest_file_with_path_std(tmp_dir.path()).unwrap();
+
+    let pkg = Pkg {
+        path: Some(tmp_dir.path().display().to_string()),
+        ..Default::default()
+    };
+
+    let node_url = format!("http://127.0.0.1:{}/v1/graphql", port);
+    let target = NodeTarget {
+        node_url: Some(node_url),
+        target: None,
+        testnet: false,
+    };
+    let cmd = cmd::Deploy {
+        pkg,
+        salt: Some(vec![format!("{}", Salt::default())]),
+        node: target,
+        default_signer: true,
+        ..Default::default()
+    };
+    let contract_ids = deploy(cmd).await.unwrap();
+    node.kill().unwrap();
+    let expected = vec![DeployedContract {
+        id: ContractId::from_str(
+            "428896412bda8530282a7b8fca5d20b2a73f30037612ca3a31750cf3bf0e976a",
+        )
+        .unwrap(),
+    }];
+
+    assert_eq!(contract_ids, expected)
+}
