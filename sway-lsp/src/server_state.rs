474d384f3b2f12b8deec46e62bdb88c11335b1be
JoshuaBatty <joshpbatty@gmail.com>
2024-07-08 09:51:55 +1000

    add tracing_info! macro to functions we want to track in the server

--- sway-lsp/src/server_state.rs
+++ sway-lsp/src/server_state.rs
@@ -257,6 +257,7 @@ impl ServerState {
     }
 
     pub fn shutdown_server(&self) -> jsonrpc::Result<()> {
+        let _p = tracing::trace_span!("shutdown_server").entered();
         tracing::info!("Shutting Down the Sway Language Server");
 
         // Drain pending compilation requests
