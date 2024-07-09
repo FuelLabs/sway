08fa8f196082145e48f31b99baa01fdf2b097270
xunilrj <xunilrj@hotmail.com>
2024-07-09 10:41:53 +0100

    update contract id interactive option

--- test/update-contract-ids.sh
+++ test/update-contract-ids.sh
@@ -1,9 +1,27 @@
 #! /bin/bash
 
-CHANGES=$(git status --porcelain | wc -l)
-if [ "$CHANGES" != "0" ]; then
-  echo "git state is not clean. commit or restore first."
-  exit
+print_help() {
+    echo "
+ -i          run interactively
+";
+}
+
+INTERACTIVELY="0"
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -i) INTERACTIVELY="1"; shift ;;
+        *) print_help; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ "$INTERACTIVELY" == "0" ]]; then
+  CHANGES=$(git status --porcelain | wc -l)
+  if [ "$CHANGES" != "0" ]; then
+    echo "git state is not clean. commit or restore first."
+    exit
+  fi
 fi
 
 BOLD_RED='\033[1;31m'
@@ -19,13 +37,25 @@ function join_by {
   fi
 }
 
+replace () {
+    if [ $# -lt 2 ]
+    then
+        echo "Recursive, interactive text replacement"
+        echo "Usage: replace text replacement"
+        return
+    fi
+
+    vim -u NONE -c ":execute ':argdo %s/$1/$2/gc | update' | :q" $(ag $1 -l)
+}
+
 grep --include \*.sw -Hno "// AUTO-CONTRACT-ID" . -R | while read line ; do
     PARTS=($(echo $line | sed 's/:/ /g'))
     FOLDER=$(dirname ${PARTS[0]})
     FILE=${PARTS[0]}
     LINE=${PARTS[1]}
 
-    CONTRACT_ARGS=($(sed "$LINE!d" $FILE))
+    SED_COMMAND="${LINE}"'!d'
+    CONTRACT_ARGS=($(sed "$SED_COMMAND" $FILE))
     CONTRACT_ARGS=$(join_by " " ${CONTRACT_ARGS[@]:6})
 
     if [[ $CONTRACT_ARGS ]]; then 
@@ -37,8 +67,13 @@ grep --include \*.sw -Hno "// AUTO-CONTRACT-ID" . -R | while read line ; do
 
         if [[ $CONTRACT_ID ]]; then 
             popd >> /dev/null
-            sed -i "${LINE}s/0x[a-zA-Z0-9]*/$CONTRACT_ID/g" $FILE
-            echo -e "    ${BOLD_GREEN}ok${NC} ($CONTRACT_ID)"
+
+            if [[ "$INTERACTIVELY" == "0" ]]; then
+              sed -i "${LINE}s/0x[a-zA-Z0-9]*/$CONTRACT_ID/g" $FILE
+              echo -e "    ${BOLD_GREEN}ok${NC} ($CONTRACT_ID)"
+            else
+              echo "$FILE" | xargs -o vim -n -c ":${LINE}s/0x[a-zA-Z0-9]*/$CONTRACT_ID/gc" -c ":x"
+            fi
         else
             echo -e "    ${BOLD_RED}error${NC}"
             cargo r -p forc --release -- contract-id --release
