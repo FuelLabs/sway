c58fc0277848c3aac799acf9c1c49ea1c6cfd99b
xunilrj <xunilrj@hotmail.com>
2024-07-09 12:27:12 +0100

    interactivity without an editor

--- test/update-contract-ids.sh
+++ test/update-contract-ids.sh
@@ -16,13 +16,8 @@ while [[ "$#" -gt 0 ]]; do
     shift
 done
 
-if [[ "$INTERACTIVELY" == "0" ]]; then
-  CHANGES=$(git status --porcelain | wc -l)
-  if [ "$CHANGES" != "0" ]; then
-    echo "git state is not clean. commit or restore first."
-    exit
-  fi
-fi
+
+CHANGES=$(git status --porcelain | wc -l)
 
 BOLD_RED='\033[1;31m'
 BOLD_GREEN="\033[1;32m"
@@ -35,6 +30,19 @@ function join_by {
   if shift 2; then
     printf %s "$f" "${@/#/$d}"
   fi
+}
+
+ask_confirmation() {
+    echo -e "${BOLD_WHITE}Command below needs confirmation before running.${NC} $2"
+    echo "> $1"
+    read -p "Run the command above? (y/n) " -n 1 -r </dev/tty
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        bash -c "$1"
+    else
+        exit 1
+    fi
 }
 
 grep --include \*.sw -Hno "// AUTO-CONTRACT-ID" . -R | while read line ; do
@@ -57,11 +65,31 @@ grep --include \*.sw -Hno "// AUTO-CONTRACT-ID" . -R | while read line ; do
         if [[ $CONTRACT_ID ]]; then 
             popd >> /dev/null
 
-            if [[ "$INTERACTIVELY" == "0" ]]; then
-              sed -i "${LINE}s/0x[a-zA-Z0-9]*/$CONTRACT_ID/g" $FILE
-              echo -e "    ${BOLD_GREEN}ok${NC} ($CONTRACT_ID)"
-            else
-              echo "$FILE" | xargs -o vim -n -c ":${LINE}s/0x[a-zA-Z0-9]*/$CONTRACT_ID/gc" -c ":x"
+            SED_EXPR="${LINE}s/0x[a-zA-Z0-9]*/$CONTRACT_ID/g"
+
+            # check if there is a diff
+            diff -s --color <(cat $FILE) <(cat $FILE | sed --expression="$SED_EXPR") > /dev/null
+            if [ $? -eq 0 ]; then
+              # no diff, continue
+              echo -e "    ${BOLD_GREEN}no changes needed${NC} ($CONTRACT_ID)"
+            else 
+              # diff detected, check we are clean to update files, if not abort
+              if [[ "$INTERACTIVELY" == "0" ]]; then
+                # Don´t change anything if git is dirty
+                if [ "$CHANGES" != "0" ]; then
+                  echo -e "    ${BOLD_RED}Aborting${NC} This contract id needs update, but git state is not clean. commit, restore first or run with \"-i\"."
+                  echo $FILE
+                  diff -s --color <(cat $FILE) <(cat $FILE | sed --expression="$SED_EXPR")
+                  exit
+                fi
+                # we are clean and can update files
+                sed -i "$SED_EXPR" $FILE
+              else
+                # ask confirmation before applying the change
+                diff -s --color <(cat $FILE) <(cat $FILE | sed --expression="$SED_EXPR")
+                ask_confirmation "sed -i \"$SED_EXPR\" $FILE" "Update contract id"
+              fi
+              echo -e "    ${BOLD_GREEN}updated${NC} ($CONTRACT_ID)"
             fi
         else
             echo -e "    ${BOLD_RED}error${NC}"
