426d2daa1e906163e6451190b172ba965c63eb27
Kaya Gokalp <kayagokalp123@gmail.com>
2024-08-09 01:12:31 +0300

    test: chunked deploy with proxy

--- forc-plugins/forc-client/tests/deploy.rs
+++ forc-plugins/forc-client/tests/deploy.rs
@@ -501,3 +501,66 @@ async fn chunked_deploy_re_routes_call() {
     node.kill().unwrap();
     assert!(result)
 }
+
+#[tokio::test]
+async fn chunked_deploy_with_proxy_re_routes_call() {
+    let (mut node, port) = run_node();
+    let tmp_dir = tempdir().unwrap();
+    let project_dir = test_data_path().join("big_contract");
+    copy_dir(&project_dir, tmp_dir.path()).unwrap();
+    patch_manifest_file_with_path_std(tmp_dir.path()).unwrap();
+    let proxy = Proxy {
+        enabled: true,
+        address: None,
+    };
+    patch_manifest_file_with_proxy_table(tmp_dir.path(), proxy).unwrap();
+
+    let pkg = Pkg {
+        path: Some(tmp_dir.path().display().to_string()),
+        ..Default::default()
+    };
+
+    let node_url = format!("http://127.0.0.1:{}/v1/graphql", port);
+    let target = NodeTarget {
+        node_url: Some(node_url.clone()),
+        target: None,
+        testnet: false,
+    };
+    let cmd = cmd::Deploy {
+        pkg,
+        salt: Some(vec![format!("{}", Salt::default())]),
+        node: target,
+        default_signer: true,
+        ..Default::default()
+    };
+    let deployed_contract = deploy(cmd).await.unwrap().remove(0);
+
+    let provider = Provider::connect(&node_url).await.unwrap();
+    let secret_key = SecretKey::from_str(forc_client::constants::DEFAULT_PRIVATE_KEY).unwrap();
+    let wallet_unlocked = WalletUnlocked::new_from_private_key(secret_key, Some(provider));
+
+    abigen!(Contract(
+        name = "BigContract",
+        abi = "forc-plugins/forc-client/test/data/big_contract/big_contract-abi.json"
+    ));
+
+    let instance = BigContract::new(deployed_contract.id, wallet_unlocked);
+    let chunks: Vec<Bech32ContractId> = deployed_contract
+        .chunks
+        .iter()
+        .cloned()
+        .map(|chunk_id| chunk_id.into())
+        .collect();
+
+    // result should be true.
+    let result = instance
+        .methods()
+        .test_function()
+        .with_contract_ids(&chunks)
+        .call()
+        .await
+        .unwrap()
+        .value;
+    node.kill().unwrap();
+    assert!(result)
+}
