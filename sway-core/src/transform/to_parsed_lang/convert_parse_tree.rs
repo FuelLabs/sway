5b844a49a18f8ff3a86f241c1277d70bf158d1a8
Jacob Johannsen <jajocnn@gmail.com>
2024-08-21 01:33:41 +0200

    Move error report to convert_part_tree to ensure parser recovery

--- sway-core/src/transform/to_parsed_lang/convert_parse_tree.rs
+++ sway-core/src/transform/to_parsed_lang/convert_parse_tree.rs
@@ -2679,12 +2679,10 @@ fn statement_to_ast_nodes(
         Statement::Expr { expr, .. } => {
             vec![expr_to_ast_node(context, handler, engines, expr, true)?]
         }
-        Statement::Error(spans, error) => {
-            let span = Span::join_all(spans.iter().cloned());
-            vec![AstNode {
-                content: AstNodeContent::Error(spans, error),
-                span,
-            }]
+        Statement::Error(_spans, error) => {
+	    // This occurs if the parser has encountered an error, but has attempted to recover from
+	    // it.  If this is the case, we report the error now.
+	    return Err(error)
         }
     };
     Ok(ast_nodes)
