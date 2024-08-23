91e1ae07ce1d0c667bed1481687fc2084635df8c
xunilrj <xunilrj@hotmail.com>
2024-08-23 15:40:21 +0100

    check if all required generic arguments are valid

--- sway-core/src/language/ty/expression/expression.rs
+++ sway-core/src/language/ty/expression/expression.rs
@@ -111,6 +111,24 @@ impl TypeCheckAnalysis for TyExpression {
         ctx: &mut TypeCheckAnalysisContext,
     ) -> Result<(), ErrorEmitted> {
         match &self.expression {
+            TyExpressionVariant::FunctionApplication { type_binding, .. } => {
+                if let Some(type_binding) = type_binding {
+                    let args = match &type_binding.type_arguments {
+                        TypeArgs::Regular(args) | TypeArgs::Prefix(args) => args,
+                    };
+                    for arg in args {
+                        match &*ctx.engines.te().get(arg.type_id) {
+                            TypeInfo::Placeholder(_) => {
+                                let _ = handler.emit_err(CompileError::UnableToInferGeneric {
+                                    ty: ctx.engines.help_out(arg.type_id).to_string(),
+                                    span: arg.span.clone(),
+                                });
+                            }
+                            _ => {}
+                        }
+                    }
+                }
+            }
             // Check literal "fits" into assigned typed.
             TyExpressionVariant::Literal(Literal::Numeric(literal_value)) => {
                 let t = ctx.engines.te().get(self.return_type);
--- test/src/e2e_vm_tests/test_programs/should_fail/missing_type_parameters/src/main.sw
+++ test/src/e2e_vm_tests/test_programs/should_fail/missing_type_parameters/src/main.sw
@@ -2,9 +2,16 @@ script;
 
 fn main() {
     let g: bool = three_generics(true, "foo", 10);
+    
+    // Should fail because compiler cannot infer generic argument
+    one_generic();
 }
 
 fn three_generics(a: A, b: B, c: C) -> A {
     let new_a: A = a;
     new_a
+}
+
+fn one_generic<T>() {
+
 }
