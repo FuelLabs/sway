dd379228ce93460bdb22dd3eca8385d8b4bcedf3
SwayStar123 <swayambhanded@gmail.com>
2024-09-12 12:35:07 +0530

    fix predicate address

--- test/src/sdk-harness/test_projects/auth/mod.rs
+++ test/src/sdk-harness/test_projects/auth/mod.rs
@@ -624,7 +624,7 @@ async fn can_get_predicate_address() {
 
     // Setup predicate.
     let hex_predicate_address: &str =
-        "0x929d56285454b2c9c876a1e90c8084d1cd602fde50455bd70482364cb3f15b72";
+        "0x5dcc82a88eebb07fb628db93d11ec38f085cbf36453a7135fea41b93cc44e118";
     let predicate_address =
         Address::from_str(hex_predicate_address).expect("failed to create Address from string");
     let predicate_bech32_address = Bech32Address::from(predicate_address);
@@ -750,7 +750,7 @@ async fn when_incorrect_predicate_address_passed() {
 async fn can_get_predicate_address_in_message() {
     // Setup predicate address.
     let hex_predicate_address: &str =
-        "0x929d56285454b2c9c876a1e90c8084d1cd602fde50455bd70482364cb3f15b72";
+        "0x5dcc82a88eebb07fb628db93d11ec38f085cbf36453a7135fea41b93cc44e118";
     let predicate_address =
         Address::from_str(hex_predicate_address).expect("failed to create Address from string");
     let predicate_bech32_address = Bech32Address::from(predicate_address);
