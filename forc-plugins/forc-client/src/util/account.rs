95e1ad2b3a7225a1e9d1c0bb076e5ba16d56cc0a
Kaya Gokalp <kayagokalp123@gmail.com>
2024-09-22 00:28:18 -0700

    simplfy lifetimes

--- forc-plugins/forc-client/src/util/account.rs
+++ forc-plugins/forc-client/src/util/account.rs
@@ -1,3 +1,4 @@
+use async_trait::async_trait;
 use fuel_crypto::{Message, Signature};
 use fuels::{
     prelude::*,
@@ -5,39 +6,25 @@ use fuels::{
 };
 use fuels_accounts::{wallet::WalletUnlocked, Account};
 
-/// Accounts that can be used with forc-client.
 #[derive(Clone, Debug)]
 pub enum ForcClientAccount {
     Wallet(WalletUnlocked),
     KmsSigner,
 }
 
+#[async_trait]
 impl Account for ForcClientAccount {
-    /// Returns a vector consisting of `Input::Coin`s and `Input::Message`s for the given
-    /// asset ID and amount. The `witness_index` is the position of the witness (signature)
-    /// in the transaction\'s list of witnesses. In the validation process, the node will
-    /// use the witness at this index to validate the coins returned by this method.
-    #[must_use]
-    #[allow(clippy::type_complexity, clippy::type_repetition_in_bounds)]
-    fn get_asset_inputs_for_amount<'life0, 'async_trait>(
-        &'life0 self,
+    async fn get_asset_inputs_for_amount(
+        &self,
         asset_id: AssetId,
         amount: u64,
         excluded_coins: Option<Vec<CoinTypeId>>,
-    ) -> ::core::pin::Pin<
-        Box<
-            dyn ::core::future::Future<Output = Result<Vec<Input>>>
-                + ::core::marker::Send
-                + 'async_trait,
-        >,
-    >
-    where
-        'life0: 'async_trait,
-        Self: 'async_trait,
-    {
+    ) -> Result<Vec<Input>> {
         match self {
             ForcClientAccount::Wallet(wallet) => {
-                wallet.get_asset_inputs_for_amount(asset_id, amount, excluded_coins)
+                wallet
+                    .get_asset_inputs_for_amount(asset_id, amount, excluded_coins)
+                    .await
             }
             ForcClientAccount::KmsSigner => todo!(),
         }
@@ -60,23 +47,11 @@ impl ViewOnlyAccount for ForcClientAccount {
     }
 }
 
+#[async_trait]
 impl Signer for ForcClientAccount {
-    fn sign<'life0, 'async_trait>(
-        &'life0 self,
-        message: Message,
-    ) -> ::core::pin::Pin<
-        Box<
-            dyn ::core::future::Future<Output = Result<Signature>>
-                + ::core::marker::Send
-                + 'async_trait,
-        >,
-    >
-    where
-        'life0: 'async_trait,
-        Self: 'async_trait,
-    {
+    async fn sign(&self, message: Message) -> Result<Signature> {
         match self {
-            ForcClientAccount::Wallet(wallet) => wallet.sign(message),
+            ForcClientAccount::Wallet(wallet) => wallet.sign(message).await,
             ForcClientAccount::KmsSigner => todo!(),
         }
     }
