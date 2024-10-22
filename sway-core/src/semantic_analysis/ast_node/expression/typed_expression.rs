d3c83d6865c87bfbb3d7614b2b200e6bbfc87d04
xunilrj <xunilrj@hotmail.com>
2024-10-22 17:16:42 +0100

    better handler of error in array items

--- sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
+++ sway-core/src/semantic_analysis/ast_node/expression/typed_expression.rs
@@ -2001,15 +2001,16 @@ impl ty::TyExpression {
                     .with_help_text("")
                     .with_type_annotation(type_engine.insert(engines, initial_type.clone(), None));
 
-                if let TypeInfo::Unknown = initial_type {
-                    // type_check_analyze unification will give the final error
-                    let handler = Handler::default();
-                    Self::type_check(&handler, ctx, expr)
-                        .unwrap_or_else(|err| ty::TyExpression::error(err, span, engines))
-                } else {
-                    Self::type_check(handler, ctx, expr)
-                        .unwrap_or_else(|err| ty::TyExpression::error(err, span, engines))
+                // type_check_analyze unification will give the final error
+                let type_check_handler = Handler::default();
+                let result = Self::type_check(&type_check_handler, ctx, expr)
+                    .unwrap_or_else(|err| ty::TyExpression::error(err, span, engines));
+
+                if let TypeInfo::ErrorRecovery(_) = &*engines.te().get(result.return_type) {
+                    handler.append(type_check_handler);
                 }
+
+                result
             })
             .collect();
 
@@ -2987,7 +2988,7 @@ mod tests {
         let _comp_res = do_type_check_for_boolx2(&handler, &expr);
         let (errors, _warnings) = handler.consume();
 
-        assert!(errors.len() == 1);
+        assert_eq!(errors.len(), 1);
         assert!(matches!(&errors[0],
                          CompileError::TypeError(TypeError::MismatchedType {
                              expected,
