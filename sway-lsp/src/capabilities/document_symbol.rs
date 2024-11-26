3b611b619a1c66337a3a5178cab41eee09f425c3
JoshuaBatty <joshpbatty@gmail.com>
2024-11-27 10:32:15 +1100

    clippy

--- sway-lsp/src/capabilities/document_symbol.rs
+++ sway-lsp/src/capabilities/document_symbol.rs
@@ -22,7 +22,7 @@ pub fn to_document_symbols(
     engines: &Engines,
     token_map: &TokenMap,
 ) -> Vec<DocumentSymbol> {
-    let source_id = engines.se().get_source_id(&path);
+    let source_id = engines.se().get_source_id(path);
 
     // Find if there is a configurable symbol in the token map that belongs to the current file
     // We will add children symbols to this when we encounter a configurable declarations below.
