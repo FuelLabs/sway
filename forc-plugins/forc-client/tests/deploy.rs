095c4864035934bdafc30e966d26636af352bdc1
xunilrj <xunilrj@hotmail.com>
2024-12-10 18:04:50 +0000

    fixing tests

--- forc-plugins/forc-client/tests/deploy.rs
+++ forc-plugins/forc-client/tests/deploy.rs
@@ -1225,12 +1225,13 @@ async fn call_with_forc_generated_overrides(node_url: &str, contract_id: Contrac
     // we are ensuring forc-deploy indeed generated the files we are basing our
     // tests below.
     let generated_loader_abi_path = tmp_dir.path().join("out/deployed_script-loader-abi.json");
-    let generated_loader_abi = fs::read_to_string(generated_loader_abi_path).unwrap();
+    let generated_loader_abi = fs::read_to_string(&generated_loader_abi_path).unwrap();
 
     // this path is basically, `forc-plugins/forc-client/test/data/deployed_script/deployed_script-loader-abi.json`.
     let used_loader_abi_path = project_dir.join("deployed_script-loader-abi.json");
-    let used_loader_abi = fs::read_to_string(used_loader_abi_path).unwrap();
+    let used_loader_abi = fs::read_to_string(&used_loader_abi_path).unwrap();
 
+    dbg!(&generated_loader_abi_path, &used_loader_abi_path);
     pretty_assertions::assert_eq!(generated_loader_abi, used_loader_abi);
 
     let generated_loader_bin = tmp_dir.path().join("out/deployed_script-loader.bin");
