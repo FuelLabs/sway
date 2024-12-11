44581b8738c9405ce32b243afdb27b19887c4ad0
Marcos Henrich <marcoshenrich@gmail.com>
2024-12-11 10:19:44 +0000

    Minor fixes.

--- sway-core/src/semantic_analysis/type_check_context.rs
+++ sway-core/src/semantic_analysis/type_check_context.rs
@@ -2,7 +2,7 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::{
-    decl_engine::{DeclEngineGet, DeclRefFunction},
+    decl_engine::{DeclEngineGet, DeclEngineGetParsedDeclId, DeclEngineInsert, DeclRefFunction},
     engine_threading::*,
     language::{
         parsed::TreeType,
