2c306b4f892bb771730cfbd56104c667cedb2c58
Igor Rončević <ironcev@hotmail.com>
2025-01-11 00:09:18 +0100

    Update SDK harness Forc.lock file

--- test/src/sdk-harness/test_projects/harness.rs
+++ test/src/sdk-harness/test_projects/harness.rs
@@ -41,7 +41,6 @@ mod storage_map;
 mod storage_map_nested;
 mod storage_string;
 mod storage_vec;
-mod storage_vec_iter;
 mod storage_vec_nested;
 mod storage_vec_of_storage_string;
 mod storage_vec_to_vec;
