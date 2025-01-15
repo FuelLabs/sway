37235f2e7d0f1b693bada185d296994f84facf25
Hannes Karppila <2204863+Dentosal@users.noreply.github.com>
2025-01-16 01:49:25 +0200

    codegen: Use idiomatic fuel-vm ops for memcpy and addr_of (#6795)
    ## Description
    
    SIgnificantly reduces bytecode sizes by transforming the following
    commonly-used operations when the constant value fits into the immediate
    part of the `*i` variant of the instruction:
    
    * `MOVI(tmp, copy_len); MCP(dst, src, tmp)` → `MCPI(dst, src, copy_len)`
    (in `compile_mem_copy_bytes`)
    * `MOVI(dst, offset); ADD(dst, dst, DATA_SECTION)` → `ADDI(dst,
    DATA_SECTION, offset)` (in `addr_of`)
    
    ## Checklist
    
    - [ ] I have linked to any relevant issues. (none that I know of)
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have updated the documentation where relevant (API docs, the
    reference, and the Sway book). (no changes required)
    - [ ] If my change requires substantial documentation changes, I have
    [requested support from the DevRel
    team](https://github.com/FuelLabs/devrel-requests/issues/new/choose)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works. (partially; some values in tests got smaller. we don't
    seem to be tracking the size and size regressions in tests)
    - [x] I have added (or requested a maintainer to add) the necessary
    `Breaking*` or `New Feature` labels where relevant. (not relevant)
    - [x] I have done my best to ensure that my PR adheres to [the Fuel Labs
    Code Review
    Standards](https://github.com/FuelLabs/rfcs/blob/master/text/code-standards/external-contributors.md).
    - [x] I have requested a review from the relevant team or maintainers.

--- test/src/sdk-harness/test_projects/auth/mod.rs
+++ test/src/sdk-harness/test_projects/auth/mod.rs
@@ -618,7 +618,7 @@ async fn can_get_predicate_address() {
 
     // Setup predicate.
     let hex_predicate_address: &str =
-        "0x8b300a68337368654e71c65ae93c3d9eb3b9837d0c11d770cbf8740a6a5a8631";
+        "0x599331f8a4696d67739a28360222f1a671e349ad51ccd0682be19a683b058d84";
     let predicate_address =
         Address::from_str(hex_predicate_address).expect("failed to create Address from string");
     let predicate_bech32_address = Bech32Address::from(predicate_address);
@@ -744,7 +744,7 @@ async fn when_incorrect_predicate_address_passed() {
 async fn can_get_predicate_address_in_message() {
     // Setup predicate address.
     let hex_predicate_address: &str =
-        "0x8b300a68337368654e71c65ae93c3d9eb3b9837d0c11d770cbf8740a6a5a8631";
+        "0x599331f8a4696d67739a28360222f1a671e349ad51ccd0682be19a683b058d84";
     let predicate_address =
         Address::from_str(hex_predicate_address).expect("failed to create Address from string");
     let predicate_bech32_address = Bech32Address::from(predicate_address);
