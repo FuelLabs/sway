0ec65c0a4d627fa6d2d27e3e5248205a8cb98b4a
xunilrj <xunilrj@hotmail.com>
2025-01-28 13:43:38 -0300

    better data section tests

--- sway-ir/src/error.rs
+++ sway-ir/src/error.rs
@@ -15,6 +15,7 @@ pub enum IrError {
     RemoveMissingBlock(String),
     ValueNotFound(String),
     InconsistentParent(String, String, String),
+    InvalidConfigFlags,
 
     VerifyArgumentValueIsNotArgument(String),
     VerifyUnaryOpIncorrectArgType,
@@ -124,6 +125,12 @@ impl fmt::Display for IrError {
                     f,
                     "For IR Entity (module/function/block) {entity}, expected parent to be {expected_parent}, \
                     but found {found_parent}."
+                )
+            }
+            IrError::InvalidConfigFlags => {
+                write!(
+                    f,
+                    "`config` should have only one byte as flags, for example: 0x00"
                 )
             }
             IrError::VerifyArgumentValueIsNotArgument(callee) => write!(
