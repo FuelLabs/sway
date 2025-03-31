eedfa7d75c263c2188b5e835fbf54b31ab432ae0
Kaya Gokalp <kayagokalp123@gmail.com>
2025-03-31 15:24:37 -0700

    point to fuel owned repo

--- forc-pkg/src/source/reg/mod.rs
+++ forc-pkg/src/source/reg/mod.rs
@@ -1,6 +1,7 @@
 pub(crate) mod file_location;
 pub mod index_file;
 
+use super::IPFSNode;
 use crate::{
     manifest::{self, GenericManifestFile, PackageManifestFile},
     source::{
@@ -19,8 +20,6 @@ use std::{
     path::{Path, PathBuf},
     str::FromStr,
 };
-
-use super::IPFSNode;
 
 const REG_DIR_NAME: &str = "registry";
 
@@ -80,9 +79,9 @@ pub enum PinnedParseError {
 
 impl GithubRegistryResolver {
     /// Default github organization name that holds the registry git repo.
-    const DEFAULT_GITHUB_ORG: &str = "kayagokalp";
+    pub const DEFAULT_GITHUB_ORG: &str = "FuelLabs";
     /// Default name of the repository that holds the registry git repo.
-    const DEFAULT_REPO_NAME: &str = "dummy-forc.pub-index";
+    pub const DEFAULT_REPO_NAME: &str = "forc.pub-index";
     /// Default chunking size of the repository that holds registry git repo.
     pub const DEFAULT_CHUNKING_SIZE: usize = 2;
     /// Default branch name for the repository repo.
