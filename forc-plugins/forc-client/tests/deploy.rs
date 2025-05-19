e6e88c53d83756192c73424afd371bb2dc012ec4
Hannes Karppila <hannes.karppila@gmail.com>
2025-05-19 09:44:42 +0300

    More contract id updates

--- forc-plugins/forc-client/tests/deploy.rs
+++ forc-plugins/forc-client/tests/deploy.rs
@@ -377,7 +377,7 @@ async fn test_simple_deploy() {
     node.kill().unwrap();
     let expected = vec![DeployedPackage::Contract(DeployedContract {
         id: ContractId::from_str(
-            "d60cfc446e133b7571d1d2ef5e64745f4d972a9d5053d4031aed26e3db881dce",
+            "533f424c47fd6b2d45de061ce6c1ecff89410f1817c69f73f8ae3e796a4ca3c1",
         )
         .unwrap(),
         proxy: None,
@@ -421,7 +421,7 @@ async fn test_deploy_submit_only() {
     node.kill().unwrap();
     let expected = vec![DeployedPackage::Contract(DeployedContract {
         id: ContractId::from_str(
-            "d60cfc446e133b7571d1d2ef5e64745f4d972a9d5053d4031aed26e3db881dce",
+            "533f424c47fd6b2d45de061ce6c1ecff89410f1817c69f73f8ae3e796a4ca3c1",
         )
         .unwrap(),
         proxy: None,
@@ -468,7 +468,7 @@ async fn test_deploy_fresh_proxy() {
     node.kill().unwrap();
     let impl_contract = DeployedPackage::Contract(DeployedContract {
         id: ContractId::from_str(
-            "d60cfc446e133b7571d1d2ef5e64745f4d972a9d5053d4031aed26e3db881dce",
+            "533f424c47fd6b2d45de061ce6c1ecff89410f1817c69f73f8ae3e796a4ca3c1",
         )
         .unwrap(),
         proxy: Some(
