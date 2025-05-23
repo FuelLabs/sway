6665e2579757fb54efcc94415031d01db2a58bda
Dannynsikak <nsikakdanny11@gmail.com>
2025-05-23 20:45:23 +0100

    fix: fixed the implement struct bug

--- sway-lsp/tests/lib.rs
+++ sway-lsp/tests/lib.rs
@@ -1384,29 +1384,28 @@ fn go_to_definition_for_structs() {
             req_uri: &uri,
             req_line: 10,
             req_char: 8,
-            def_line: 3, // force lookup to point too struct MyStruct<T, U>
-            def_start_char: 16,
-            def_end_char: 17,
+            def_line: 9,
+            def_start_char: 19,
+            def_end_char: 20,
             def_path: uri.as_str(),
         };
         // Type Params
-        lsp::definition_check(&server, &go_to).await;
+        lsp::definition_check(service.inner(), &go_to).await;
         go_to.def_line = 3;
         go_to.def_start_char = 5;
         go_to.def_end_char = 9;
-        lsp::definition_check_with_req_offset(&server, &mut go_to, 12, 8).await;
-        lsp::definition_check_with_req_offset(&server, &mut go_to, 13, 16).await;
-        lsp::definition_check_with_req_offset(&server, &mut go_to, 14, 9).await;
-        lsp::definition_check_with_req_offset(&server, &mut go_to, 15, 16).await;
-        lsp::definition_check_with_req_offset(&server, &mut go_to, 15, 23).await;
-        // Ensure no incorrect resolution to `impl<T, U>`
+        lsp::definition_check_with_req_offset(service.inner(), &mut go_to, 12, 8).await;
+        lsp::definition_check_with_req_offset(service.inner(), &mut go_to, 13, 16).await;
+        lsp::definition_check_with_req_offset(service.inner(), &mut go_to, 14, 9).await;
+        lsp::definition_check_with_req_offset(service.inner(), &mut go_to, 15, 16).await;
+        lsp::definition_check_with_req_offset(service.inner(), &mut go_to, 15, 23).await;
         go_to = GotoDefinition {
             req_uri: &uri,
             req_line: 16,
             req_char: 11,
-            def_line: 3,
-            def_start_char: 16,
-            def_end_char: 17,
+            def_line: 84,
+            def_start_char: 9,
+            def_end_char: 15,
             def_path: "sway-lib-std/src/option.sw",
         };
         // Type Params
