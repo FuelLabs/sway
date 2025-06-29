50eda9d3bf8ad081ca6f7374f4d18b5378c8caf0
JoshuaBatty <joshpbatty@gmail.com>
2025-06-30 09:39:41 +1000

    remove redundant clap flags

--- forc-plugins/forc-doc/src/lib.rs
+++ forc-plugins/forc-doc/src/lib.rs
@@ -45,39 +45,32 @@ pub struct Command {
     /// Path to the project.
     ///
     /// If not specified, current working directory will be used.
-    #[clap(short, long, alias = "manifest-path")]
+    #[clap(short, alias = "manifest-path")]
     pub path: Option<String>,
     /// Include non-public items in the documentation.
-    #[clap(long)]
     pub document_private_items: bool,
     /// Open the docs in a browser after building them.
-    #[clap(long)]
     pub open: bool,
     /// Offline mode, prevents Forc from using the network when managing dependencies.
     /// Meaning it will only try to use previously downloaded dependencies.
-    #[clap(long = "offline")]
     pub offline: bool,
     /// Requires that the Forc.lock file is up-to-date. If the lock file is missing, or it
     /// needs to be updated, Forc will exit with an error.
-    #[clap(long)]
     pub locked: bool,
     /// Do not build documentation for dependencies.
-    #[clap(long)]
     pub no_deps: bool,
     /// The IPFS Node to use for fetching IPFS sources.
     ///
     /// Possible values: FUEL, PUBLIC, LOCAL, <GATEWAY_URL>
-    #[clap(long)]
     pub ipfs_node: Option<IPFSNode>,
     /// The path to the documentation output directory.
     ///
     /// If not specified, the default documentation output directory will be used.
-    #[clap(long)]
     pub doc_path: Option<String>,
     #[clap(flatten)]
     pub experimental: sway_features::CliFields,
     /// Silent mode. Don't output any warnings or errors to the command line.
-    #[clap(long = "silent", short = 's')]
+    #[clap(short = 's')]
     pub silent: bool,
 }
 
