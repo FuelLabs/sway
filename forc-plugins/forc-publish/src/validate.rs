8963312ba87b776cb8071f96ef1c34c1e1a1406e
kaya <20915464+kayagokalp@users.noreply.github.com>
2025-07-21 19:03:58 -0700

    feat: add preliminary local checks for forc-publish (#7253)
    ## Description
    closes #7252.
    
    This pull request introduces enhancements to the `forc-publish` plugin,
    focusing on validating project configurations and improving error
    handling. Key changes include the addition of a `validate` module for
    preemptive checks, updates to error definitions, and modifications to
    the tarball creation logic to incorporate validation. New test cases and
    sample data directories have also been added to ensure robust validation
    functionality.
    
    ### Validation Enhancements:
    * Added a new `validate` module in `forc-publish/src/validate.rs` to
    check for missing `Forc.toml`, missing version fields, and unsupported
    dependency types.
    * Updated the `Error` enum in `forc-publish/src/error.rs` to include new
    error variants for validation failures like `InvalidForcToml`,
    `MissingVersionField`, and `DependencyMissingVersion`.
    
    ### Tarball Creation Updates:
    * Refactored `create_tarball_from_current_dir` in
    `forc-publish/src/tarball.rs` to use the new `validate_dir` function for
    preemptive validation. Removed redundant checks for `Forc.toml`
    existence.
    * Adjusted test cases in `forc-publish/src/tarball.rs` to use sample
    directories with predefined configurations for validation testing.
    [[1]](diffhunk://#diff-e3f20cadab3a1cf99fc967e0adc12e908f435a185e1529df9b152388927adf45L79-R85)
    [[2]](diffhunk://#diff-e3f20cadab3a1cf99fc967e0adc12e908f435a185e1529df9b152388927adf45L140-R135)
    
    ### Test Data Additions:
    * Added sample directories under `tests/data` for various validation
    scenarios, including projects with missing versions, dependencies
    without versions, and valid configurations.
    [[1]](diffhunk://#diff-b171d6da23a1fe36735cd962cfcf506831115c3714654236b31dd999c2853823R1-R11)
    [[2]](diffhunk://#diff-016348eaea4a95d041537b29d370619ab0d5ca99cc84f9b30d8ad561aa757a1eR1-R9)
    [[3]](diffhunk://#diff-0495f782769450a8c200fdcc1f29805f4811a26f4849720fa34d5b37ba45e471R1-R8)
    * Included `.gitignore` files in test data directories to exclude `out`
    and `target` folders.

---
+++ forc-plugins/forc-publish/src/validate.rs
@@ -0,0 +1,96 @@
+use crate::error::{Error, Result};
+use forc_pkg::manifest::{GenericManifestFile, ManifestFile};
+use std::path::Path;
+
+/// Checks the following cases for an early error generation:
+///  1. Target dir doesn't contain a Forc.toml
+///  2. Target manifest file doesn't contain a version
+///  3. Target project's dependencies are not all version based (git or path
+///     based dep detection)
+pub fn validate_dir(path: &Path) -> Result<()> {
+    // Check if Forc.toml exists
+    let forc_toml_path = path.join("Forc.toml");
+    if !forc_toml_path.exists() {
+        return Err(Error::ForcTomlNotFound);
+    }
+
+    let manifest_file = ManifestFile::from_file(forc_toml_path)?;
+
+    match manifest_file {
+        ManifestFile::Package(package_manifest_file) => {
+            // Check if the version exists
+            if package_manifest_file.as_ref().project.version.is_none() {
+                return Err(Error::MissingVersionField);
+            }
+
+            // Check if all the dependencies are declared with dep
+            for (dep_name, dep) in package_manifest_file
+                .as_ref()
+                .dependencies
+                .iter()
+                .flat_map(|deps| deps.iter())
+            {
+                if dep.version().is_none() {
+                    return Err(Error::DependencyMissingVersion(dep_name.to_string()));
+                }
+            }
+        }
+        ManifestFile::Workspace(_) => {
+            return Err(Error::WorkspaceNotSupported);
+        }
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::validate_dir;
+    use std::path::PathBuf;
+
+    #[test]
+    fn without_version_should_fail() {
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let tests_path = manifest_dir.join("tests").join("data");
+
+        let no_version_project_test = tests_path.join("without_version");
+        let res = validate_dir(&no_version_project_test);
+
+        assert!(res.is_err());
+        assert!(res.err().is_some_and(|err| {
+            err.to_string() == "missing version field in [project] section of Forc.toml"
+        }));
+    }
+
+    #[test]
+    fn deps_without_version_should_fail() {
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let tests_path = manifest_dir.join("tests").join("data");
+
+        let no_version_project_test = tests_path.join("deps_without_version");
+        let res = validate_dir(&no_version_project_test);
+
+        assert!(res.is_err());
+        assert!(res.err().is_some_and(|err| {
+            err.to_string() == "dep_a is not a forc.pub dependency, depend on it using version."
+        }));
+    }
+
+    #[test]
+    fn success_without_deps() {
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let tests_path = manifest_dir.join("tests").join("data");
+
+        let success_without_deps = tests_path.join("success_with_no_deps");
+        validate_dir(&success_without_deps).unwrap()
+    }
+
+    #[test]
+    fn success_with_deps() {
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let tests_path = manifest_dir.join("tests").join("data");
+
+        let success_without_deps = tests_path.join("success_with_deps");
+        validate_dir(&success_without_deps).unwrap()
+    }
+}
---
+++ forc-plugins/forc-publish/tests/data/without_version/src/lib.sw
@@ -0,0 +1,3 @@
+library;
+
+// anything `pub` here will be exported as a part of this library's API
