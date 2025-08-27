8cb7c57344d230258222110e22fef54e8bdef165
Elaela Solis <elaelasoliss@gmail.com>
2025-08-27 03:49:29 -0300

    chore: fix typos (#7358)
    ## Description
    
    Corrected misspellings:
    - `more then` → `more than`

--- sway-core/src/semantic_analysis/module.rs
+++ sway-core/src/semantic_analysis/module.rs
@@ -710,7 +710,7 @@ fn check_is_valid_error_type_enum(
     {
         let (last_occurrence, previous_occurrences) = duplicated_error_messages
             .split_last()
-            .expect("`duplicated_error_messages` has more then one element");
+            .expect("`duplicated_error_messages` has more than one element");
         handler.emit_warn(CompileWarning {
             span: last_occurrence.clone(),
             warning_content: Warning::ErrorDuplicatedErrorMessage {
