f57dd52ca3939c6360f1d12042ab456edd15922d
xunilrj <xunilrj@hotmail.com>
2025-10-14 11:59:01 -0300

    fix parsed_decl_id

--- sway-core/src/decl_engine/id.rs
+++ sway-core/src/decl_engine/id.rs
@@ -245,7 +245,7 @@ impl SubstTypes for DeclId<TyConstantDecl> {
         let decl_engine = ctx.engines.de();
         let mut decl = (*decl_engine.get(self)).clone();
         if decl.subst(ctx).has_changes() {
-            *self = *decl_engine.insert(decl, None).id();
+            *self = *decl_engine.insert(decl, decl_engine.get_parsed_decl_id(self).as_ref()).id();
             HasChanges::Yes
         } else {
             HasChanges::No
