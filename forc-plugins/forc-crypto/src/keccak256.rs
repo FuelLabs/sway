5a4e599731a48fc87aac7be9db15808549d6f1b4
Joshua Batty <joshpbatty@gmail.com>
2025-12-09 18:06:43 +1100

    remove forc-crypto

--- forc-plugins/forc-crypto/src/keccak256.rs
+++
@@ -1,9 +0,0 @@
-use serde_json::json;
-use sha3::{Digest, Keccak256};
-
-/// Hashes a given data using Keccak256
-pub fn hash<T: Into<Vec<u8>>>(data: T) -> anyhow::Result<serde_json::Value> {
-    let mut hasher = Keccak256::new();
-    hasher.update(data.into());
-    Ok(json!(hex::encode(hasher.finalize())))
-}
