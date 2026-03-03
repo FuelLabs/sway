56c192beb8e64f8927ab777409a69a91c8a9f88f
xunilrj <xunilrj@hotmail.com>
2026-03-03 10:16:28 -0300

    removing test-proj

--- test-proj/build.rs
+++
@@ -1,7 +0,0 @@
-//! This build script is used to compile the sway project using `forc` prior to running tests.
-
-use std::process::Command;
-
-fn main() {
-    Command::new("forc").args(&["build"]).status().unwrap();
-}
