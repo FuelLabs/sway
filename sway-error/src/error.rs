8d5a41ec931d025fd2cfb7dd1aa89246df9ca591
xunilrj <xunilrj@hotmail.com>
2026-03-24 07:54:13 -0300

    TrivialBool and TrivialEnum

--- sway-error/src/error.rs
+++ sway-error/src/error.rs
@@ -3388,7 +3388,7 @@ impl ToDiagnostic for CompileError {
                     issue: Issue::error(
                         source_engine,
                         span.clone(),
-                        format!("{} is not trivially decodable.", span.as_str()),
+                        format!("`{}` is not trivially decodable.", span.as_str()),
                     ),
                     hints,
                     help: [
