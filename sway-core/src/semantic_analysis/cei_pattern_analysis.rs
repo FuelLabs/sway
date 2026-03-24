44a9aed2f58d330f3353857e04b0b5e0d8c52e3a
xunilrj <xunilrj@hotmail.com>
2026-03-24 07:58:38 -0300

    fmt and clippy issues

--- sway-core/src/semantic_analysis/cei_pattern_analysis.rs
+++ sway-core/src/semantic_analysis/cei_pattern_analysis.rs
@@ -679,7 +679,7 @@ fn effects_of_intrinsic(intr: &sway_ast::Intrinsic) -> HashSet<Effect> {
         | Dbg
         | Alloc
         | RuntimeMemoryId
-        | EncodingMemoryId 
+        | EncodingMemoryId
         | EnumDiscriminantCount => HashSet::new(),
     }
 }
