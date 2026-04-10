0507c2c9a65a147ef2026fda8ac52ba7223aca40
Igor Rončević <ironcev@hotmail.com>
2026-04-10 12:16:35 +0200

    Update `fuel-vm` and `fuels` dependencies in `sdk-harness` tests (#7591)
    ## Description
    
    This PR updates the `fuel-vm` and `fuels` dependencies in `sdk-harness`
    tests to the same versions used in the rest of the Sway repository.
    
    The PR extends the `check-sdk-harness-version.sh` script to also be able
    to check the `fuel-vm` and `fuels` dependencies, and adds those to the
    `PACKAGE_NAMES` to be checked in the CI step.
    
    ## Checklist
    
    - [ ] I have linked to any relevant issues.
    - [x] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have updated the documentation where relevant (API docs, the
    reference, and the Sway book).
    - [ ] If my change requires substantial documentation changes, I have
    [requested support from the DevRel
    team](https://github.com/FuelLabs/devrel-requests/issues/new/choose)
    - [ ] I have added tests that prove my fix is effective or that my
    feature works.
    - [ ] I have added (or requested a maintainer to add) the necessary
    `Breaking*` or `New Feature` labels where relevant.
    - [x] I have done my best to ensure that my PR adheres to [the Fuel Labs
    Code Review
    Standards](https://github.com/FuelLabs/rfcs/blob/master/text/code-standards/external-contributors.md).
    - [x] I have requested a review from the relevant team or maintainers.

--- .github/workflows/scripts/check-sdk-harness-version.sh
+++ .github/workflows/scripts/check-sdk-harness-version.sh
@@ -1,17 +1,32 @@
+# Get the version of a package from a Cargo.toml file.
+# The version can be specified in two ways:
+# 1. some_package = { version = "x.y.z", ... }
+# 2. some_package = "x.y.z"
+get_version() {
+	local file="$1"
+	local key="$2"
+	local version
+	version=$(toml get "$file" "$key.version" 2>/dev/null)
+	if [ -z "$version" ]; then
+		version=$(toml get "$file" "$key" 2>/dev/null)
+	fi
+	echo "$version"
+}
+
 IFS=',' read -ra PACKAGES <<< "$PACKAGE_NAMES"
 mismatch=0
 for PACKAGE in "${PACKAGES[@]}"; do
-	VERSION_FIRST=$(toml get ./Cargo.toml "workspace.dependencies.$PACKAGE.version")
-	VERSION_SECOND=$(toml get ./test/src/sdk-harness/Cargo.toml "dependencies.$PACKAGE.version")
-        printf "$PACKAGE Version - First: $VERSION_FIRST, Second: $VERSION_SECOND\n"
+	VERSION_FIRST=$(get_version ./Cargo.toml "workspace.dependencies.$PACKAGE")
+	VERSION_SECOND=$(get_version ./test/src/sdk-harness/Cargo.toml "dependencies.$PACKAGE")
+        printf "$PACKAGE\n    sway repo:   $VERSION_FIRST\n    sdk-harness: $VERSION_SECOND\n"
         if [ "$VERSION_FIRST" != "$VERSION_SECOND" ]; then
-        	printf "Version mismatch for $PACKAGE: First: $VERSION_FIRST, Second: $VERSION_SECOND\n"
+        	printf "ERROR: Version mismatch for $PACKAGE\n"
         	mismatch=1
         fi
 done
 if [ $mismatch -ne 0 ]; then
-	printf "Version mismatch between fuel-core-client used in sdk-harness and rest of sway repo.\nThis will cause problems if two versions are incompatible or it might simply cause invalid/outdated test suite.\nIf you are bumping fuel-core versions used in sway repo, please also use same version in sdk-harness.\n"
+	printf "\nVersion mismatch between dependencies used in the sdk-harness tests and the rest of the sway repository.\nThis will cause problems if two versions are incompatible or it might simply cause invalid/outdated test suite.\nIf you are bumping dependency versions used in the sway repo, please use the same versions in the sdk-harness.\n"
 	exit 1
 else
-	echo "All specified package versions match."
+	printf "\nAll specified package versions match.\n"
 fi
