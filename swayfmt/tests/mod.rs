e543db405922241c5188d37e2358335671277f4c
Dnreikronos <dnreikronos2@gmail.com>
2026-06-11 19:41:09 -0300

    Add empty if-else tests for comments, displaced else, and nested else if

--- swayfmt/tests/mod.rs
+++ swayfmt/tests/mod.rs
@@ -2160,6 +2160,102 @@ fn empty_if_with_else() {
 }
 
 #[test]
+fn empty_if_else_if_with_block_comment() {
+    check(
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 { /* Block comment */    }    else
+            if __is_reference_type::<T>() {
+                let i = 42;
+            }
+        }
+        "#},
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 { /* Block comment */     } else if __is_reference_type::<T>() {
+                let i = 42;
+            }
+        }
+        "#},
+    );
+}
+
+#[test]
+fn empty_if_else_if_with_line_comment() {
+    check(
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 {
+                // Single line comment.
+            }    else    if __is_reference_type::<T>() {
+                let i = 42;
+            }
+        }
+        "#},
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 {
+                // Single line comment.
+            } else if __is_reference_type::<T>() {
+                let i = 42;
+            }
+        }
+        "#},
+    );
+}
+
+#[test]
+fn empty_if_else_if_chain_collapsed() {
+    check(
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 { } else if __is_reference_type::<T>() {    } else { let i = 42; }
+        }
+        "#},
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 {
+            } else if __is_reference_type::<T>() {
+            } else {
+                let i = 42;
+            }
+        }
+        "#},
+    );
+}
+
+#[test]
+fn empty_if_multiple_else_if_levels() {
+    check(
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 {    } else if __is_reference_type::<T>() {    } else if __is_reference_type::<U>() {    } else {
+                let i = 42;
+            }
+        }
+        "#},
+        indoc! {r#"
+        library;
+        fn test() {
+            if __size_of::<T>() == 0 {
+            } else if __is_reference_type::<T>() {
+            } else if __is_reference_type::<U>() {
+            } else {
+                let i = 42;
+            }
+        }
+        "#},
+    );
+}
+
+#[test]
 fn bug_whitespace_added_after_comment() {
     check(
         indoc! {r#"
